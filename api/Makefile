9d331acd6aa1b6d8050f0daa4b6f5be32d148e2c
Karthik Thirumalasetti <>
2022-04-04 22:15:34 +0530

    wip

--- api/Makefile
+++ api/Makefile
@@ -1,6 +1,7 @@
-wireguard.gql:
-	cd apps/wireguard/internal/app && go run github.com/99designs/gqlgen
 
+# wireguard
+wireguard.gql:
+	cd apps/wireguard/internal/app && go run github.com/99designs/gqlgen generate
 wireguard.run: export MONGO_URI = mongodb://localhost:27017
 wireguard.run: export MONGO_DB_NAME = sample
 wireguard.run: export PORT = 3000
--- api/apps/wireguard/internal/domain/entities/device.go
+++ api/apps/wireguard/internal/domain/entities/device.go
@@ -2,21 +2,13 @@ package entities
 
 import "kloudlite.io/pkg/repos"
 
-type Peer struct {
-	Id         repos.ID `json:"id" bson:"id"`
-	Address    *string  `json:"address" bson:"address"`
-	PublicKey  *string  `json:"public_key" bson:"public_key"`
-	AllowedIPs []string `json:"allowed_i_ps" bson:"allowed_i_ps"`
-}
-
 type Device struct {
 	repos.BaseEntity `bson:",inline"`
-	Name             string          `json:"name" bson:"name"`
-	ClusterId        repos.ID        `json:"cluster_id" bson:"cluster_id"`
-	UserId           repos.ID        `json:"user_id" bson:"user_id"`
-	PrivateKey       *string         `json:"private_key" bson:"private_key"`
-	PublicKey        *string         `json:"public_key" bson:"public_key"`
-	Peers            map[string]Peer `json:"peers" bson:"peers"`
+	Name             string   `json:"name" bson:"name"`
+	ClusterId        repos.ID `json:"cluster_id" bson:"cluster_id"`
+	UserId           repos.ID `json:"user_id" bson:"user_id"`
+	PrivateKey       *string  `json:"private_key" bson:"private_key"`
+	PublicKey        *string  `json:"public_key" bson:"public_key"`
 }
 
 /*
