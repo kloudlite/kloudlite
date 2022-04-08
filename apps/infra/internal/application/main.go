2ca020aaecf0e5bb8ba2222498f6e5798afa3a80
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-04-09 00:29:44 +0530

    :construction: wip
    Former-commit-id: 6b1b087690f104dce954fd75cde2631b32940410

--- apps/infra/internal/application/main.go
+++ apps/infra/internal/application/main.go
@@ -4,11 +4,12 @@ import (
 	"go.uber.org/fx"
 	"kloudlite.io/apps/infra/internal/domain"
 	"kloudlite.io/pkg/config"
-	"kloudlite.io/pkg/logger"
-	"kloudlite.io/pkg/messaging"
+	// "kloudlite.io/pkg/logger"
+	// "kloudlite.io/pkg/messaging"
 )
 
 type InfraEnv struct {
+	DoImageId       string `env:"DO_IMAGE_ID", required:"true"`
 	DoAPIKey        string `env:"DO_API_KEY", required:"true"`
 	DataPath        string `env:"DATA_PATH", required:"true"`
 	SshKeysPath     string `env:"SSH_KEYS_PATH", required:"true"`
@@ -16,30 +17,30 @@ type InfraEnv struct {
 	KafkaGroupId    string `env:"KAFKA_GROUP_ID", required:"true"`
 }
 
-func fxConsumer(env *InfraEnv, mc messaging.KafkaClient, d domain.Domain, logger logger.Logger) (messaging.Consumer, error) {
-	consumer, err := messaging.NewKafkaConsumer[domain.SetupClusterAction](
-		mc,
-		[]string{env.KafkaInfraTopic},
-		env.KafkaGroupId,
-		logger,
-		func(topic string, action domain.SetupClusterAction) error {
-			logger.Debugf("topic (%s) action (%+v)", topic, action)
-			return d.CreateCluster(action)
-			// return errors.New("just kidding")
-		},
-	)
+// func fxConsumer(env *InfraEnv, mc messaging.KafkaClient, d domain.Domain, logger logger.Logger) (messaging.Consumer, error) {
+// 	consumer, err := messaging.NewKafkaConsumer[domain.SetupClusterAction](
+// 		mc,
+// 		[]string{env.KafkaInfraTopic},
+// 		env.KafkaGroupId,
+// 		logger,
+// 		func(topic string, action domain.SetupClusterAction) error {
+// 			logger.Debugf("topic (%s) action (%+v)", topic, action)
+// 			return d.CreateCluster(action)
+// 			// return errors.New("just kidding")
+// 		},
+// 	)
 
-	return consumer, err
-}
+// 	return consumer, err
+// }
 
-func fxProducer(mc messaging.KafkaClient) (messaging.Producer[messaging.Json], error) {
-	return messaging.NewKafkaProducer[messaging.Json](mc)
-}
+// func fxProducer(mc messaging.KafkaClient) (messaging.Producer[messaging.Json], error) {
+// 	return messaging.NewKafkaProducer[messaging.Json](mc)
+// }
 
 var Module = fx.Module("application",
 	fx.Provide(config.LoadEnv[InfraEnv]()),
 	fx.Provide(fxInfraClient),
-	fx.Provide(fxProducer),
+	// fx.Provide(fxProducer),
 	domain.Module,
 	//fx.Provide(fxConsumer),
 
--- apps/infra/internal/application/tf/do/init.sh
+++ apps/infra/internal/application/tf/do/init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+/root/scripts/wait-for-on.sh
+
 cat >> /root/.ssh/authorized_keys << EOF
 ${pubkey}
 EOF
+
