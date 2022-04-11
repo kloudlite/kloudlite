edb41a74d96f0b2f2e0e2123d9a1eda8d844b255
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-04-11 13:31:25 +0530

    :construction: wip
    Former-commit-id: 64cc34c6e2220b529f024c50d1f0c57a2b26e3da

--- apps/infra/infra-scripts/do/tf/variables.tf
+++ apps/infra/infra-scripts/do/tf/variables.tf
@@ -20,6 +20,7 @@ variable "keys-path" {
 }
 variable "do-image-id" {
   default = "ubuntu-20-04-x64"
+  # default = "105910703"
 }
 variable "ssh_keys" {
   default = ["25:d8:56:2b:70:15:43:c5:dd:e2:ff:d7:47:1b:68:22"]
