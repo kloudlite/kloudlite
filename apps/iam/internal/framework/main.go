cd10fb9daf7697cbe06451f798f0cc9d466c5d9f
Karthik Thirumalasetti <>
2022-04-15 09:47:24 +0530

    t
    Former-commit-id: 3b88f0bee6d242dbcd3766858d6496cc02b91de9

--- apps/iam/internal/framework/main.go
+++ apps/iam/internal/framework/main.go
@@ -2,12 +2,11 @@ package framework
 
 import (
 	"context"
-	"fmt"
 	"go.uber.org/fx"
 	"google.golang.org/grpc"
 	"kloudlite.io/apps/iam/internal/application"
 	"kloudlite.io/pkg/config"
-	"net"
+	rpc "kloudlite.io/pkg/grpc"
 )
 
 type Env struct {
@@ -21,18 +20,7 @@ var Module = fx.Module("framework",
 	fx.Invoke(func(lifecycle fx.Lifecycle, env *Env, server *grpc.Server) {
 		lifecycle.Append(fx.Hook{
 			OnStart: func(ctx context.Context) error {
-				listen, portFormatError := net.Listen("tcp", fmt.Sprintf(":%d", env.Port))
-				if portFormatError != nil {
-					return portFormatError
-				}
-				go func() error {
-					serverStartError := server.Serve(listen)
-					if serverStartError != nil {
-						return serverStartError
-					}
-					return nil
-				}()
-				return nil
+				return rpc.GRPCStartServer(ctx, server, env.Port)
 			},
 			OnStop: func(ctx context.Context) error {
 				server.Stop()
