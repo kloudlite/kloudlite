a782158188173325bc4739c0a7722ab505fe1883
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-04-15 15:20:00 +0530

    :construction: wip
    Former-commit-id: 506b31dedae5e08c09e2bfa4766658409cc74574

---
+++ api/apps/finance/internal/app/graph/entity.resolvers.go
@@ -0,0 +1,29 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+
+	"kloudlite.io/apps/finance/internal/app/graph/generated"
+	"kloudlite.io/apps/finance/internal/app/graph/model"
+	"kloudlite.io/pkg/repos"
+)
+
+func (r *entityResolver) FindClusterByID(ctx context.Context, id repos.ID) (*model.Cluster, error) {
+	return r.Query().GetCluster(ctx, id)
+}
+
+func (r *entityResolver) FindDeviceByID(ctx context.Context, id repos.ID) (*model.Device, error) {
+	return r.Query().GetDevice(ctx, id)
+}
+
+func (r *entityResolver) FindUserByID(ctx context.Context, id repos.ID) (*model.User, error) {
+	return &model.User{ID: id}, nil
+}
+
+// Entity returns generated.EntityResolver implementation.
+func (r *Resolver) Entity() generated.EntityResolver { return &entityResolver{r} }
+
+type entityResolver struct{ *Resolver }
