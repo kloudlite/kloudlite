2d8fda1b00c45e1c57db9278761b4996dfa01b7a
Karthik Thirumalasetti <>
2022-04-15 19:33:29 +0530

    clean up
    Former-commit-id: 314b10be3b12c9451c4901ac355bfcca6eca0d07

---
+++ common/constants.go
@@ -0,0 +1,17 @@
+package common
+
+const (
+	ResourceConfig          = "config"
+	ResourceSecret          = "secret"
+	ResourceApp             = "app"
+	ResourceProject         = "project"
+	ResourceRouter          = "router"
+	ResourceManagedService  = "mService"
+	ResourceManagedResource = "mResource"
+	ResourceGitPipeline     = "gitPipeline"
+)
+
+const (
+	CacheSessionPrefix = "hotspot:auth:sessions"
+	CookieName         = "hotspot-session"
+)
