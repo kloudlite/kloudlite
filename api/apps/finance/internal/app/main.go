621ab5a632c734e788cd8821d5505b0aca94f831
Karthik Thirumalasetti <>
2022-04-16 11:45:23 +0530

    init
    Former-commit-id: dbedf7c25e25005462372c2306b450abe66fba2d

---
+++ api/apps/finance/internal/app/main.go
@@ -0,0 +1,42 @@
+package app
+
+import (
+	"go.uber.org/fx"
+	"kloudlite.io/apps/finance/internal/app/graph"
+	"kloudlite.io/apps/finance/internal/app/graph/generated"
+	"kloudlite.io/apps/finance/internal/domain"
+	"kloudlite.io/common"
+	"kloudlite.io/pkg/cache"
+	"kloudlite.io/pkg/config"
+	httpServer "kloudlite.io/pkg/http-server"
+	"net/http"
+)
+
+type Env struct {
+	CookieDomain string `env:"COOKIE_DOMAIN" required:"true"`
+}
+
+var Module = fx.Module(
+	"application",
+	fx.Provide(config.LoadEnv[Env]()),
+	fx.Invoke(func(
+		server *http.ServeMux,
+		d domain.Domain,
+		env *Env,
+		cacheClient cache.Client,
+	) {
+		schema := generated.NewExecutableSchema(
+			generated.Config{Resolvers: graph.NewResolver(d)},
+		)
+		httpServer.SetupGQLServer(
+			server,
+			schema,
+			cache.NewSessionRepo[*common.AuthSession](
+				cacheClient,
+				common.CookieName,
+				env.CookieDomain,
+				common.CacheSessionPrefix,
+			),
+		)
+	}),
+)
