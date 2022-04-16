1c23500b4f01b3d4c5fc2a20cd357c742c1818b0
Karthik Thirumalasetti <>
2022-04-16 09:25:12 +0530

    cleanup
    Former-commit-id: 90d6e91dc28f90b381675e35bbc54b25a5c15023

--- apps/auth/internal/app/messenger.go
+++ apps/auth/internal/app/messenger.go
@@ -4,23 +4,14 @@ import (
 	"context"
 	"fmt"
 	"kloudlite.io/apps/auth/internal/domain"
+	"kloudlite.io/pkg/messaging"
 )
 
 type messengerI struct {
 }
 
-func (m *messengerI) SendVerificationEmail(ctx context.Context, invitationId string, user *domain.User) error {
-	fmt.Println("VERIFICATION", invitationId, user)
-	return nil
-}
-
-func (m *messengerI) SendWelcomeEmail(ctx context.Context, invitationId string, user *domain.User) error {
-	fmt.Println("WELCOME", invitationId, user)
-	return nil
-}
-
-func (m *messengerI) SendResetPasswordEmail(ctx context.Context, invitationId string, user *domain.User) error {
-	fmt.Println("RESETPASSWORD", invitationId, user)
+func (m *messengerI) SendEmail(ctx context.Context, template string, payload messaging.Json) error {
+	fmt.Println(template, payload)
 	return nil
 }
 
