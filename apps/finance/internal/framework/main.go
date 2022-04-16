e1ff20aec74a931f91e3dc1e396b2825d66550a1
Karthik Thirumalasetti <>
2022-04-16 16:20:45 +0530

    WIP
    Former-commit-id: 9050678b1812bd4f18ae5e2e53de42abd0587af4

--- apps/finance/internal/framework/main.go
+++ apps/finance/internal/framework/main.go
@@ -2,20 +2,45 @@ package framework
 
 import (
 	"go.uber.org/fx"
+	"kloudlite.io/apps/finance/internal/app"
+	"kloudlite.io/pkg/cache"
 	"kloudlite.io/pkg/config"
+	httpServer "kloudlite.io/pkg/http-server"
+	"kloudlite.io/pkg/logger"
 	"kloudlite.io/pkg/repos"
 )
 
 type Env struct {
-	DBName string `env:"DB_NAME"`
-	DBUrl  string `env:"DB_URL"`
+	DBName        string `env:"DB_NAME"`
+	DBUrl         string `env:"DB_URL"`
+	RedisHosts    string `env:"REDIS_HOSTS"`
+	RedisUsername string `env:"REDIS_USERNAME"`
+	RedisPassword string `env:"REDIS_PASSWORD"`
+	HttpPort      uint16 `env:"HTTP_PORT"`
+	HttpCors      string `env:"ORIGINS"`
 }
 
 func (e *Env) GetMongoConfig() (url string, dbName string) {
 	return e.DBUrl, e.DBName
 }
 
+func (e *Env) RedisOptions() (hosts, username, password string) {
+	return e.RedisHosts, e.RedisUsername, e.RedisPassword
+}
+
+func (e *Env) GetHttpPort() uint16 {
+	return e.HttpPort
+}
+
+func (e *Env) GetHttpCors() string {
+	return e.HttpCors
+}
+
 var Module = fx.Module("framework",
-	fx.Provide(config.LoadEnv[*Env]()),
+	fx.Provide(config.LoadEnv[Env]()),
+	fx.Provide(logger.NewLogger),
 	repos.NewMongoClientFx[*Env](),
+	cache.NewRedisFx[*Env](),
+	httpServer.NewHttpServerFx[*Env](),
+	app.Module,
 )
