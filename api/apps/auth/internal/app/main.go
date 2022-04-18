753a659d6f776c72525521f09da2fb5819316161
nxtcoder17 <nxtcoder17@gmail.com>
2022-04-18 12:02:27 +0530

    WIP.........
    Former-commit-id: d600b55459c899076c6b18e8d4a03565e9124ac7

--- api/apps/auth/internal/app/main.go
+++ api/apps/auth/internal/app/main.go
@@ -16,14 +16,35 @@ import (
 
 type Env struct {
 	CookieDomain string `env:"COOKIE_DOMAIN" required:"true"`
+
+	GithubClientId     string `env:"GITHUB_CLIENT_ID" required:"true"`
+	GithubClientSecret string `env:"GITHUB_CLIENT_SECRET" required:"true"`
+	GithubCallbackUrl  string `env:"GITHUB_CALLBACK_URL" required:"true"`
+	GithubAppId        string `env:"GITHUB_APP_ID" required:"true"`
+	GithubAppPKFile    string `env:"GITHUB_APP_PK_FILE" required:"true"`
+
+	GitlabClientId     string `env:"GITLAB_CLIENT_ID" required:"true"`
+	GitlabClientSecret string `env:"GITLAB_CLIENT_SECRET" required:"true"`
+	GitlabCallbackUrl  string `env:"GITLAB_CALLBACK_URL" required:"true"`
+
+	GoogleClientId     string `env:"GOOGLE_CLIENT_ID" required:"true"`
+	GoogleClientSecret string `env:"GOOGLE_CLIENT_SECRET" required:"true"`
+	GoogleCallbackUrl  string `env:"GOOGLE_CALLBACK_URL" required:"true"`
+}
+
+func (cfg *Env) GithubConfig() (clientId string, clientSecret string, callbackUrl string) {
+	return cfg.GithubClientId, cfg.GithubClientSecret, cfg.GithubCallbackUrl
 }
 
 var Module = fx.Module("app",
-	fx.Provide(config.LoadEnv[Env]()),
+	config.EnvFx[Env](),
 	repos.NewFxMongoRepo[*domain.User]("users", "usr", domain.UserIndexes),
 	repos.NewFxMongoRepo[*domain.AccessToken]("access_tokens", "tkn", domain.AccessTokenIndexes),
 	cache.NewFxRepo[*domain.VerifyToken](),
 	cache.NewFxRepo[*domain.ResetPasswordToken](),
+
+	fx.Provide(fxGithub),
+
 	fx.Provide(fxMessenger),
 	fx.Invoke(func(
 		server *http.ServeMux,
