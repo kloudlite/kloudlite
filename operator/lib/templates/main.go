6c0793ff59bcbc8dfa695429cacd6bc8e9f081e5
nxtcoder17 <nxtcoder17@gmail.com>
2022-04-23 19:23:10 +0530

    revelation, we can write Go Controllers for CRDs generated by helm resources too

---
+++ operator/lib/templates/main.go
@@ -0,0 +1,47 @@
+package templates
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path"
+	"text/template"
+
+	"github.com/Masterminds/sprig"
+	"operators.kloudlite.io/lib/errors"
+)
+
+func UseTemplate(filename templateFile) (KlTemplate, error) {
+	tPath := path.Join(os.Getenv("PWD"), fmt.Sprintf("lib/templates/%s", filename))
+	t, err := template.New(filename.String()).Funcs(sprig.TxtFuncMap()).ParseFiles(tPath)
+	if err != nil {
+		return nil, errors.NewEf(err, "could not parse template %s", filename)
+	}
+	return &kt{t}, nil
+}
+
+type kt struct {
+	*template.Template
+}
+
+func (kt *kt) WithValues(v interface{}) ([]byte, error) {
+	w := new(bytes.Buffer)
+	if err := kt.Execute(w, v); err != nil {
+		return nil, errors.NewEf(err, "could not execute template")
+	}
+	return w.Bytes(), nil
+}
+
+type KlTemplate interface {
+	WithValues(v interface{}) ([]byte, error)
+}
+
+type templateFile string
+
+func (tf templateFile) String() string {
+	return string(tf)
+}
+
+const (
+	MongoDBStandalone templateFile = "mongodb-helm-standalone.tmpl.yml"
+)
