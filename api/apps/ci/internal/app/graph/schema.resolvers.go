9734ba8f8a9c017776aea2380ac147b40261cee9
Karthik Thirumalasetti <>
2022-04-26 17:45:42 +0530

    wip
    Former-commit-id: 33fbee4681d2a2cc154376dfcd10528843dc2635

--- api/apps/ci/internal/app/graph/schema.resolvers.go
+++ api/apps/ci/internal/app/graph/schema.resolvers.go
@@ -5,15 +5,43 @@ package graph
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 
 	"kloudlite.io/apps/ci/internal/app/graph/generated"
 	"kloudlite.io/apps/ci/internal/app/graph/model"
+	"kloudlite.io/apps/ci/internal/domain"
+	"kloudlite.io/common"
+	"kloudlite.io/pkg/cache"
 	"kloudlite.io/pkg/repos"
 )
 
 func (r *mutationResolver) CiDeleteGitPipeline(ctx context.Context, pipelineID repos.ID) (bool, error) {
 	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CiCreatePipeline(ctx context.Context, in model.GitPipelineIn) (map[string]interface{}, error) {
+	pipeline, err := r.Domain.CretePipeline(ctx, domain.Pipeline{
+		Name:                 in.Name,
+		ImageName:            in.ImageName,
+		GitProvider:          in.GitProvider,
+		GitRepoUrl:           in.GitRepoURL,
+		DockerFile:           in.DockerFile,
+		ContextDir:           in.ContextDir,
+		GithubInstallationId: in.GithubInstallationID,
+		BuildArgs:            in.BuildArgs,
+	})
+	marshal, err := json.Marshal(pipeline)
+	if err != nil {
+		return nil, err
+	}
+	x := make(map[string]any)
+	err = json.Unmarshal(marshal, &x)
+	if err != nil {
+		return nil, err
+	}
+	return x, err
 }
 
 func (r *queryResolver) CiGitlabRepos(ctx context.Context, groupID repos.ID, search *string, limit *int, page *int) ([]map[string]interface{}, error) {
@@ -28,28 +56,59 @@ func (r *queryResolver) CiGitlabRepoBranches(ctx context.Context, repoURL string
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) CiGithubInstallations(ctx context.Context) ([]map[string]interface{}, error) {
+func (r *queryResolver) CiGithubInstallations(ctx context.Context) (interface{}, error) {
+	session := cache.GetSession[*common.AuthSession](ctx)
+	if session == nil {
+		return nil, errors.New("not authenticated")
+	}
+	return r.Domain.GithubListInstallations(ctx, session.UserId)
+}
+
+func (r *queryResolver) CiGithubRepos(ctx context.Context, installationID int, limit *int, page *int) (interface{}, error) {
+	session := cache.GetSession[*common.AuthSession](ctx)
+	if session == nil {
+		return nil, errors.New("not authenticated")
+	}
+	return r.Domain.GithubListRepos(ctx, session.UserId, int64(installationID), *limit, *page)
+}
+
+func (r *queryResolver) CiGithubRepoBranches(ctx context.Context, repoURL string, limit *int, page *int) (interface{}, error) {
+	session := cache.GetSession[*common.AuthSession](ctx)
+	if session == nil {
+		return nil, errors.New("not authenticated")
+	}
+	branches, err := r.Domain.GithubListBranches(ctx, session.UserId, repoURL, *page, *limit)
+	return branches, err
+}
+
+func (r *queryResolver) CiSearchGithubRepos(ctx context.Context, search *string, org string, limit *int, page *int) (interface{}, error) {
+	session := cache.GetSession[*common.AuthSession](ctx)
+	if session == nil {
+		return nil, errors.New("not authenticated")
+	}
+	return r.Domain.GithubSearchRepos(ctx, session.UserId, *search, org, *page, *limit)
+}
+
+func (r *queryResolver) CiGetPipelines(ctx context.Context, projectID repos.ID) ([]*model.GitPipeline, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) CiGithubRepos(ctx context.Context, installationID string, limit *int, page *int) ([]map[string]interface{}, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *queryResolver) CiGithubRepoBranches(ctx context.Context, repoURL string, limit *int, page *int) ([]map[string]interface{}, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *queryResolver) CiSearchGithubRepos(ctx context.Context, search *string, org string, limit *int, page *int) ([]map[string]interface{}, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *queryResolver) CiGitPipelines(ctx context.Context, projectID repos.ID, query map[string]interface{}) ([]*model.GitPipeline, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *queryResolver) CiGitPipeline(ctx context.Context, pipelineID repos.ID) (*model.GitPipeline, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *queryResolver) CiGetPipeline(ctx context.Context, pipelineID repos.ID) (*model.GitPipeline, error) {
+	pipelineE, err := r.Domain.GetPipeline(ctx, pipelineID)
+	if err != nil {
+		return nil, err
+	}
+	return &model.GitPipeline{
+		ID:                   pipelineE.Id,
+		Name:                 pipelineE.Name,
+		ImageName:            pipelineE.ImageName,
+		GitProvider:          pipelineE.GitProvider,
+		GitRepoURL:           pipelineE.GitRepoUrl,
+		DockerFile:           pipelineE.DockerFile,
+		ContextDir:           pipelineE.ContextDir,
+		GithubInstallationID: pipelineE.GithubInstallationId,
+		BuildArgs:            pipelineE.BuildArgs,
+	}, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
--- api/apps/ci/internal/domain/entities.go
+++ api/apps/ci/internal/domain/entities.go
@@ -7,16 +7,16 @@ import (
 
 type Pipeline struct {
 	repos.BaseEntity     `bson:",inline"`
-	Name                 string            `json:"name,omitempty"`
-	ImageName            string            `json:"image_name,omitempty"`
-	PipelineEnv          string            `json:"pipeline_env,omitempty"`
-	GitProvider          string            `json:"git_provider,omitempty"`
-	GitRepoUrl           string            `json:"git_repo_url,omitempty"`
-	DockerFile           string            `json:"docker_file,omitempty"`
-	ContextDir           string            `json:"context_dir,omitempty"`
-	GithubInstallationId string            `json:"github_installation_id,omitempty"`
-	GitlabTokenId        string            `json:"gitlab_token,omitempty"`
-	BuildArgs            map[string]string `json:"build_args,omitempty"`
+	Name                 string                 `json:"name,omitempty"`
+	ImageName            string                 `json:"image_name,omitempty"`
+	PipelineEnv          string                 `json:"pipeline_env,omitempty"`
+	GitProvider          string                 `json:"git_provider,omitempty"`
+	GitRepoUrl           string                 `json:"git_repo_url,omitempty"`
+	DockerFile           *string                `json:"docker_file,omitempty"`
+	ContextDir           *string                `json:"context_dir,omitempty"`
+	GithubInstallationId *int                   `json:"github_installation_id,omitempty"`
+	GitlabTokenId        string                 `json:"gitlab_token,omitempty"`
+	BuildArgs            map[string]interface{} `json:"build_args,omitempty"`
 }
 
 var PipelineIndexes = []repos.IndexField{
