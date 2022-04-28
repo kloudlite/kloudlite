b0812efdc0a6fbbef1dd90bf923fe2e9e466d414
Karthik Thirumalasetti <>
2022-04-29 04:48:33 +0530

    producer and consumer fx extracted
    Former-commit-id: 3d0d04bf26d82d3bf5cfdc3a1902526145356e40

--- api/apps/console/internal/app/workload-messenger.go
+++ api/apps/console/internal/app/workload-messenger.go
@@ -1,18 +1,26 @@
 package app
 
 import (
+	"kloudlite.io/apps/console/internal/domain"
 	"kloudlite.io/pkg/messaging"
 )
 
 type workloadMessengerImpl struct {
-	env      *Env
+	topic    string
 	producer messaging.Producer[messaging.Json]
 }
 
 func (i *workloadMessengerImpl) SendAction(action string, resId string, res any) error {
-	err := i.producer.SendMessage(i.env.KafkaWorkloadTopic, resId, messaging.Json{
+	err := i.producer.SendMessage(i.topic, resId, messaging.Json{
 		"action":  action,
 		"payload": res,
 	})
 	return err
+}
+
+func fxWorkloadMessenger(env *WorkloadConsumerEnv, p messaging.Producer[messaging.Json]) domain.WorkloadMessenger {
+	return &workloadMessengerImpl{
+		topic:    env.ResponseTopic,
+		producer: p,
+	}
 }
