4df7999835f33f982fffa899e7b4abaa859441b8
Karthik Thirumalasetti <>
2022-04-30 15:57:05 +0530

    wip
    Former-commit-id: 726e6b065d10265fc8d535d55b03df60d4a28f8a

--- apps/auth/internal/app/grpc-server.go
+++ apps/auth/internal/app/grpc-server.go
@@ -1,1 +1,29 @@
 package app
+
+import (
+	"context"
+	"kloudlite.io/apps/auth/internal/domain"
+	"kloudlite.io/grpc-interfaces/kloudlite.io/rpc/auth"
+)
+
+type authGrpcServerImpl struct {
+	auth.UnimplementedAuthServer
+	d domain.Domain
+}
+
+func (a *authGrpcServerImpl) GetAccessToken(ctx context.Context, request *auth.GetAccessTokenRequest) (*auth.AccessTokenOut, error) {
+	token, err := a.d.GetAccessToken(ctx, request.Provider, request.UserId)
+	if err != nil {
+		return nil, err
+	}
+	return &auth.AccessTokenOut{
+		UserId:   string(token.UserId),
+		Email:    token.Email,
+		Provider: token.Provider,
+		OauthToken: &auth.OauthToken{
+			AccessToken:  token.Token.AccessToken,
+			RefreshToken: token.Token.RefreshToken,
+			Expiry:       token.Token.Expiry.UnixMilli(),
+		},
+	}, err
+}
