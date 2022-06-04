8103205723a78c1e5f2553b2cf0952ccb3693944
Karthik Thirumalasetti <karthik1729.alphadev@gmail.com>
2022-06-04 11:52:28 +0530

    wip
    Former-commit-id: 51e8f7a30adb2bdc0eeda35245807a6ea6e457a4

--- apps/infra/infra-scripts/do/init-scripts/init.sh
+++ apps/infra/infra-scripts/do/init-scripts/init.sh
@@ -3,12 +3,16 @@ CONFIG_DIR=.
 kubectl apply -f $CONFIG_DIR/wireguard
 kubectl apply -f $CONFIG_DIR/csi/crds.yaml
 kubectl apply -f $CONFIG_DIR/csi
-kubectl apply -f ./cert-manager/crd.yml
+kubectl apply -f $CONFIG_DIR/cert-manager/crd.yml
+kubectl apply -f $CONFIG_DIR/knative
 kubectl delete sc local-path
+
+
 
 sh $CONFIG_DIR/loki/init.sh upgrade
 sh $CONFIG_DIR/ingress/init.sh upgrade
 sh $CONFIG_DIR/cert-manager/install.sh upgrade
+sh $CONFIG_DIR/linkerd/install.sh upgrade
 
 
 helm uninstall traefik --namespace kube-system
---
+++ cmd/internal/cmd/apps.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// appsCmd represents the apps command
+var appsCmd = &cobra.Command{
+	Use:   "apps",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("apps called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(appsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// appsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// appsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
