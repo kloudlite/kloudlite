30c9c93e71bd4aa960774604648fba4457ba2d72
nxtcoder17 <nxtcoder17@gmail.com>
2022-06-13 16:15:44 +0530

    WIP,.......

--- operator/controllers/crds/router_controller.go
+++ operator/controllers/crds/router_controller.go
@@ -3,206 +3,159 @@ package crds
 import (
 	"context"
 	"fmt"
-	"k8s.io/apimachinery/pkg/api/meta"
-	crdsv1 "operators.kloudlite.io/apis/crds/v1"
-	"operators.kloudlite.io/lib"
-
-	"go.uber.org/zap"
 	networkingv1 "k8s.io/api/networking/v1"
 	apiErrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
+	"net/http"
+	crdsv1 "operators.kloudlite.io/apis/crds/v1"
+	"operators.kloudlite.io/lib"
+	"operators.kloudlite.io/lib/conditions"
+	"operators.kloudlite.io/lib/errors"
+	fn "operators.kloudlite.io/lib/functions"
+	rApi "operators.kloudlite.io/lib/operator"
+	"operators.kloudlite.io/lib/templates"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"operators.kloudlite.io/lib/errors"
-	"operators.kloudlite.io/lib/finalizers"
-	reconcileResult "operators.kloudlite.io/lib/reconcile-result"
 )
 
 // RouterReconciler reconciles a Router object
 type RouterReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
-	logger *zap.SugaredLogger
-	router *crdsv1.Router
 	lib.MessageSender
-	lt metav1.Time
-}
-
-func (r *RouterReconciler) notifyAndDie(ctx context.Context, err error) (ctrl.Result, error) {
-	r.buildConditions("", metav1.Condition{
-		Type:    "Ready",
-		Status:  "False",
-		Reason:  "ErrWhileReconcilation",
-		Message: err.Error(),
-	})
-	return r.notify(ctx)
-}
-
-func (r *RouterReconciler) notify(ctx context.Context) (ctrl.Result, error) {
-	err := r.SendMessage(r.router.LogRef(), lib.MessageReply{
-		Key:        r.router.LogRef(),
-		Conditions: r.router.Status.Conditions,
-		Status:     meta.IsStatusConditionTrue(r.router.Status.Conditions, "Ready"),
-	})
-	if err != nil {
-		return reconcileResult.FailedE(errors.NewEf(err, "could not send message into kafka"))
-	}
-
-	if err := r.Status().Update(ctx, r.router); err != nil {
-		return reconcileResult.FailedE(errors.NewEf(err, "could not update status for (%s)", r.router.LogRef()))
-	}
-	return reconcileResult.OK()
-}
-
-func (r *RouterReconciler) buildConditions(source string, conditions ...metav1.Condition) {
-	meta.SetStatusCondition(&r.router.Status.Conditions, metav1.Condition{
-		Type:               "Ready",
-		Status:             "False",
-		Reason:             "ChecksNotCompleted",
-		LastTransitionTime: r.lt,
-		Message:            "Not All Checks completed",
-	})
-	for _, c := range conditions {
-		if c.Reason == "" {
-			c.Reason = "NotSpecified"
-		}
-		if !c.LastTransitionTime.IsZero() {
-			if c.LastTransitionTime.Time.Sub(r.lt.Time).Seconds() > 0 {
-				r.lt = c.LastTransitionTime
-			}
-		}
-		if c.LastTransitionTime.IsZero() {
-			c.LastTransitionTime = r.lt
-		}
-		if source != "" {
-			c.Reason = fmt.Sprintf("%s:%s", source, c.Reason)
-			c.Type = fmt.Sprintf("%s%s", source, c.Type)
-		}
-		meta.SetStatusCondition(&r.router.Status.Conditions, c)
-	}
 }
 
 // +kubebuilder:rbac:groups=crds.kloudlite.io,resources=routers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=crds.kloudlite.io,resources=routers/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=crds.kloudlite.io,resources=routers/finalizers,verbs=update
 
-func (r *RouterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	r.logger = GetLogger(req.NamespacedName)
-	logger := r.logger.With("RECONCILE", "true")
+func (r *RouterReconciler) Reconcile(ctx context.Context, oReq ctrl.Request) (ctrl.Result, error) {
+	req := rApi.NewRequest(ctx, r.Client, oReq.NamespacedName, &crdsv1.Router{})
 
-	logger.Infof("Request received for: %v\n", req.String())
-
-	router := &crdsv1.Router{}
-	if err := r.Get(ctx, req.NamespacedName, router); err != nil {
-		if apiErrors.IsNotFound(err) {
-			return reconcileResult.OK()
-		}
-		return reconcileResult.Failed()
-	}
-	r.router = router
-
-	if router.GetDeletionTimestamp() != nil {
-		return r.finalizeRouter(ctx, router)
+	if req == nil {
+		return ctrl.Result{}, nil
 	}
 
-	var ingRules []networkingv1.IngressRule
-	for _, domain := range router.Spec.Domains {
-		rule := networkingv1.IngressRule{
-			Host: domain,
-			IngressRuleValue: networkingv1.IngressRuleValue{
-				HTTP: &networkingv1.HTTPIngressRuleValue{},
-			},
+	if req.Object.GetDeletionTimestamp() != nil {
+		if x := r.finalize(req); !x.ShouldProceed() {
+			return x.Result(), x.Err()
 		}
-
-		rule.IngressRuleValue.HTTP.Paths = []networkingv1.HTTPIngressPath{}
-		pathType := networkingv1.PathTypePrefix
-		for _, route := range router.Spec.Routes {
-			p := networkingv1.HTTPIngressPath{
-				Path:     route.Path,
-				PathType: &pathType,
-				Backend: networkingv1.IngressBackend{
-					Service: &networkingv1.IngressServiceBackend{
-						Name: route.App,
-						Port: networkingv1.ServiceBackendPort{
-							Number: int32(route.Port),
-						},
-					},
-				},
-			}
-			rule.HTTP.Paths = append(rule.HTTP.Paths, p)
-		}
-		ingRules = append(ingRules, rule)
 	}
 
-	ing := &networkingv1.Ingress{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: router.Namespace,
-			Name:      router.Name,
-		},
-	}
-	if _, err := controllerutil.CreateOrUpdate(ctx, r.Client, ing, func() error {
-		ing.ObjectMeta.Annotations = IngressAnnotations
-		ing.Spec.TLS = []networkingv1.IngressTLS{
-			{
-				Hosts:      router.Spec.Domains,
-				SecretName: fmt.Sprintf("%s-router-tls", router.Name),
-			},
-		}
-		ing.Spec.Rules = ingRules
-		if err := controllerutil.SetControllerReference(router, ing, r.Scheme); err != nil {
-			return errors.NewEf(err, "could not set controller reference")
-		}
-		return nil
-	}); err != nil {
-		return r.notifyAndDie(ctx, errors.NewEf(err, "could not apply ingress"))
+	req.Logger.Info("-------------------- NEW RECONCILATION------------------")
+
+	if x := req.EnsureLabels(); !x.ShouldProceed() {
+		return x.Result(), x.Err()
 	}
 
-	router.Status.IPs = []string{}
-	if len(ing.Status.LoadBalancer.Ingress) > 0 {
-		for _, item := range ing.Status.LoadBalancer.Ingress {
-			router.Status.IPs = append(router.Status.IPs, item.IP)
-		}
+	if x := r.reconcileStatus(req); !x.ShouldProceed() {
+		return x.Result(), x.Err()
 	}
-	r.buildConditions("", metav1.Condition{
-		Type:    "Ready",
-		Status:  metav1.ConditionTrue,
-		Reason:  "IngressIsLive",
-		Message: "Ingress resource has acquired IP",
-	})
-	logger.Info("Reconcile Completed ...")
-	return r.notify(ctx)
+
+	if x := r.reconcileOperations(req); !x.ShouldProceed() {
+		return x.Result(), x.Err()
+	}
+
+	return ctrl.Result{}, nil
 }
 
-func (r *RouterReconciler) finalizeRouter(ctx context.Context, router *crdsv1.Router) (ctrl.Result, error) {
-	logger := r.logger.With("FINALIZER", "true")
-	logger.Debug("finalizing ...")
+func (r *RouterReconciler) finalize(req *rApi.Request[*crdsv1.Router]) rApi.StepResult {
+	return req.Finalize()
+}
 
-	if controllerutil.ContainsFinalizer(router, finalizers.Router.String()) {
-		controllerutil.RemoveFinalizer(router, finalizers.Router.String())
-		if err := r.Update(ctx, router); err != nil {
-			return reconcileResult.FailedE(err)
+func (r *RouterReconciler) reconcileStatus(req *rApi.Request[*crdsv1.Router]) rApi.StepResult {
+	ctx := req.Context()
+	router := req.Object
+
+	isReady := false
+	var cs []metav1.Condition
+
+	_, err := rApi.Get(ctx, r.Client, fn.NN(router.Namespace, router.Name), &networkingv1.Ingress{})
+	if err != nil {
+		if !apiErrors.IsNotFound(err) {
+			return req.FailWithOpError(errors.NewEf(err, "failed to get ingress resource"))
 		}
-		return reconcileResult.Retry()
+		isReady = false
 	}
 
-	if controllerutil.ContainsFinalizer(router, finalizers.Foreground.String()) {
-		var tr networkingv1.Ingress
-		if err := r.Get(ctx, types.NamespacedName{Name: router.Name, Namespace: router.Namespace}, &tr); err != nil {
-			if apiErrors.IsNotFound(err) {
-				controllerutil.RemoveFinalizer(router, finalizers.Foreground.String())
-				if err := r.Update(ctx, router); err != nil {
-					return reconcileResult.FailedE(err)
-				}
-				return reconcileResult.Retry()
-			}
+	for idx, domain := range router.Spec.Domains {
+		httpReq, err := http.NewRequest(http.MethodHead, fmt.Sprintf("https://%s/", domain), nil)
+		if err != nil {
+			return req.FailWithStatusError(errors.NewEf(err, "could not create http request"))
+		}
+		httpResp, err := http.DefaultClient.Do(httpReq)
+		if err != nil || httpReq == nil || httpResp.StatusCode < 200 || httpResp.StatusCode > 300 {
+			isReady = false
+			cs = append(
+				cs,
+				conditions.New(
+					fmt.Sprintf("%d-HasValidSSL", idx),
+					false,
+					"SSLCheckFailed",
+					errors.NewEf(err, "while making http request to (url=%s)", domain).Error(),
+				),
+			)
 		}
 	}
 
-	return reconcileResult.OK()
+	newConditions, hasUpdated, err := conditions.Patch(router.Status.Conditions, cs)
+	if err != nil {
+		return req.FailWithOpError(errors.NewEf(err, "could not patch conditions"))
+	}
+
+	if !hasUpdated && isReady == router.Status.IsReady {
+		return req.Next()
+	}
+
+	router.Status.IsReady = isReady
+	router.Status.Conditions = newConditions
+	router.Status.OpsConditions = []metav1.Condition{}
+
+	if err := r.Status().Update(ctx, router); err != nil {
+		return req.FailWithStatusError(err)
+	}
+
+	return req.Done()
+}
+
+func (r *RouterReconciler) reconcileOperations(req *rApi.Request[*crdsv1.Router]) rApi.StepResult {
+	router := req.Object
+
+	ingressObj, err := templates.ParseObject(
+		templates.Ingress, map[string]any{
+			"object":         router,
+			"cluster-issuer": "contour-cert-issuer",
+			"ingress-class":  "contour",
+			"cluster-domain": "svc.cluster.local",
+		},
+	)
+	if err != nil {
+		return req.FailWithOpError(errors.NewEf(err, "could not parse (template=%s) into Object", templates.Ingress))
+	}
+
+	if err := fn.KubectlApply(req.Context(), r.Client, ingressObj); err != nil {
+		return req.FailWithOpError(errors.NewEf(err, "could not apply ingress ingressObj"))
+	}
+
+	return req.Done()
+}
+
+func (r *RouterReconciler) notify(req *rApi.Request[*crdsv1.Router]) rApi.StepResult {
+	router := req.Object
+
+	err := r.SendMessage(
+		router.NameRef(), lib.MessageReply{
+			Key:        router.NameRef(),
+			Conditions: router.Status.Conditions,
+			Status:     meta.IsStatusConditionTrue(router.Status.Conditions, "Ready"),
+		},
+	)
+	if err != nil {
+		return req.FailWithOpError(errors.NewEf(err, "could not send message into kafka"))
+	}
+	return req.Done()
 }
 
 // SetupWithManager sets up the controller with the Manager.
