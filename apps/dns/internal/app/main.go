4432b15be934ca43f76fb51c4561b58d9f1f82dc
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-06-23 20:25:14 +0530

    :construction: wip
    Former-commit-id: af0499c4e19b10a9081d21dee041304aa35147df

--- apps/dns/internal/app/main.go
+++ apps/dns/internal/app/main.go
@@ -3,21 +3,18 @@ package app
 import (
 	"context"
 	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/miekg/dns"
 	"go.uber.org/fx"
 	"kloudlite.io/apps/dns/internal/app/graph"
 	"kloudlite.io/apps/dns/internal/app/graph/generated"
 	"kloudlite.io/apps/dns/internal/domain"
-
-	// "kloudlite.io/common"
-	// "kloudlite.io/pkg/cache"
-	"net"
-
+	"kloudlite.io/common"
+	"kloudlite.io/pkg/cache"
 	"kloudlite.io/pkg/config"
 	httpServer "kloudlite.io/pkg/http-server"
 	"kloudlite.io/pkg/repos"
+	"net"
 )
 
 type Env struct {
@@ -25,7 +22,8 @@ type Env struct {
 }
 
 type DNSHandler struct {
-	domain domain.Domain
+	domain      domain.Domain
+	recordCache cache.Repo[[]*domain.Record]
 }
 
 func (h *DNSHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
@@ -36,33 +34,31 @@ func (h *DNSHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 		switch q.Qtype {
 		case dns.TypeA:
 			msg.Authoritative = true
-			domain := q.Name
+			d := q.Name
 			todo := context.TODO()
-			records, err := h.domain.GetRecords(todo, domain[:len(domain)-1])
-			if err != nil {
-				fmt.Println("ERROR:", err)
-				return
+			host := d[:len(d)-1]
+			var records []*domain.Record
+			if matchedRecords, err := h.recordCache.Get(context.TODO(), host); err == nil && matchedRecords != nil {
+				records = matchedRecords
+			} else {
+				records, err = h.domain.GetRecords(todo, host)
+				if err != nil {
+					fmt.Println("ERROR:", err)
+					return
+				}
+				err := h.recordCache.Set(context.TODO(), host, records)
+				if err != nil {
+					fmt.Println("ERROR:", err)
+				}
 			}
-
-			matched := false
-
 			for _, r := range records {
 				if r.Type == "A" {
 					msg.Answer[i] = &dns.A{
-						Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: r.TTL},
+						Hdr: dns.RR_Header{Name: d, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: r.TTL},
 						A:   net.ParseIP(r.Answer),
 					}
-					matched = true
-					// fmt.Println("Answer:", msg.Answer[i])
 				}
 			}
-
-			if !matched {
-				msg.Answer[i] = &dns.A{
-					Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 300},
-				}
-			}
-
 		}
 	}
 
@@ -75,12 +71,14 @@ var Module = fx.Module(
 	repos.NewFxMongoRepo[*domain.Record]("records", "rec", domain.RecordIndexes),
 	repos.NewFxMongoRepo[*domain.Site]("sites", "site", domain.SiteIndexes),
 	repos.NewFxMongoRepo[*domain.Verification]("site_verifications", "svrf", domain.VerificationIndexes),
+	cache.NewFxRepo[[]*domain.Record](),
 	domain.Module,
-	fx.Invoke(func(lifecycle fx.Lifecycle, s *dns.Server, d domain.Domain) {
+	fx.Invoke(func(lifecycle fx.Lifecycle, s *dns.Server, d domain.Domain, recCache cache.Repo[[]*domain.Record]) {
 		lifecycle.Append(fx.Hook{
 			OnStart: func(ctx context.Context) error {
 				s.Handler = &DNSHandler{
-					d,
+					domain:      d,
+					recordCache: recCache,
 				}
 				return nil
 			},
@@ -89,54 +87,21 @@ var Module = fx.Module(
 	fx.Invoke(func(
 		server *fiber.App,
 		d domain.Domain,
-		// env *Env,
-		// cacheClient cache.Client,
+		env *Env,
+		cacheClient cache.Client,
 	) {
 		schema := generated.NewExecutableSchema(
 			generated.Config{Resolvers: graph.NewResolver(d)},
 		)
-
-		server.Post("/upsert-domain", func(c *fiber.Ctx) error {
-			var data struct {
-				Domain   string
-				ARecords []string
-			}
-			err := c.JSONP(&data)
-			if err != nil {
-				return err
-			}
-			err = d.AddARecords(c.Context(), data.Domain, data.ARecords, "kloudlite")
-
-			if err != nil {
-				return err
-			}
-
-			c.Send([]byte("done"))
-			return nil
-
-		})
-
-		server.Delete("/delete-domain/:domain_name", func(c *fiber.Ctx) error {
-			domainName := c.Params("domain_name")
-			err := d.DeleteRecords(c.Context(), domainName, "kloudlite")
-
-			if err != nil {
-				return err
-			}
-
-			c.Send([]byte("done"))
-			return nil
-		})
-
 		httpServer.SetupGQLServer(
 			server,
 			schema,
-			// httpServer.NewSessionMiddleware[*common.AuthSession](
-			// 	cacheClient,
-			// 	common.CookieName,
-			// 	env.CookieDomain,
-			// 	common.CacheSessionPrefix,
-			// ),
+			httpServer.NewSessionMiddleware[*common.AuthSession](
+				cacheClient,
+				common.CookieName,
+				env.CookieDomain,
+				common.CacheSessionPrefix,
+			),
 		)
 	}),
 )
