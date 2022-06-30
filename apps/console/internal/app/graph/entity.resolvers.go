04810d8e02524ef06d049c01cb7efd76719c5f43
Karthik Thirumalasetti <karthik1729.alphadev@gmail.com>
2022-06-30 13:19:23 +0530

    wip
    Former-commit-id: 6a9abd0a4870a9378b75660b135dfe977497d4ea

--- apps/console/internal/app/graph/entity.resolvers.go
+++ apps/console/internal/app/graph/entity.resolvers.go
@@ -19,26 +19,6 @@ func (r *entityResolver) FindClusterByID(ctx context.Context, id repos.ID) (*mod
 	return r.Query().InfraGetCluster(ctx, id)
 }
 
-func (r *entityResolver) FindComputeInventoryItemByName(ctx context.Context, name string) (*model.ComputeInventoryItem, error) {
-	plan, err := r.Domain.GetComputePlan(ctx, name)
-	if err != nil {
-		return nil, err
-	}
-	return &model.ComputeInventoryItem{
-		Name: name,
-		Data: &model.ComputeInventoryData{
-			Memory: &model.ComputeInventoryMetricSize{
-				Quantity: plan.Memory.Quantity,
-				Unit:     plan.Memory.Unit,
-			},
-			CPU: &model.ComputeInventoryMetricSize{
-				Quantity: plan.Cpu.Quantity,
-				Unit:     plan.Cpu.Unit,
-			},
-		},
-	}, nil
-}
-
 func (r *entityResolver) FindDeviceByID(ctx context.Context, id repos.ID) (*model.Device, error) {
 	device, err := r.Domain.GetDevice(ctx, id)
 	return &model.Device{
@@ -58,3 +38,29 @@ func (r *entityResolver) FindUserByID(ctx context.Context, id repos.ID) (*model.
 func (r *Resolver) Entity() generated.EntityResolver { return &entityResolver{r} }
 
 type entityResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+func (r *entityResolver) FindComputeInventoryItemByName(ctx context.Context, name string) (*model.ComputeInventoryItem, error) {
+	plan, err := r.Domain.GetComputePlan(ctx, name)
+	if err != nil {
+		return nil, err
+	}
+	return &model.ComputeInventoryItem{
+		Name: name,
+		Data: &model.ComputeInventoryData{
+			Memory: &model.ComputeInventoryMetricSize{
+				Quantity: plan.Memory.Quantity,
+				Unit:     plan.Memory.Unit,
+			},
+			CPU: &model.ComputeInventoryMetricSize{
+				Quantity: plan.Cpu.Quantity,
+				Unit:     plan.Cpu.Unit,
+			},
+		},
+	}, nil
+}
