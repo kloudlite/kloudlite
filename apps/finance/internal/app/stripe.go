3e0711b3455ec8935924b45a580b9512cf737fb4
nxtcoder17 <nxtcoder17@gmail.com>
2022-06-30 15:22:23 +0530

    :sparkles: stripe integrated
    stripe works
    
    
    Former-commit-id: 632571a0e88386711c16e9c02cd6d358036c37d9

--- apps/finance/internal/app/stripe.go
+++ apps/finance/internal/app/stripe.go
@@ -1,1 +1,27 @@
 package app
+
+import (
+	"kloudlite.io/apps/finance/internal/domain"
+	"kloudlite.io/pkg/stripe"
+)
+
+type pStripe struct {
+	stripeCli *stripe.Client
+}
+
+func (p pStripe) MakePayment(customerId stripe.CustomerId, paymentMethodId string, amount float64) (*stripe.Payment,
+	error) {
+	return p.stripeCli.NewPayment(customerId, paymentMethodId, amount)
+}
+
+func (p pStripe) CreateCustomer(accountId string, paymentMethodId string) (*stripe.CustomerId, error) {
+	return p.stripeCli.NewCustomer(accountId, paymentMethodId)
+}
+
+func (p pStripe) GetSetupIntent() (string, error) {
+	return p.stripeCli.NewSetupIntent()
+}
+
+func NewStripeClient(env *Env) domain.Stripe {
+	return &pStripe{stripeCli: stripe.NewClient(env.StripeSecretKey)}
+}
