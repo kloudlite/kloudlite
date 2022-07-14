fc54ab26ce4ae2607dee308b1fafa1e23d6984ad
Karthik Thirumalasetti <karthik1729.alphadev@gmail.com>
2022-07-14 13:50:14 +0530

    wip
    Former-commit-id: a0546547f01e1df63599d59f328a548fa08a8031

--- apps/console/internal/app/grpc-server.go
+++ apps/console/internal/app/grpc-server.go
@@ -3,7 +3,6 @@ package app
 import (
 	"context"
 	"kloudlite.io/apps/console/internal/domain"
-	"kloudlite.io/apps/console/internal/domain/entities"
 	"kloudlite.io/grpc-interfaces/kloudlite.io/rpc/console"
 	"kloudlite.io/pkg/repos"
 )
@@ -19,18 +18,6 @@ func (c *consoleServerI) GetProjectName(ctx context.Context, in *console.Project
 		return nil, err
 	}
 	return &console.ProjectOut{Name: project.Name}, nil
-}
-
-func (c *consoleServerI) SetupClusterForAccount(ctx context.Context, in *console.AccountIn) (*console.SetupClusterVoid, error) {
-	_, err := c.d.CreateClusterAccount(
-		ctx, &entities.ClusterAccount{
-			AccountID: repos.ID(in.AccountId),
-		}, in.Region, in.Provider,
-	)
-	if err != nil {
-		return nil, err
-	}
-	return &console.SetupClusterVoid{}, nil
 }
 
 func fxConsoleGrpcServer(d domain.Domain) console.ConsoleServer {
