f5b34a5b8e87b434526a216829b3a7021b4b1b50
nxtcoder17 <nxtcoder17@gmail.com>
2022-07-14 09:54:55 +0530

    tried tekton event listener, but figured out event listener is just super slow
    Former-commit-id: c0d624a2b7050b2f05113d0b4b7054f55d50046e

--- .tools/pipeline/test.sh
+++ .tools/pipeline/test.sh
@@ -1,1 +1,1 @@
-curl -H 'Content-Type: application/json' -XPOST 'https://tk.webhooks.dev.kloudlite.io/github?pipelineId=pip-lidjuq9-z0ddg9pzsawlk-s6s4ei' --data @test.json
+curl -H 'Content-Type: application/json' -XPOST 'https://tk.webhooks.dev.kloudlite.io/github?pipelineId=pip-9cnxjhxkqat9abb0wc2enxyf6bzj' --data @test.json
