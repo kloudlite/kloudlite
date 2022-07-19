95fa56f7f1df63cdf4032a62be7b2353700fc630
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-07-19 13:00:51 +0530

    :art: improved
    Former-commit-id: 213ec6179db6f838505aa2a2643079414e243763

--- apps/validate-price/main.go
+++ apps/validate-price/main.go
@@ -7,130 +7,12 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"gopkg.in/yaml.v2"
 	v8 "rogchap.com/v8go"
 )
 
-func getCompute() (string, error) {
+func ValidatePrice(functionString, inputString string) (string, error) {
 
-	configPath := os.Getenv("PRICING_PATH")
-	if configPath == "" {
-		return "", errors.New("CAN'T FIND CONFIG")
-	}
-
-	computeObj := []map[string]any{}
-
-	computeByte, err := os.ReadFile(configPath + "compute.yaml")
-	if err != nil {
-		return "", err
-	}
-
-	err = yaml.Unmarshal(computeByte, &computeObj)
-	if err != nil {
-		return "", err
-	}
-
-	storageObj := []map[string]any{}
-
-	storageByte, err := os.ReadFile(configPath + "storage.yaml")
-	if err != nil {
-		return "", err
-	}
-
-	err = yaml.Unmarshal(storageByte, &storageObj)
-	if err != nil {
-		return "", err
-	}
-
-	lambdaObj := []map[string]any{}
-	lambdaByte, err := os.ReadFile(configPath + "lambda.yaml")
-	if err != nil {
-		return "", err
-	}
-
-	err = yaml.Unmarshal(lambdaByte, &lambdaObj)
-	if err != nil {
-		return "", err
-	}
-
-	ciObj := []map[string]any{}
-
-	ciByte, err := os.ReadFile(configPath + "ci.yaml")
-	if err != nil {
-		return "", err
-	}
-
-	err = yaml.Unmarshal(ciByte, &ciObj)
-	if err != nil {
-		return "", err
-	}
-
-	b := map[string]map[string]any{}
-
-	for _, i := range ciObj {
-		name := i["name"]
-		b[name.(string)] = i
-	}
-
-	ci, err := json.Marshal(b)
-	if err != nil {
-		return "", err
-	}
-
-	b = map[string]map[string]any{}
-
-	for _, i := range computeObj {
-		name := i["name"]
-		b[name.(string)] = i
-	}
-
-	compute, err := json.Marshal(b)
-
-	if err != nil {
-		return "", err
-	}
-
-	b = map[string]map[string]any{}
-
-	for _, i := range storageObj {
-		name := i["name"]
-		b[name.(string)] = i
-	}
-
-	storage, err := json.Marshal(b)
-
-	if err != nil {
-		return "", err
-	}
-
-	b = map[string]map[string]any{}
-
-	for _, i := range lambdaObj {
-		name := i["name"]
-		b[name.(string)] = i
-	}
-
-	lambda, err := json.Marshal(b)
-
-	if err != nil {
-		return "", err
-	}
-
-	return `{
-        compute: ` + string(compute) + `,
-        storage: ` + string(storage) + `,
-        lambda: ` + string(lambda) + `,
-        ci: ` + string(ci) + `,
-      }`, nil
-
-}
-
-func ValidatePrice(functionString, inputString, priceDetails string) (string, error) {
-
-	f := `(` + functionString + `)
-      (` + inputString + `,
-      ` + priceDetails + `
-      )`
+	f := `(` + functionString + `)(` + inputString + `)`
 
 	ctx := v8.NewContext()                  // creates a new V8 context with a new Isolate aka VM
 	val, err := ctx.RunScript(f, "math.js") // executes a script on the global context
@@ -153,15 +35,8 @@ func main() {
 	app := fiber.New()
 	runPort := os.Getenv("PORT")
 
-	priceDetails, configParseError := getCompute()
-	if configParseError != nil {
-		panic(configParseError)
-	}
-
 	app.Get("/validate-price", func(c *fiber.Ctx) error {
-		if configParseError != nil {
-			return configParseError
-		}
+
 		var data struct {
 			FunctionString string
 			InputString    string
@@ -174,7 +49,7 @@ func main() {
 			return errors.New("You must provide: {functionstring,inputstring}")
 		}
 
-		out, err := ValidatePrice(data.FunctionString, data.InputString, priceDetails)
+		out, err := ValidatePrice(data.FunctionString, data.InputString)
 
 		if err != nil {
 			fmt.Println(err)
