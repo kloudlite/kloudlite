d778999018332b46c3f3f885fb9fefdb8595f723
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-07-25 10:39:03 +0530

    :construction: wip
    Former-commit-id: 4171d774c0cb50c7f701c0136c59b7e519b7e1af

--- apps/proxy/main.go
+++ apps/proxy/main.go
@@ -5,12 +5,62 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net"
 	"os"
 	"sync"
+
+	"github.com/fsnotify/fsnotify"
 )
 
+func WaitForFileChange(pathAddr string) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal("NewWatcher failed: ", err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		defer close(done)
+
+		for {
+			select {
+			case _, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				// log.Printf("%s %s\n", event.Name, event.Op)
+				return
+			case _, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+
+	}()
+
+	err = watcher.Add(pathAddr)
+	if err != nil {
+		log.Fatal("Add failed:", err)
+	}
+	<-done
+}
+
+func remove(s []net.Listener, i int) []net.Listener {
+	if len(s)-1 < i {
+		return s
+	}
+	// fmt.Println(len(s))
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
 func main() {
+
+	confFile := os.Getenv("CONFIG_FILE")
 
 	type Service struct {
 		Name   string `json:"name"`
@@ -22,57 +72,93 @@ func main() {
 		Services []Service `json:"services"`
 	}
 
-	confFile := os.Getenv("CONFIG_FILE")
-	configData, err := ioutil.ReadFile(confFile)
-	if err != nil {
-		fmt.Println("Error reading config file:", confFile, err)
-	}
+	listeners := []net.Listener{}
 
-	err = json.Unmarshal(configData, &data)
-	if err != nil {
-		fmt.Println("Error unmarshalling config file:", err)
-	}
+	for {
 
-	for _, service := range data.Services {
-		go func(service Service) {
+		configData, err := ioutil.ReadFile(confFile)
+		if err != nil {
+			fmt.Println("Error reading config file:", confFile, err)
+		}
 
-			fmt.Println(fmt.Sprint(service.Name, ":", service.Target, "->", service.Port))
+		err = json.Unmarshal(configData, &data)
+		if err != nil {
+			fmt.Println("Error unmarshalling config file:", err)
+		}
 
-			listener, err := net.Listen("tcp", fmt.Sprintf(":%d", service.Port))
-			if err != nil {
-				fmt.Println("Error listening:", err)
-				return
-			}
-			defer listener.Close()
+		for len(listeners) > 0 {
+			i := len(listeners) - 1
+			listener := listeners[i]
+			fmt.Println("closing: ", listener.Addr().String())
+			listener.Close()
+			listeners = remove(listeners, i)
+		}
 
-			for {
-				conn, err := listener.Accept()
+		// for i, listener := range listeners {
+		// }
+
+		// for _, conn := range connections {
+		// 	if conn != nil {
+		// 		fmt.Println("closing: ", conn.RemoteAddr().String())
+		// 		conn.Close()
+
+		// 	}
+		// }
+
+		// listeners = []net.Listener{}
+		// connections = []net.Conn{}
+
+		for _, service := range data.Services {
+			go func(service Service) {
+
+				fmt.Println(fmt.Sprint(service.Name, ":", service.Target, "->", service.Port))
+
+				listener, err := net.Listen("tcp", fmt.Sprintf(":%d", service.Port))
 				if err != nil {
-					fmt.Println("Error accepting connection: ", err)
-					continue
+					fmt.Println("Error listening:", err)
+					return
 				}
-				upconn, err := net.Dial("tcp", fmt.Sprint(service.Name, ":", service.Target))
-				if err != nil {
-					fmt.Println("Error dialing target: ", err)
-					conn.Close()
-					continue
+
+				defer listener.Close()
+
+				listeners = append(listeners, listener)
+
+				for {
+					conn, err := listener.Accept()
+					if err != nil {
+						listener.Close()
+						// fmt.Println("Error accepting connection: ", err)
+						break
+					}
+					upconn, err := net.Dial("tcp", fmt.Sprint(service.Name, ":", service.Target))
+					if err != nil {
+						fmt.Println("Error dialing target: ", err)
+						conn.Close()
+						continue
+					}
+					go func() {
+						io.Copy(conn, upconn)
+						conn.Close()
+						upconn.Close()
+					}()
+					go func() {
+						io.Copy(upconn, conn)
+						upconn.Close()
+						conn.Close()
+					}()
 				}
-				go func() {
-					io.Copy(conn, upconn)
-					conn.Close()
-					upconn.Close()
-				}()
-				go func() {
-					io.Copy(upconn, conn)
-					upconn.Close()
-					conn.Close()
-				}()
-			}
-		}(service)
+			}(service)
+		}
+
+		WaitForFileChange(confFile)
+		// time.Sleep(time.Second)
+
 	}
+
 	fmt.Println("running")
 
 	var wg sync.WaitGroup
 	wg.Add(1)
 	wg.Wait()
+
 }
