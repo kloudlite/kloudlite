2580308ca7257f9fe22e24eace16143a5aaa3aeb
Karthik Thirumalasetti <karthik1729.alphadev@gmail.com>
2022-08-15 15:43:56 +0530

    wip
    Former-commit-id: 9d7b1525040305b239676a6ffa873a5dc8da55a7

--- apps/finance/internal/app/grpc-client.go
+++ apps/finance/internal/app/grpc-client.go
@@ -4,13 +4,11 @@ import (
 	"go.uber.org/fx"
 	"google.golang.org/grpc"
 	"kloudlite.io/grpc-interfaces/kloudlite.io/rpc/auth"
-	"kloudlite.io/grpc-interfaces/kloudlite.io/rpc/ci"
 	"kloudlite.io/grpc-interfaces/kloudlite.io/rpc/comms"
 	"kloudlite.io/grpc-interfaces/kloudlite.io/rpc/console"
 	"kloudlite.io/grpc-interfaces/kloudlite.io/rpc/iam"
 )
 
-type CIGrpcClientConn *grpc.ClientConn
 type AuthGrpcClientConn *grpc.ClientConn
 type ConsoleClientConnection *grpc.ClientConn
 type CommsClientConnection *grpc.ClientConn
@@ -27,12 +25,6 @@ var IAMClientFx = fx.Provide(func(conn IAMClientConnection) iam.IAMClient {
 var CommsClientFx = fx.Provide(func(conn CommsClientConnection) comms.CommsClient {
 	return comms.NewCommsClient((*grpc.ClientConn)(conn))
 })
-
-var CiClientFx = fx.Provide(
-	func(conn CIGrpcClientConn) ci.CIClient {
-		return ci.NewCIClient((*grpc.ClientConn)(conn))
-	},
-)
 
 var AuthClientFx = fx.Provide(
 	func(conn AuthGrpcClientConn) auth.AuthClient {
