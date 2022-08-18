384eb78652360ecad2bba330ebf0ee0d85aca468
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-08-18 13:39:35 +0530

    :construction: wip
    Former-commit-id: 569e74b219bd66a238b320a2a721ee843942ad53

--- cmd/internal/common/main.go
+++ cmd/internal/common/main.go
@@ -3,8 +3,20 @@ package common
 import (
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/briandowns/spinner"
 )
 
 func PrintError(err error) {
 	fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+}
+
+func NewSpinner() *spinner.Spinner {
+	message := []string{}
+	sp := spinner.CharSets[11]
+	for _, v := range sp {
+		message = append(message, fmt.Sprintf("%s loading please wait...", v))
+	}
+	return spinner.New(message, 100*time.Millisecond)
 }
