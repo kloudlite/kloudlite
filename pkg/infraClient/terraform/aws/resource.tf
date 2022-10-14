faf80d6c46aed0fd020662b31c8a88986a46dc62
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-10-14 12:37:12 +0530

    :construction: wip
    Former-commit-id: d04ee82e34415978b3c7ac313aadcbb850236930

--- pkg/infraClient/terraform/aws/resource.tf
+++ pkg/infraClient/terraform/aws/resource.tf
@@ -19,6 +19,20 @@ resource "aws_security_group" "sg" {
   # name = "${var.node_id}-sg"
 
   ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    protocol = "tcp"
+    to_port = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port = 30000
     protocol = "tcp"
     to_port = 32768
