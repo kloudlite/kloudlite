0deba52aa3156530f0836fc7ee14f86ee43d0952
nxtcoder17 <nxtcoder17@gmail.com>
2022-11-25 08:32:23 +0530

    WIP ...

--- operator/operators/routers/internal/controllers/lib.go
+++ operator/operators/routers/internal/controllers/lib.go
@@ -1,23 +1,23 @@
 package controllers
 
 import (
-	"fmt"
+  "fmt"
 )
 
 func GetClusterIssuerName(region string) string {
-	if region == "" {
-		return "kl-cert-issuer"
-	}
-	return fmt.Sprintf("kl-cert-issuer-%s", region)
+  if region == "" || region == "master" {
+    return "kl-cert-issuer"
+  }
+  return fmt.Sprintf("kl-cert-issuer-%s", region)
 }
 
 func GetCertIssuerSecret(region string) string {
-	return GetClusterIssuerName(region) + "-tls"
+  return GetClusterIssuerName(region) + "-tls"
 }
 
 func GetIngressClassName(region string) string {
-	if region == "" {
-		return "ingress-nginx"
-	}
-	return fmt.Sprintf("ingress-nginx-%s", region)
+  if region == "" {
+    return "ingress-nginx"
+  }
+  return fmt.Sprintf("ingress-nginx-%s", region)
 }
