9573f515c9fde5232828fb5a503ee4f1146cb34e
nxtcoder17 <nxtcoder17@gmail.com>
2022-11-25 10:09:14 +0530

    new accounts created would now be having current cluster to be associated with
    Former-commit-id: 7cd99a6e63649f4bae074a4eb5f188ce2c5432de

--- api/apps/finance/main.go
+++ api/apps/finance/main.go
@@ -4,18 +4,34 @@ import (
 	"flag"
 
 	"go.uber.org/fx"
+	"k8s.io/client-go/rest"
 	"kloudlite.io/apps/finance/internal/framework"
+	"kloudlite.io/pkg/k8s"
 	"kloudlite.io/pkg/logging"
 )
 
 func main() {
-	isDev := flag.Bool("dev", false, "--dev")
+	var isDev bool
+	flag.BoolVar(&isDev, "dev", false, "--dev")
 	flag.Parse()
+
 	fx.New(
+		fx.Provide(
+			func() (*k8s.YAMLClient, error) {
+				if isDev {
+					return k8s.NewYAMLClient(&rest.Config{Host: "localhost:8080"})
+				}
+				inclusterCfg, err := rest.InClusterConfig()
+				if err != nil {
+					return nil, err
+				}
+				return k8s.NewYAMLClient(inclusterCfg)
+			},
+		),
 		framework.Module,
 		fx.Provide(
 			func() (logging.Logger, error) {
-				return logging.New(&logging.Options{Name: "finance", Dev: *isDev})
+				return logging.New(&logging.Options{Name: "finance", Dev: isDev})
 			},
 		),
 	).Run()
