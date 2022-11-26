d8eaaa85edf1981ac42e85ce80850e491753b6b1
Anshuman Bhaskar <nxtcoder17@gmail.com>
2022-11-26 12:44:00 +0530

    mongo managed resources are now getting ready, fixed bug in userExists check (#149)

--- operators/msvc-mongo/main.go
+++ operators/msvc-mongo/main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-	mongodbMsvcv1 "operators.kloudlite.io/apis/mongodb.msvc/v1"
-	"operators.kloudlite.io/operator"
-	"operators.kloudlite.io/operators/msvc-mongo/internal/controllers/database"
-	standalone_service "operators.kloudlite.io/operators/msvc-mongo/internal/controllers/standalone-service"
-	"operators.kloudlite.io/operators/msvc-mongo/internal/env"
+  mongodbMsvcv1 "operators.kloudlite.io/apis/mongodb.msvc/v1"
+  "operators.kloudlite.io/operator"
+  "operators.kloudlite.io/operators/msvc-mongo/internal/controllers/database"
+  "operators.kloudlite.io/operators/msvc-mongo/internal/env"
 )
 
 func main() {
-	ev := env.GetEnvOrDie()
-	mgr := operator.New("mongodb")
-	mgr.AddToSchemes(
-		mongodbMsvcv1.AddToScheme,
-	)
-	mgr.RegisterControllers(
-		&standalone_service.ServiceReconciler{Name: "mongodb-standalone-svc", Env: ev},
-		&database.Reconciler{Name: "mongodb-database", Env: ev},
-	)
-	mgr.Start()
+  ev := env.GetEnvOrDie()
+  mgr := operator.New("mongodb")
+  mgr.AddToSchemes(
+    mongodbMsvcv1.AddToScheme,
+  )
+  mgr.RegisterControllers(
+    // &standalone_service.ServiceReconciler{Name: "mongodb-standalone-svc", Env: ev},
+    &database.Reconciler{Name: "mongodb-database", Env: ev},
+  )
+  mgr.Start()
 }
