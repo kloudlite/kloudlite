941680c733cf505cdc2bf23d4e31973b39343d30
nxtcoder17 <nxtcoder17@gmail.com>
2023-02-01 18:15:55 +0530

    can be made into single binary

--- operators/app-n-lambda/main.go
+++ operators/app-n-lambda/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"time"
+
 	crdsv1 "github.com/kloudlite/operator/apis/crds/v1"
 	"github.com/kloudlite/operator/operator"
 	"github.com/kloudlite/operator/operators/app-n-lambda/internal/controllers/app"
 	"github.com/kloudlite/operator/operators/app-n-lambda/internal/env"
 )
+
+func Register(mgr operator.Operator) {
+	mgr.AddToSchemes(crdsv1.AddToScheme)
+	mgr.RegisterControllers(
+		&app.Reconciler{Name: "app", Env: &env.Env{
+			ReconcilePeriod:         30 * time.Second,
+			MaxConcurrentReconciles: 1,
+		}},
+	)
+}
 
 func main() {
 	ev := env.GetEnvOrDie()
