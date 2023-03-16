c519f7c20247b7001604b68672033e75a0fd7b7a
nxtcoder17 <nxtcoder17@gmail.com>
2023-03-16 21:59:38 +0530

    [neovim] http-cli tests and dap configuration updates
    Former-commit-id: eb4995a902e607708c4df9b5752873f758e29778

--- api/.tools/nvim/dap/go.lua
+++ api/.tools/nvim/dap/go.lua
@@ -24,7 +24,19 @@ dap.configurations.go = {
     envFile = {
       vim.g.root_dir .. "/apps/console" .. "/.secrets/env",
     },
-    dlvToolPath =
-    "/usr/local/go/bin/dlv --headless=true --api-version=2 -r stdout:/tmp/debug.stdout -r stderr:/tmp/debug2.stderr",
+    -- dlvToolPath =
+    -- "/usr/local/go/bin/dlv --headless=true --api-version=2 -r stdout:/tmp/debug.stdout -r stderr:/tmp/debug2.stderr",
+  },
+  {
+    type = "go",
+    name = "Debug finance-api",
+    request = "launch",
+    program = vim.g.root_dir .. "/apps/finance",
+    args = { "--dev" },
+    console = "externalTerminal",
+    -- externalTerminal = true,
+    envFile = {
+      vim.g.root_dir .. "/apps/finance" .. "/.secrets/env",
+    },
   },
 }
