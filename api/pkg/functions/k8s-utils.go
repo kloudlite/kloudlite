2be5a7b49fdf9e32cef1cabed1709046f311dae6
nxtcoder17 <nxtcoder17@gmail.com>
2023-03-16 13:54:59 +0530

    [pkg/redpanda,pkg/functions] uncommented fx hook to ping kafka brokers, and utility to conver k8s client.Object to YAMLs
    Former-commit-id: 3f10d879198f43b92c59379831db379ee17b0c5c

--- api/pkg/functions/k8s-utils.go
+++ api/pkg/functions/k8s-utils.go
@@ -1,7 +1,15 @@
 package functions
 
-import "k8s.io/apimachinery/pkg/types"
+import (
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/yaml"
+)
 
 func NN(namespace, name string) types.NamespacedName {
 	return types.NamespacedName{Namespace: namespace, Name: name}
+}
+
+func K8sObjToYAML(obj client.Object) ([]byte, error) {
+	return yaml.Marshal(obj)
 }
