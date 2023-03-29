7ee0323ead9c651f372ea45ec89d18b01e9d3399
nxtcoder17 <nxtcoder17@gmail.com>
2023-03-29 14:26:42 +0530

    [apps/iam] WIP
    Former-commit-id: 37148b7144d6f5a4fc28250dc2fe5da3752a7743

--- apps/iam/internal/domain/entities/types.go
+++ apps/iam/internal/domain/entities/types.go
@@ -4,14 +4,14 @@ import "kloudlite.io/pkg/repos"
 
 type RoleBinding struct {
 	repos.BaseEntity `json:",inline" bson:",inline"`
-	UserId           string `json:"user_id" bson:"user_id"`
-	ResourceType     string `json:"resource_type" bson:"resource_type"`
-	ResourceId       string `json:"resource_id" bson:"resource_id"`
-	Role             string `json:"role" bson:"role"`
-	Accepted         bool   `json:"accepted" bson:"accepted"`
+	UserId           repos.ID `json:"user_id" bson:"user_id"`
+	ResourceType     string   `json:"resource_type" bson:"resource_type"`
+	ResourceId       string   `json:"resource_id" bson:"resource_id"`
+	Role             string   `json:"role" bson:"role"`
+	Accepted         bool     `json:"accepted" bson:"accepted"`
 }
 
-var RoleBindingIndexes = []string{"id", "user_id", "resource_id", "role"}
+// var RoleBindingIndexes = []string{"id", "user_id", "resource_id", "role"}
 
 var RoleBindingIndices = []repos.IndexField{
 	{
@@ -26,5 +26,10 @@ var RoleBindingIndices = []repos.IndexField{
 			{Key: "resource_id", Value: repos.IndexDesc},
 		},
 		Unique: true,
+	},
+	{
+		Field: []repos.IndexKey{
+			{Key: "role", Value: repos.IndexAsc},
+		},
 	},
 }
--- apps/iam/internal/framework/main.go
+++ apps/iam/internal/framework/main.go
@@ -3,39 +3,34 @@ package framework
 import (
 	"go.uber.org/fx"
 	"kloudlite.io/apps/iam/internal/application"
+	"kloudlite.io/apps/iam/internal/env"
 	"kloudlite.io/pkg/cache"
 	"kloudlite.io/pkg/config"
 	rpc "kloudlite.io/pkg/grpc"
 	"kloudlite.io/pkg/repos"
 )
 
-type Env struct {
-	Port          uint16 `env:"GRPC_PORT" required:"true"`
-	MongoDbUri    string `env:"MONGO_DB_URI" required:"true"`
-	MongoDbName   string `env:"MONGO_DB_NAME" required:"true"`
-	RedisHosts    string `env:"REDIS_HOSTS" required:"true"`
-	RedisUsername string `env:"REDIS_USERNAME" required:"true"`
-	RedisPassword string `env:"REDIS_PASSWORD" required:"true"`
-	RedisPrefix   string `env:"REDIS_PREFIX" required:"true"`
+type fm struct {
+	*env.Env
 }
 
-func (env *Env) RedisOptions() (hosts, username, password, basePrefix string) {
-	return env.RedisHosts, env.RedisUsername, env.RedisPassword, env.RedisPrefix
+func (f *fm) RedisOptions() (hosts, username, password, basePrefix string) {
+	return f.RedisHosts, f.RedisUsername, f.RedisPassword, f.RedisPrefix
 }
 
-func (env *Env) GetMongoConfig() (url, dbName string) {
-	return env.MongoDbUri, env.MongoDbName
+func (f *fm) GetMongoConfig() (url, dbName string) {
+	return f.MongoDbUri, f.MongoDbName
 }
 
-func (env *Env) GetGRPCPort() uint16 {
-	return env.Port
+func (f *fm) GetGRPCPort() uint16 {
+	return f.Port
 }
 
 var Module fx.Option = fx.Module(
 	"framework",
-	config.EnvFx[Env](),
-	repos.NewMongoClientFx[*Env](),
-	cache.NewRedisFx[*Env](),
-	rpc.NewGrpcServerFx[*Env](),
+	config.EnvFx[fm](),
+	repos.NewMongoClientFx[*fm](),
+	cache.NewRedisFx[*fm](),
+	rpc.NewGrpcServerFx[*fm](),
 	application.Module,
 )
