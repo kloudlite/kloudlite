f6b9433dd8ac42566d30808075d2951f9c5d6a08
nxtcoder17 <nxtcoder17@gmail.com>
2023-04-24 18:04:14 +0530

    [apps/message-office] refactorings, and added upx for binary compression
    Former-commit-id: a719c918aa7b426f5769bb182dfc803de529390b

--- apps/message-office/main.go
+++ apps/message-office/main.go
@@ -11,7 +11,6 @@ import (
 
 	env "kloudlite.io/apps/message-office/internal/env"
 	"kloudlite.io/apps/message-office/internal/framework"
-	fn "kloudlite.io/pkg/functions"
 )
 
 func main() {
@@ -31,11 +30,17 @@ func main() {
 				return logging.New(&logging.Options{Name: "message-office", Dev: isDev})
 			},
 		),
-		fn.FxErrorHandler(),
+		// fn.FxErrorHandler(),
 		framework.Module,
 	)
 
-	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancelFn := func() (context.Context, context.CancelFunc) {
+		if isDev {
+			return context.WithCancel(context.TODO())
+		}
+		return context.WithTimeout(context.TODO(), 5*time.Second)
+	}()
+
 	defer cancelFn()
 	if err := app.Start(ctx); err != nil {
 		panic(err)
@@ -53,31 +58,3 @@ func main() {
 
 	<-app.Done()
 }
-
-// func main() {
-// 	// ev := env.LoadEnvOrDie()
-// 	//
-// 	// logger := logging.NewOrDie(&logging.Options{
-// 	// 	Name: "message-office",
-// 	// 	Dev:  true,
-// 	// })
-//
-// 	// producer, err :=
-// 	// if err != nil {
-// 	// 	panic(err)
-// 	// }
-// 	// defer producer.Close()
-//
-// 	gServer := grpc.NewServer()
-// 	grpcImpl := grpcServer{}
-//
-// 	messages.RegisterMessageDispatchServiceServer(gServer, grpcImpl)
-// 	listener, err := net.Listen("tcp", ":50051")
-// 	if err != nil {
-// 		log.Fatalf("failed to listen: %v", err)
-// 	}
-// 	logger.Infof("[GRPC] server listening on addr: :%v", 50051)
-// 	if err := gServer.Serve(listener); err != nil {
-// 		log.Fatalf("failed to serve: %v", err)
-// 	}
-// }
