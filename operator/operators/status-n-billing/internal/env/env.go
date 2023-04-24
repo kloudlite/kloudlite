37a77c043f13bc0271f2cb6d78bc4dc9bd8223da
nxtcoder17 <nxtcoder17@gmail.com>
2023-04-24 11:16:29 +0530

    [CRs] and refactorings

--- operator/operators/status-n-billing/internal/env/env.go
+++ operator/operators/status-n-billing/internal/env/env.go
@@ -10,15 +10,6 @@ type Env struct {
 	ReconcilePeriod         time.Duration `env:"RECONCILE_PERIOD" required:"true"`
 	MaxConcurrentReconciles int           `env:"MAX_CONCURRENT_RECONCILES" required:"true"`
 
-	// KafkaBrokers      string `env:"KAFKA_BROKERS" required:"true"`
-	// KafkaSASLUsername string `env:"KAFKA_SASL_USERNAME" required:"true"`
-	// KafkaSASLPassword string `env:"KAFKA_SASL_PASSWORD" required:"true"`
-	//
-	// KafkaTopicInfraUpdates       string `env:"KAFKA_TOPIC_INFRA_UPDATES" required:"true"`
-	// KafkaTopicStatusUpdates      string `env:"KAFKA_TOPIC_STATUS_UPDATES" required:"true"`
-	// KafkaTopicBillingUpdates     string `env:"KAFKA_TOPIC_BILLING_UPDATES" required:"true"`
-	// KafkaTopicPipelineRunUpdates string `env:"KAFKA_TOPIC_PIPELINE_RUN_UPDATES" required:"true"`
-
 	GrpcAddr string `env:"GRPC_ADDR" required:"true"`
 
 	AccountName string `env:"ACCOUNT_NAME" required:"true"`
