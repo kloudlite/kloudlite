19b812bafe8d95ad45fba4a2032fda362ad2bf48
Karthik Thirumalasetti <karthik1729.alphadev@gmail.com>
2023-04-26 16:55:44 +0530

    ci testing

--- apps/auth/main.go
+++ apps/auth/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
-
 	"go.uber.org/fx"
 	"kloudlite.io/apps/auth/internal/framework"
 	"kloudlite.io/pkg/logging"
 )
 
 func main() {
-	fmt.Println("Hello, World!2")
 	isDev := flag.Bool("dev", false, "--dev")
 	flag.Parse()
 	fx.New(
