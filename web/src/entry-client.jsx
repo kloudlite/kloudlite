50a5f2360cce849bfd3fcfbe28a4e178c564048c
karthik1729 <karthik@kloudlite.io>
2023-05-02 00:24:30 +0530

    wip

--- web/src/entry-client.jsx
+++ web/src/entry-client.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import {App} from './App'
-import "./setup-theme.js"
+import {App} from './app'
 
 ReactDOM.hydrateRoot(
   document.getElementById('app'),
