361d6183026826dfcf5cfe48b1c4af10e291e3ac
karthik1729 <karthik@kloudlite.io>
2023-05-02 00:49:00 +0530

    wip

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup'
+import {basePath} from "./src/base-path.js";
 
 export default defineConfig({
+  base:basePath,
   plugins: [react(), mdx()],
   build: {
     minify: true,
