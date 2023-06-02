a07871a549c3ff89713948faf5e6a4b980caec96
Bikash Ojha <bikashojha@Karthiks-iMac.local>
2023-06-02 11:24:17 +0530

    Fixed switch multiple instance issue, added LayoutId for tab to fix multiple instance issue

--- web/src/components/atoms/switch.jsx
+++ web/src/components/atoms/switch.jsx
@@ -2,59 +2,62 @@ import { Switch as SwitchComp } from '@headlessui/react';
 import PropTypes from "prop-types";
 import { BounceIt } from "../bounce-it";
 import classNames from "classnames";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { motion } from "framer-motion"
 
-export const Switch = ({error,disabled,...props})=>{
+export const Switch = ({ error, disabled, ...props }) => {
   const [checked, setChecked] = useState(props.checked);
-  useEffect(()=>{
-    if(props.onChange) props.onChange(checked)
+  const layoutId = useMemo(() => Math.random().toString(36).substring(2), [])
+
+  useEffect(() => {
+    if (props.onChange) props.onChange(checked)
   }, [checked])
+
   return <BounceIt disable={props.disabled} className='cursor-pointer'>
-      <SwitchComp className={classNames(
-        "flex gap-1 items-center group", 
-        "focus:ring-2 ring-border-focus ring-offset-1",
-        "outline-none transition-all rounded-full",
-        )} {...props} checked={checked} onChange={setChecked} disabled={disabled}>
-        {({checked}) => (
-          <>
-            <div className={classNames(
-              'w-[48px] rounded-full border  flex items-center p-0.5  transition-all',
-              disabled?'bg-surface-default border-border-disabled':{
-                'bg-surface-primary-default border-border-primary': checked,
-                'bg-surface-default border-border-default': !checked,
+    <SwitchComp className={classNames(
+      "flex gap-1 items-center group",
+      "focus:ring-2 ring-border-focus ring-offset-1",
+      "outline-none transition-all rounded-full",
+    )} {...props} checked={checked} onChange={setChecked} disabled={disabled}>
+      {({ checked }) => (
+        <>
+          <div className={classNames(
+            'w-[48px] rounded-full border  flex items-center p-0.5  transition-all',
+            disabled ? 'bg-surface-default border-border-disabled' : {
+              'bg-surface-primary-default border-border-primary': checked,
+              'bg-surface-default border-border-default': !checked,
+            }
+          )}>
+            <div className='w-[21px] h-[21px]'>
+              {
+                checked ? null : (<motion.div layoutId={layoutId} className={
+                  classNames(
+                    'rounded-full w-full h-full',
+                    {
+                      'bg-icon-disabled': disabled,
+                      'bg-surface-primary-default': !disabled,
+                    }
+                  )
+                } />)
               }
-            )}>
-              <div className='w-[21px] h-[21px]'>
-                {
-                  checked? null : (<motion.div layoutId="knob" className={
-                    classNames(
-                      'rounded-full w-full h-full',
-                      {
-                        'bg-icon-disabled': disabled,
-                        'bg-surface-primary-default': !disabled,
-                      }
-                    )
-                  } />)
-                }
-                
-              </div>
-              <div className='w-[21px] h-[21px]'>
-                {
-                  checked? (<motion.div layoutId="knob" className={
-                    classNames(
-                      ' rounded-full  w-full h-full',
-                      {
-                        'bg-icon-disabled': disabled,
-                        'bg-surface-default': !disabled,
-                      }
-                    )
-                  } />):null
-                }
-              </div>
+
             </div>
-          </>
-        )}
+            <div className='w-[21px] h-[21px]'>
+              {
+                checked ? (<motion.div layoutId={layoutId} className={
+                  classNames(
+                    ' rounded-full  w-full h-full',
+                    {
+                      'bg-icon-disabled': disabled,
+                      'bg-surface-default': !disabled,
+                    }
+                  )
+                } />) : null
+              }
+            </div>
+          </div>
+        </>
+      )}
     </SwitchComp>
   </BounceIt>
 }
@@ -64,13 +67,11 @@ Switch.propTypes = {
   onChange: PropTypes.func,
   disabled: PropTypes.bool,
   error: PropTypes.bool,
-  indeterminate: PropTypes.bool,
   checked: PropTypes.bool,
 }
 
 Switch.defaultProps = {
-  onChange: ()=>{},
+  onChange: () => { },
   disabled: false,
   error: false,
-  indeterminate: false,
 }
--- web/src/components/atoms/tabs.jsx
+++ web/src/components/atoms/tabs.jsx
@@ -5,6 +5,7 @@ import { useLink } from "react-aria";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
 import { motion } from "framer-motion";
+import PropTypes from "prop-types";
 
 
 export const NavTab = ({ href, label, onPress, active, fitted }) => {
@@ -15,20 +16,20 @@ export const NavTab = ({ href, label, onPress, active, fitted }) => {
       "text-text-soft": !active
     })}>
     <Link {...linkProps} to={href} className={classNames("outline-none flex flex-col",
-    {
-      "p-4":!fitted,
-      "pt-2 pb-3":fitted
-    })}>
+      {
+        "p-4": !fitted,
+        "pt-2 pb-3": fitted
+      })}>
       {label}
     </Link>
     {
       active && <motion.div layoutId="underline" className={classNames("h-1 bg-surface-primary-pressed z-10")}></motion.div>
     }
-    <div class="h-1 group-hover:bg-surface-hovered group-active:bg-surface-pressed bg-none transition-all bottom-0 absolute w-full z-0"></div>
+    <div className="h-1 group-hover:bg-surface-hovered group-active:bg-surface-pressed bg-none transition-all bottom-0 absolute w-full z-0"></div>
   </div>
 }
 
-export const NavTabs = ({ children, fitted, onChange }) => {
+export const NavTabs = ({ children, fitted, onChange, layoutId }) => {
   const [active, setActive] = useState(0);
   useEffect(() => {
     if (onChange) {
@@ -36,16 +37,42 @@ export const NavTabs = ({ children, fitted, onChange }) => {
     }
   }, [active])
   return <div className="flex flex-row gap-6">
-    <LayoutGroup>
+    <LayoutGroup id={layoutId}>
       {children.map((child, index) => {
         return cloneElement(child, {
           onPress: () => {
             setActive(index)
           },
           fitted,
+          key: index,
           active: active === index,
         })
       })}
     </LayoutGroup>
   </div>
+}
+
+NavTab.propTypes = {
+  href: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  onPress: PropTypes.func,
+  active: PropTypes.bool,
+  fitted: PropTypes.bool
+}
+
+NavTabs.propTypes = {
+   /**
+   * LayoutId should be provided in order to prevent multiple tabs to share same instance.
+   */
+  layoutId: PropTypes.string.isRequired,
+  children: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  fitted: PropTypes.bool,
+  onChange: PropTypes.func,
+}
+
+
+NavTab.defaultProps = {
+  label: "Item",
+  active: false,
+  fitted: false,
 }
