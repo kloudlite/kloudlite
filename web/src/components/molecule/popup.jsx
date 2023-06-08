0614f1456925f5298bf8f2b864d531ce93ae3119
Bikash Ojha <bikashojha@Karthiks-iMac.local>
2023-06-08 16:32:25 +0530

    Implemented popup using react aria

--- web/src/components/molecule/popup.jsx
+++ web/src/components/molecule/popup.jsx
@@ -1,78 +1,96 @@
-import React, {Fragment, useRef} from 'react';
-import { Dialog, Transition } from '@headlessui/react'
+import React, { useRef, useState } from 'react';
 
 import PropTypes from 'prop-types';
 import classnames from "classnames";
-import { BounceIt } from "../bounce-it.jsx";
 import { XFill } from '@jengaicons/react';
-import { Button } from '../atoms/button.jsx';
-import {useButton} from "react-aria";
+import { Button, IconButton } from '../atoms/button.jsx';
+import { useModalOverlay } from 'react-aria';
+import { useOverlayTriggerState } from 'react-stately';
+import { Overlay } from 'react-aria';
+import { useDialog } from 'react-aria';
+import { AnimatePresence, motion } from 'framer-motion';
 
-export const Popup = ({ header, body, show, onClose, secondaryAction, action }) => {
-    const closeButtonRef = useRef();
-    const closeButtonProps = useButton({ onPress: onClose }, closeButtonRef);
+
+export const Modal = (props) => {
+    let ref = useRef(null);
+    let { modalProps, underlayProps } = useModalOverlay(props, props.state, ref);
+
     return (
-        <>
-            <Transition appear show={show} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={onClose}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
+        <AnimatePresence>
+            {props.state.isOpen && <Overlay>
+                <motion.div
+                    className={classnames('fixed z-10 inset-0 flex items-center justify-center')}
+                    style={{ backgroundColor: props.backdrop && 'rgba(0,0,0,0.5)' }}
+                    {...underlayProps}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{
+                        duration: 0.25,
+                        ease: 'anticipate',
+                    }}
+                    key={"overlay"}
+                >
+                    <motion.div
+                        key={"modal"}
+                        {...modalProps}
+                        ref={ref}
+                        className='w-full max-w-[612px]'
+                        initial={{ opacity: 0, translateY: 4, scale: 0.97 }}
+                        animate={{ opacity: 1, translateY: 0, scale: 1 }}
+                        exit={{ opacity: 0, translateY: 4, scale: 0.97 }}
+                        transition={{
+                            duration: 0.25,
+                            ease: 'anticipate',
+                        }}
                     >
-                        <div className="fixed inset-0 bg-black bg-opacity-25" />
-                    </Transition.Child>
-
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded bg-surface-default shadow-modal border border-border-default transition-all">
-                                    <Dialog.Title
-                                        as="h3"
-                                    >
-                                        <div className='flex flex-row p-5 border-b border-border-default items-center'>
-                                            <span className='headingMd flex-1'>{header}</span>
-                                            <BounceIt>
-                                                <span {...closeButtonProps} ref={closeButtonRef}
-                                                    className={classnames("outline-none flex items-center rounded ring-offset-1 focus:ring-2 focus:ring-border-focus justify-center")} onPress={onClose}>
-                                                    <XFill size={24} color="currentColor" />
-                                                </span>
-                                            </BounceIt>
-                                        </div>
-                                    </Dialog.Title>
-                                    <div className='p-5 bodyMd'>
-                                        {body}
-                                    </div>
-                                    {(action || secondaryAction) && <div className='flex flex-row gap-2 p-5 justify-end'>
-                                        {secondaryAction && <Button label={secondaryAction.content} style='outline' onClick={secondaryAction.onAction} />}
-                                        {action && <Button label={action.content} onClick={action.onAction} />}
-                                    </div>}
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
-                    </div>
-                </Dialog>
-            </Transition>
-        </>
+                        {props.children}
+                    </motion.div>
+                </motion.div>
+            </Overlay>}
+        </AnimatePresence>
     )
 }
 
 
+export function AlertDialog(props) {
+    let { children, onClose, secondaryAction, action, header } = props;
+
+    let ref = useRef(null);
+    let { dialogProps, titleProps } = useDialog(
+        {
+            ...props,
+            role: "alertdialog"
+        },
+        ref
+    );
+
+    return (
+        <div {...dialogProps} ref={ref} className="outline-none transform overflow-hidden rounded bg-surface-default shadow-modal border border-border-default transition-all">
+            <h3 {...titleProps} className='flex flex-row p-5 border-b border-border-default items-center'>
+                <span className='headingMd flex-1'>{header}</span>
+                <IconButton IconComp={XFill} style={'plain'} size='small' onClick={onClose} />
+            </h3>
+            <div className="p-5 bodyMd">{children}</div>
+            {(action || secondaryAction) && <div className='flex flex-row gap-2 p-5 justify-end'>
+                {secondaryAction && <Button label={secondaryAction.content} style='outline' onClick={secondaryAction.onAction} />}
+                {action && <Button label={action.content} onClick={action.onAction} />}
+            </div>}
+        </div>
+    );
+}
+export const Popup = ({ header, show, onClose, secondaryAction, action, children, backdrop }) => {
+    let state = useOverlayTriggerState({ isOpen: show });
+    return <Modal state={state} backdrop={backdrop}>
+        <AlertDialog header={header} secondaryAction={secondaryAction} action={action} onClose={onClose}>
+            {children}
+        </AlertDialog>
+    </Modal>
+}
+
 Popup.propTypes = {
     header: PropTypes.string.isRequired,
-    body: PropTypes.any.isRequired,
+    children: PropTypes.any.isRequired,
     onClose: PropTypes.func,
     action: PropTypes.shape({
         content: PropTypes.string.isRequired,
@@ -82,12 +100,13 @@ Popup.propTypes = {
         content: PropTypes.string.isRequired,
         onAction: PropTypes.func
     }),
-    show: PropTypes.bool.isRequired
+    show: PropTypes.bool.isRequired,
+    backdrop: PropTypes.bool
 };
 
 Popup.defaultProps = {
     header: "Heading",
-    body: <p>
+    children: <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>,
     action: {
@@ -99,5 +118,6 @@ Popup.defaultProps = {
         onAction: () => { }
     },
     onClose: () => { },
-    show: true
+    show: true,
+    backdrop: false
 };
