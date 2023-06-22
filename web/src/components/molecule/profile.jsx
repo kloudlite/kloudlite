c8fb3f28fa3bc8b78097d9112f477340d5b81696
Bikash Ojha <bikashojha@Karthiks-iMac.local>
2023-06-22 12:55:41 +0530

    Updated profile

--- web/src/components/molecule/profile.jsx
+++ web/src/components/molecule/profile.jsx
@@ -1,14 +1,23 @@
-import { Avatar } from "../atoms/avatar.jsx";
+import { Avatar, AvatarBase } from "../atoms/avatar.jsx";
 import PropTypes from "prop-types";
+import { AriaButton } from "../atoms/button.jsx";
+import classNames from "classnames";
+import { useFocusRing } from "react-aria";
 
 export const Profile = ({ name, subtitle, color, size }) => {
-  return <div className={"inline-block flex py-0.5 px-1 gap-2 items-center"}>
-    <Avatar label={name} color={color} size={size} />
-    <div className="flex flex-col gap-y-1">
+  let { isFocusVisible, focusProps } = useFocusRing()
+  return <AriaButton
+    {...focusProps}
+    className={classNames("outline-none flex py-0.5 px-1 gap-2 items-center ring-offset-1 outline-none transition-all rounded",
+      {
+        "focus:ring-2 focus:ring-border-focus": isFocusVisible
+      })}>
+    <AvatarBase label={name} color={color} size={size} renderAs="div" />
+    <div className="flex flex-col gap-y-1  items-start">
       <div className={"bodyMd-medium"}>{name}</div>
       {subtitle && <div className={"bodySm text-text-soft"}>{subtitle}</div>}
     </div>
-  </div>
+  </AriaButton>
 }
 
 Profile.propTypes = {
