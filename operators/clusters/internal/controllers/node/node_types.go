8db9721f369d081383dd330b1065eef3ccbac71a
Abdhesh Nayak <abdheshnayak@gmail.com>
2023-06-23 17:10:10 +0530

    :label: Fixed some types to make more clear (#10)
    * :label: Fixed some types to make more clear
    
    updated some types inside nodepool of operator clusters for the awsnodeconfig.
    
    * :pencil2: Fixed typos: NadeName -> NodeName in clusters/v1
    
    By Mistake there was NadeName, so fixed it to NodeName and made changes accordingly.
    
    * :bulb: Removed some unwanted comments
    
    there was some extra comments which are no more needed so deleted those comments.

--- operators/clusters/internal/controllers/node/node_types.go
+++ operators/clusters/internal/controllers/node/node_types.go
@@ -6,16 +6,6 @@ type AwsProviderConfig struct {
 	AccountName  string `yaml:"accountName" json:"accountName"`
 }
 
-type AWSNode struct {
-	NodeId       string `yaml:"nodeId" json:"nodeId"`
-	Region       string `yaml:"region" json:"region"`
-	InstanceType string `yaml:"instanceType" json:"instanceType"`
-	VPC          string `yaml:"vpc" json:"vpc"`
-	ImageId      string `yaml:"imageId" json:"imageId"`
-	IsGpu        bool   `yaml:"isGpu" json:"isGpu"`
-	NodeType     string `yaml:"nodeType" json:"nodeType"`
-}
-
 type CommonProviderData struct {
 	TfTemplates string            `yaml:"tfTemplates"`
 	Labels      map[string]string `yaml:"labels"`
