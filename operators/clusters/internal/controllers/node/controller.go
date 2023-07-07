67adb84abf6c7da886d1d992b118053f8860d142
Abdhesh Nayak <abdheshnayak@gmail.com>
2023-07-07 13:40:57 +0530

    Feature/nodejob (#12)
    * :construction: wip
    
    * :construction: Wip

--- operators/clusters/internal/controllers/node/controller.go
+++ operators/clusters/internal/controllers/node/controller.go
@@ -169,9 +169,10 @@ func (r *Reconciler) finalize(req *rApi.Request[*clustersv1.Node]) stepResult.Re
 		if err := createDeleteNodeJob(); err != nil {
 			return failed(err)
 		}
+		return req.Done()
 	}
 
-	if j != nil && j.Status.Succeeded >= 1 {
+	if j.Status.Succeeded >= 1 {
 		return req.Finalize()
 	}
 
