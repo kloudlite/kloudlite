2e9e812575254024ddccd539eea1ba4c2eee3bc9
Anshuman Bhaskar <nxtcoder17@gmail.com>
2023-07-20 22:04:30 +0530

    Feature/improve agent authentication (#42)
    * fix(apps/auth): auth OAuth2 env validation
    
    - removes `GITHUB_WEBHOOK_URL` from github env validation for OAuth2
      support
    - removes `GITLAB_WEBHOOK_URL` from gitlab env validation for OAuth2
      support
    
    * fix(apps/message-office): implement access token validation
    
    - implements new way of authenticating agents, which does not include
      going back to database for each request
    
    - ensures, access token is valid before allowing agent to perform
      any action, except validating and getting a new token

--- apps/message-office/internal/app/grpc-server.go
+++ apps/message-office/internal/app/grpc-server.go
@@ -2,12 +2,16 @@ package app
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/kloudlite/operator/grpc-interfaces/grpc/messages"
 	"github.com/kloudlite/operator/pkg/kubectl"
 	"github.com/pkg/errors"
+	"google.golang.org/grpc/metadata"
 
 	"kloudlite.io/apps/message-office/internal/domain"
 	"kloudlite.io/apps/message-office/internal/env"
@@ -36,9 +40,87 @@ type grpcServer struct {
 	clusterUpdatesCounter  int64
 }
 
-func (g *grpcServer) parseError(ctx context.Context, errMsg *messages.ErrorData) (err error) {
+func encodeAccessToken(accountName, clusterName, clusterToken string, tokenSecret string) string {
+	info := fmt.Sprintf("account=%s;cluster=%s;token=%s", accountName, clusterName, clusterToken)
+
+	h := sha256.New()
+	h.Write([]byte(info + tokenSecret))
+	sum := fmt.Sprintf("%x", h.Sum(nil))
+
+	info += fmt.Sprintf(";sha256sum=%s", sum)
+
+	return base64.StdEncoding.EncodeToString([]byte(info))
+}
+
+func validateAndDecodeAccessToken(accessToken string, tokenSecret string) (accountName string, clusterName string, err error) {
+	b, err := base64.StdEncoding.DecodeString(accessToken)
+	if err != nil {
+		return "", "", errors.Wrap(err, "invalid access token, incorrect format")
+	}
+
+	info := string(b)
+
+	sp := strings.SplitN(info, ";sha256sum=", 2)
+	if len(sp) != 2 {
+		return "", "", errors.New("invalid access token, incorrect format")
+	}
+	data := sp[0]
+	sum := sp[1]
+
+	h := sha256.New()
+	h.Write([]byte(data + tokenSecret))
+	calculatedSum := fmt.Sprintf("%x", h.Sum(nil))
+
+	if sum != calculatedSum {
+		return "", "", errors.New("invalid access token, checksum mismatch")
+	}
+
+	s := strings.SplitN(data, ";", 3)
+	if len(s) != 3 {
+		return "", "", errors.New("invalid access token, incorrect data format")
+	}
+
+	for _, v := range s {
+		sp := strings.SplitN(v, "=", 2)
+		if len(sp) != 2 {
+			return "", "", errors.New("invalid access token, incorrect data format")
+		}
+		if sp[0] == "account" {
+			accountName = sp[1]
+		}
+		if sp[0] == "cluster" {
+			clusterName = sp[1]
+		}
+	}
+
+	return accountName, clusterName, nil
+}
+
+func validateAndDecodeFromGrpcContext(ctx context.Context, tokenSecret string) (accountName string, clusterName string, err error) {
+	authToken := metadata.ValueFromIncomingContext(ctx, "authorization")
+	if len(authToken) != 1 {
+		return "", "", errors.New("no authorization header passed")
+	}
+	return validateAndDecodeAccessToken(authToken[0], tokenSecret)
+}
+
+func (g *grpcServer) ValidateAccessToken(ctx context.Context, msg *messages.ValidateAccessTokenIn) (*messages.ValidateAccessTokenOut, error) {
+	logger := g.logger.WithKV("accountName", msg.AccountName).WithKV("cluster", msg.ClusterName)
+	logger.Infof("request received for access token validation")
+	isValid := true
+	defer func() {
+		logger.Infof("is access token valid? (%v)", isValid)
+	}()
+
+	if _, _, err := validateAndDecodeAccessToken(msg.AccessToken, g.ev.TokenHashingSecret); err != nil {
+		isValid = false
+	}
+	return &messages.ValidateAccessTokenOut{Valid: isValid}, nil
+}
+
+func (g *grpcServer) parseError(ctx context.Context, accountName string, clusterName string, errMsg *messages.ErrorData) (err error) {
 	g.errorMessagesCounter++
-	logger := g.logger.WithKV("accountName", errMsg.AccountName).WithKV("cluster", errMsg.ClusterName)
+	logger := g.logger.WithKV("accountName", accountName).WithKV("cluster", clusterName)
 
 	logger.Infof("[%v] received error-on-apply message", g.errorMessagesCounter)
 	defer func() {
@@ -50,11 +132,7 @@ func (g *grpcServer) parseError(ctx context.Context, errMsg *messages.ErrorData)
 		logger.Infof("[%v] processed error-on-apply message", g.infraUpdatesCounter)
 	}()
 
-	if err := g.domain.ValidateAccessToken(ctx, errMsg.AccessToken, errMsg.AccountName, errMsg.ClusterName); err != nil {
-		return errors.Wrap(err, "while validating access token")
-	}
-
-	if _, err := g.producer.Produce(ctx, g.ev.KafkaTopicErrorOnApply, errMsg.ClusterName, errMsg.Data); err != nil {
+	if _, err := g.producer.Produce(ctx, g.ev.KafkaTopicErrorOnApply, clusterName, errMsg.Message); err != nil {
 		return errors.Wrap(err, fmt.Sprintf("while producing to topic (%s)", g.ev.KafkaTopicErrorOnApply))
 	}
 	logger.Infof("[%v] dispatched error-on-apply message", g.errorMessagesCounter)
@@ -63,12 +141,16 @@ func (g *grpcServer) parseError(ctx context.Context, errMsg *messages.ErrorData)
 
 // ReceiveErrors implements messages.MessageDispatchServiceServer
 func (g *grpcServer) ReceiveErrors(server messages.MessageDispatchService_ReceiveErrorsServer) error {
+	accountName, clusterName, err := validateAndDecodeFromGrpcContext(server.Context(), g.ev.TokenHashingSecret)
+	if err != nil {
+		return err
+	}
 	for {
 		errorMsg, err := server.Recv()
 		if err != nil {
 			return err
 		}
-		_ = g.parseError(server.Context(), errorMsg)
+		_ = g.parseError(server.Context(), accountName, clusterName, errorMsg)
 	}
 }
 
@@ -76,15 +158,19 @@ func (g *grpcServer) ReceiveErrors(server messages.MessageDispatchService_Receiv
 func (g *grpcServer) GetAccessToken(ctx context.Context, msg *messages.GetClusterTokenIn) (*messages.GetClusterTokenOut, error) {
 	g.logger.Infof("request received for cluster-token (%q) exchange", msg.ClusterToken)
 
-	record, err := g.domain.GenAccessToken(ctx, msg.ClusterToken)
+	ct, err := g.domain.GetClusterToken(ctx, msg.AccountName, msg.ClusterName)
 	if err != nil {
 		return nil, err
 	}
+	if ct != msg.ClusterToken {
+		return nil, errors.New("invalid cluster-token,account-name,cluster-name triplet")
+	}
 
-	g.logger.Infof("SUCCESSFUL cluster-token (%q) exchange for account=%q, cluster=%q", msg.ClusterToken, record.AccountName, record.ClusterName)
+	s := encodeAccessToken(msg.AccountName, msg.ClusterName, msg.ClusterToken, g.ev.TokenHashingSecret)
+	g.logger.Infof("SUCCESSFUL cluster-token exchange for account=%q, cluster=%q", msg.ClusterToken, msg.AccountName, msg.ClusterName)
 
 	return &messages.GetClusterTokenOut{
-		AccessToken: record.AccessToken,
+		AccessToken: s,
 	}, nil
 }
 
@@ -99,21 +185,25 @@ func (g *grpcServer) createConsumer(ev *env.Env, topicName string) (redpanda.Con
 	}, []string{topicName})
 }
 
-func (g *grpcServer) SendActions(request *messages.StreamActionsRequest, server messages.MessageDispatchService_SendActionsServer) error {
-	if err := g.domain.ValidateAccessToken(server.Context(), request.AccessToken, request.AccountName, request.ClusterName); err != nil {
+func (g *grpcServer) SendActions(request *messages.Empty, server messages.MessageDispatchService_SendActionsServer) error {
+	accountName, clusterName, err := validateAndDecodeFromGrpcContext(server.Context(), g.ev.TokenHashingSecret)
+	if err != nil {
 		return err
 	}
 
-	key := fmt.Sprintf("%s/%s", request.AccountName, request.ClusterName)
+	logger := g.logger.WithKV("accountName", accountName, "clusterName", clusterName)
+	logger.Infof("request received for sending actions to cluster")
+	defer func() {
+		logger.Infof("stopping sending actions to cluster")
+	}()
+
+	key := fmt.Sprintf("%s/%s", accountName, clusterName)
 
 	consumer, err := func() (redpanda.Consumer, error) {
 		if c, ok := g.consumers[key]; ok {
 			return c, nil
 		}
-		c, err := g.createConsumer(
-			g.ev,
-			common.GetKafkaTopicName(request.AccountName, request.ClusterName),
-		)
+		c, err := g.createConsumer(g.ev, common.GetKafkaTopicName(accountName, clusterName))
 		if err != nil {
 			return nil, err
 		}
@@ -139,15 +229,15 @@ func (g *grpcServer) SendActions(request *messages.StreamActionsRequest, server 
 		defer func() {
 			g.logger.WithKV("timestamp", timeStamp).Infof("processed message")
 		}()
-		return server.Send(&messages.Action{Data: msg})
+		return server.Send(&messages.Action{Message: msg})
 	})
 	return nil
 }
 
-func (g *grpcServer) processResourceUpdate(ctx context.Context, msg *messages.ResourceUpdate) (err error) {
+func (g *grpcServer) processResourceUpdate(ctx context.Context, accountName string, clusterName string, msg *messages.ResourceUpdate) (err error) {
 	g.resourceUpdatesCounter++
 
-	logger := g.logger.WithKV("accountName", msg.AccountName).WithKV("clusterName", msg.ClusterName)
+	logger := g.logger.WithKV("accountName", accountName).WithKV("clusterName", clusterName)
 	logger.Infof("[%v] received resource status update", g.resourceUpdatesCounter)
 	defer func() {
 		if err != nil {
@@ -158,11 +248,7 @@ func (g *grpcServer) processResourceUpdate(ctx context.Context, msg *messages.Re
 		logger.Infof("[%v] processed resource status update", g.infraUpdatesCounter)
 	}()
 
-	if err = g.domain.ValidateAccessToken(ctx, msg.AccessToken, msg.AccountName, msg.ClusterName); err != nil {
-		return errors.Wrap(err, fmt.Sprintf("[%v] while validating accessToken", g.resourceUpdatesCounter))
-	}
-
-	if _, err := g.producer.Produce(ctx, g.ev.KafkaTopicStatusUpdates, msg.ClusterName, msg.Message); err != nil {
+	if _, err := g.producer.Produce(ctx, g.ev.KafkaTopicStatusUpdates, clusterName, msg.Message); err != nil {
 		return errors.Wrap(err, fmt.Sprintf("while producing resource update to topic %q", g.ev.KafkaTopicStatusUpdates))
 	}
 
@@ -171,18 +257,22 @@ func (g *grpcServer) processResourceUpdate(ctx context.Context, msg *messages.Re
 }
 
 func (g *grpcServer) ReceiveResourceUpdates(server messages.MessageDispatchService_ReceiveResourceUpdatesServer) error {
+	accountName, clusterName, err := validateAndDecodeFromGrpcContext(server.Context(), g.ev.TokenHashingSecret)
+	if err != nil {
+		return err
+	}
 	for {
 		statusMsg, err := server.Recv()
 		if err != nil {
 			return err
 		}
-		_ = g.processResourceUpdate(server.Context(), statusMsg)
+		_ = g.processResourceUpdate(server.Context(), accountName, clusterName, statusMsg)
 	}
 }
 
-func (g *grpcServer) processClusterUpdate(ctx context.Context, msg *messages.ClusterUpdate) (err error) {
+func (g *grpcServer) processClusterUpdate(ctx context.Context, accountName string, clusterName string, msg *messages.ClusterUpdate) (err error) {
 	g.clusterUpdatesCounter++
-	logger := g.logger.WithKV("accountName", msg.AccountName).WithKV("clusterName", msg.ClusterName)
+	logger := g.logger.WithKV("accountName", accountName).WithKV("clusterName", clusterName)
 
 	logger.Infof("[%v] received Cluster update", g.clusterUpdatesCounter)
 	defer func() {
@@ -194,11 +284,7 @@ func (g *grpcServer) processClusterUpdate(ctx context.Context, msg *messages.Clu
 		logger.Infof("[%v] processed Cluster update", g.infraUpdatesCounter)
 	}()
 
-	if err = g.domain.ValidateAccessToken(ctx, msg.AccessToken, msg.AccountName, msg.ClusterName); err != nil {
-		return errors.Wrap(err, "while validating access token")
-	}
-
-	if _, err := g.producer.Produce(ctx, g.ev.KafkaTopicClusterUpdates, msg.ClusterName, msg.Message); err != nil {
+	if _, err := g.producer.Produce(ctx, g.ev.KafkaTopicClusterUpdates, clusterName, msg.Message); err != nil {
 		return errors.Wrap(err, fmt.Sprintf("while producing message into kafka topic (%s) for ", g.ev.KafkaTopicClusterUpdates))
 	}
 
@@ -207,20 +293,23 @@ func (g *grpcServer) processClusterUpdate(ctx context.Context, msg *messages.Clu
 }
 
 func (g *grpcServer) ReceiveClusterUpdates(server messages.MessageDispatchService_ReceiveClusterUpdatesServer) (err error) {
+	accountName, clusterName, err := validateAndDecodeFromGrpcContext(server.Context(), g.ev.TokenHashingSecret)
+	if err != nil {
+		return err
+	}
 	for {
 		clientUpdateMsg, err := server.Recv()
 		if err != nil {
 			return err
 		}
 
-		_ = g.processClusterUpdate(server.Context(), clientUpdateMsg)
+		_ = g.processClusterUpdate(server.Context(), accountName, clusterName, clientUpdateMsg)
 	}
 }
 
-func (g *grpcServer) processInfraUpdate(ctx context.Context, msg *messages.InfraUpdate) (err error) {
+func (g *grpcServer) processInfraUpdate(ctx context.Context, accountName string, clusterName string, msg *messages.InfraUpdate) (err error) {
 	g.infraUpdatesCounter++
-
-	logger := g.logger.WithKV("accountName", msg.AccountName).WithKV("clusterName", msg.ClusterName)
+	logger := g.logger.WithKV("accountName", accountName).WithKV("clusterName", clusterName)
 
 	logger.Infof("[%v] received infra update", g.infraUpdatesCounter)
 	defer func() {
@@ -232,11 +321,7 @@ func (g *grpcServer) processInfraUpdate(ctx context.Context, msg *messages.Infra
 		g.logger.Infof("[%v] processed infra update", g.infraUpdatesCounter)
 	}()
 
-	if err := g.domain.ValidateAccessToken(ctx, msg.AccessToken, msg.AccountName, msg.ClusterName); err != nil {
-		return err
-	}
-
-	po, err := g.producer.Produce(ctx, g.ev.KafkaTopicInfraUpdates, msg.ClusterName, msg.Message)
+	po, err := g.producer.Produce(ctx, g.ev.KafkaTopicInfraUpdates, clusterName, msg.Message)
 	if err != nil {
 		return err
 	}
@@ -250,11 +335,15 @@ func (g *grpcServer) processInfraUpdate(ctx context.Context, msg *messages.Infra
 
 // ReceiveInfraUpdates implements messages.MessageDispatchServiceServer
 func (g *grpcServer) ReceiveInfraUpdates(server messages.MessageDispatchService_ReceiveInfraUpdatesServer) (err error) {
+	accountName, clusterName, err := validateAndDecodeFromGrpcContext(server.Context(), g.ev.TokenHashingSecret)
+	if err != nil {
+		return err
+	}
 	for {
 		statusMsg, err := server.Recv()
 		if err != nil {
 			return err
 		}
-		_ = g.processInfraUpdate(server.Context(), statusMsg)
+		_ = g.processInfraUpdate(server.Context(), accountName, clusterName, statusMsg)
 	}
 }
