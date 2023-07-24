2213dae29cc35c8affb8046cfde73df430ff1a92
Bikash Ojha <tulsiojhaamg@gmail.com>
2023-07-24 12:30:17 +0530

    Added Toast Component, Added spacing tokens

--- web/src/apps/console/root.jsx
+++ web/src/apps/console/root.jsx
@@ -1,6 +1,8 @@
 import Root, { links as baseLinks } from '~/lib/app-setup/root';
 import authStylesUrl from './styles/index.css';
 
+export { loader } from '~/lib/app-setup/root.jsx';
+
 export const links = () => {
   return [...baseLinks(), { rel: 'stylesheet', href: authStylesUrl }];
 };
