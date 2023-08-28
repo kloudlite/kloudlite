27802bea74a67f1eae8daa4e74fa63cd8fb141c7
nxtcoder17 <nxtcoder17@gmail.com>
2023-08-28 12:45:33 +0530

    repo renamed to IAC-production

---
+++ infrastructure-as-code/terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "access_key" {
+  default = ""
+}
+
+variable "secret_key" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
+
+variable "node_name" {
+  default = ""
+}
+
+variable "instance_type" {
+  default = ""
+}
+
+variable "pubkey" {
+  default = ""
+}
+
+variable "keys_path" {
+  default = ""
+}
+
+variable "ami" {
+  default = ""
+}
