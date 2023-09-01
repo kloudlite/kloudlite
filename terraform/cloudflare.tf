580c0df1bd7db1271965b4496e25d67ff2c9492d
nxtcoder17 <nxtcoder17@gmail.com>
2023-09-01 07:24:13 +0530

    feat: provisioning, and managing IPs in cloudflare
    - HA k8s masters provisioning
    - k8s workers getting attached to the cluster
    - cloudflare DNS records updated for given domain, with master's IPs

---
+++ terraform/cloudflare.tf
@@ -0,0 +1,22 @@
+provider "cloudflare" {
+  api_token = var.cloudflare.api_token
+}
+
+resource "cloudflare_record" "k8s_first_master_A_record" {
+  zone_id = var.cloudflare.zone_id
+  name    = "test-prod"
+  value   = aws_instance.k8s_first_master.public_ip
+  type    = "A"
+  ttl     = 300
+  proxied = false
+}
+
+resource "cloudflare_record" "k8s_masters_A_records" {
+  for_each = { for idx, value in aws_instance.k8s_masters : idx => value.public_ip }
+  zone_id  = var.cloudflare.zone_id
+  name     = var.cloudflare.domain
+  value    = each.value
+  type     = "A"
+  ttl      = 300
+  proxied  = false
+}
--- terraform/resource.tf
+++ terraform/resource.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.13.0"
+    }
   }
 
   required_version = ">= 1.2.0"
@@ -14,7 +19,6 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
-
 
 data "aws_ami" "latest-ubuntu" {
   most_recent = true
@@ -30,7 +34,6 @@ data "aws_ami" "latest-ubuntu" {
     values = ["hvm"]
   }
 }
-
 
 resource "aws_security_group" "sg" {
   # name = "${var.node_name}-sg"
@@ -140,19 +143,50 @@ resource "aws_security_group" "sg" {
   # }
 }
 
+locals {
+  master_azs = split(",", var.master_nodes_config["availability_zones"])
+  master_config = [
+    for i in range(1, var.master_nodes_config["count"] + 1) : {
+      instance_name     = "${var.master_nodes_config["name"]}-${i}"
+      instance_type     = var.master_nodes_config["instance_type"]
+      ami               = var.master_nodes_config["ami"]
+      availability_zone = local.master_azs[(tonumber(i) - 1) % length(local.master_azs)]
+      # availability_zone = local.master_azs_map[(tonumber(i) - 1) % length(local.master_azs)].zone
+    }
+  ]
 
+  worker_azs = split(",", var.worker_nodes_config["availability_zones"])
+  worker_config = [
+    for i in range(1, var.worker_nodes_config["count"] + 1) : {
+      instance_name     = "${var.worker_nodes_config["name"]}-${i}"
+      instance_type     = var.worker_nodes_config["instance_type"]
+      ami               = var.worker_nodes_config["ami"]
+      availability_zone = local.worker_azs[(tonumber(i) - 1) % length(local.worker_azs)]
+      # availability_zone = local.worker_azs_map[(i-1) % length(local.worker_azs)].zone
+    }
+  ]
+}
 
-resource "aws_instance" "byoc-node" {
-  ami             = var.ami == "" ? "${data.aws_ami.latest-ubuntu.id}" : var.ami
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.sg.name]
+resource "aws_key_pair" "k8s_masters_ssh_key" {
+  key_name   = "iac-production"
+  public_key = file(var.public_key_path)
+}
 
-  user_data = templatefile("./init.sh", {
-    pubkey = file("${var.keys_path}/access.pub")
-  })
+# resource "aws_eip" "k8s_first_master_public_ip" {
+#   instance = aws_instance.k8s_first_master.id
+# }
+
+resource "aws_instance" "k8s_first_master" {
+  #  ami               = var.master_nodes_config["ami"]
+  ami               = local.master_config[0].ami
+  instance_type     = local.master_config[0].instance_type
+  security_groups   = [aws_security_group.sg.name]
+  key_name          = aws_key_pair.k8s_masters_ssh_key.key_name
+  availability_zone = local.master_config[0].availability_zone
 
   tags = {
-    Name = var.node_name
+    #    Name = "${var.master_nodes_config["name"]}-1"
+    Name = local.master_config[0].instance_name
   }
 
   root_block_device {
@@ -162,12 +196,138 @@ resource "aws_instance" "byoc-node" {
     # kms_key_id  = data.aws_kms_key.customer_master_key.arn
   }
 
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    host        = self.public_ip
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      <<-EOT
+      cat > runner-config.yml <<EOF2
+      runAs: primaryMaster
+      primaryMaster:
+        publicIP: ${self.public_ip}
+        token: ${var.k3s_token}
+        nodeName: ${local.master_config[0].instance_name}
+        SANs:
+          - ${var.cloudflare.domain}
+      EOF2
+      sudo ln -sf $PWD/runner-config.yml /runner-config.yml
+      EOT
+    ]
+  }
 }
 
-output "node-ip" {
-  value = aws_instance.byoc-node.public_ip
+resource "aws_instance" "k8s_masters" {
+  for_each          = { for idx, config in local.master_config : idx => config if idx >= 1 }
+  ami               = var.master_nodes_config["ami"]
+  instance_type     = each.value.instance_type
+  security_groups   = [aws_security_group.sg.name]
+  key_name          = aws_key_pair.k8s_masters_ssh_key.key_name
+  availability_zone = each.value.availability_zone
+
+  tags = {
+    Name = each.value.instance_name
+  }
+
+  root_block_device {
+    volume_size = 100 # in GB <<----- I increased this!
+    volume_type = "standard"
+    encrypted   = false
+    # kms_key_id  = data.aws_kms_key.customer_master_key.arn
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    host        = self.public_ip
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      <<-EOT
+      cat > runner-config.yml <<EOF2
+      runAs: secondaryMaster
+      secondaryMaster:
+        publicIP: ${self.public_ip}
+        serverIP: ${aws_instance.k8s_first_master.public_ip}
+        token: ${var.k3s_token}
+        nodeName: ${each.value.instance_name}
+        SANs:
+          - ${var.cloudflare.domain}
+      EOF2
+      sudo ln -sf $PWD/runner-config.yml /runner-config.yml
+      # sudo rm -f ~/.ssh/authorized_keys
+      EOT
+    ]
+  }
 }
 
-output "node-name" {
-  value = var.node_name
+
+resource "aws_instance" "k8s_workers" {
+  for_each          = { for idx, config in local.worker_config : idx => config }
+  ami               = var.worker_nodes_config["ami"]
+  instance_type     = each.value.instance_type
+  security_groups   = [aws_security_group.sg.name]
+  availability_zone = each.value.availability_zone
+  key_name          = aws_key_pair.k8s_masters_ssh_key.key_name
+
+  tags = {
+    Name = each.value.instance_name
+  }
+
+  root_block_device {
+    volume_size = 100
+    volume_type = "standard"
+    encrypted   = false
+    # kms_key_id  = data.aws_kms_key.customer_master_key.arn
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    host        = self.public_ip
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      <<-EOT
+      cat > runner-config.yml <<EOF2
+      runAs: agent
+      agent:
+        publicIP: ${self.public_ip}
+        # serverIP: ${aws_instance.k8s_first_master.public_ip}
+        serverIP: ${var.cloudflare.domain}
+        token: ${var.k3s_token}
+        nodeName: ${each.value.instance_name}
+      EOF2
+      sudo ln -sf $PWD/runner-config.yml /runner-config.yml
+      EOT
+    ]
+  }
 }
+
+resource "null_resource" "grab_kube_config" {
+  provisioner "local-exec" {
+    # command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${var.private_key_path} ubuntu@${aws_instance.k8s_first_master.public_ip}:/etc/rancher/k3s/k3s.yaml kubeconfig.yaml"
+    command = "ssh -i ${var.private_key_path} ubuntu@${aws_instance.k8s_first_master.public_ip} -C 'while true; do [ ! -f /etc/rancher/k3s/k3s.yaml ] && echo 'k3s yaml not found, re-checking in 1s' && sleep 1 && continue;  sudo cat /etc/rancher/k3s/k3s.yaml; break; done' > kubeconfig.yaml"
+    # command = "ssh -i ${var.private_key_path} ubuntu@${aws_instance.k8s_first_master.public_ip} -C 'sudo cat /etc/rancher/k3s/k3s.yaml' > kubeconfig.yaml"
+  }
+}
+
+output "k8s_masters_public_ips" {
+  value = concat([aws_instance.k8s_first_master.public_ip], [for instance in aws_instance.k8s_masters : instance.public_ip])
+}
+
+# output "node-ip" {
+#   value = aws_instance.k8s_masters[*].public_ip
+# }
+#
+# output "node-name" {
+#   value = var.node_name
+# }
