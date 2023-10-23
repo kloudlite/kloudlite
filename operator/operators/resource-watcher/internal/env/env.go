5e7de75b9f658fe6c7af76208c3fd609e6315af5
nxtcoder17 <nxtcoder17@gmail.com>
2023-10-23 12:13:21 +0530

    feat(operators/resource-watcher): support running on kloudlite platform
    - watcher controller now accepts a generic interface with implemented
      methods to dispatch status updates, instead of relying on GRPC (previously)
    - as platform cluster, will have kafka available, when running
      controller with `--running-on-platform`, it will use kafka to dispatch
      status updates, and when it is ommitted (by default), it will use
      GRPC and message-office the usual way

--- operator/operators/resource-watcher/internal/env/env.go
+++ operator/operators/resource-watcher/internal/env/env.go
@@ -7,13 +7,26 @@ import (
 )
 
 type Env struct {
+	CommonEnv
+	RunningOnPlatformEnv
+	RunningOnTargetClusterEnv
+}
+
+type CommonEnv struct {
 	ReconcilePeriod         time.Duration `env:"RECONCILE_PERIOD" required:"true"`
 	MaxConcurrentReconciles int           `env:"MAX_CONCURRENT_RECONCILES" required:"true"`
+	AccountName             string        `env:"ACCOUNT_NAME" required:"true"`
+	ClusterName             string        `env:"CLUSTER_NAME" required:"true"`
+}
 
-	GrpcAddr string `env:"GRPC_ADDR" required:"true"`
+type RunningOnPlatformEnv struct {
+	KafkaBrokers              string `env:"KAFKA_BROKERS" required:"true"`
+	KafkaResourceUpdatesTopic string `env:"KAFKA_RESOURCE_UPDATES_TOPIC" required:"true"`
+	KafkaInfraUpdatesTopic    string `env:"KAFKA_INFRA_UPDATES_TOPIC" required:"true"`
+}
 
-	AccountName string `env:"ACCOUNT_NAME" required:"true"`
-	ClusterName string `env:"CLUSTER_NAME" required:"true"`
+type RunningOnTargetClusterEnv struct {
+	GrpcAddr    string `env:"GRPC_ADDR" required:"true"`
 	AccessToken string `env:"ACCESS_TOKEN" required:"true"`
 
 	ClusterIdentitySecretName      string `env:"CLUSTER_IDENTITY_SECRET_NAME" required:"true"`
@@ -22,10 +35,26 @@ type Env struct {
 	OperatorsNamespace string `env:"OPERATORS_NAMESPACE" required:"true"`
 }
 
-func GetEnvOrDie() *Env {
-	ev := Env{}
+func GetCommonEnv() (CommonEnv, error) {
+	ev := CommonEnv{}
 	if err := env.Set(&ev); err != nil {
-		panic(err)
+		return CommonEnv{}, err
 	}
-	return &ev
+	return ev, nil
+}
+
+func GetPlatformEnv() (RunningOnPlatformEnv, error) {
+	ev := RunningOnPlatformEnv{}
+	if err := env.Set(&ev); err != nil {
+		return RunningOnPlatformEnv{}, err
+	}
+	return ev, nil
+}
+
+func GetTargetClusterEnvs() (RunningOnTargetClusterEnv, error) {
+	ev := RunningOnTargetClusterEnv{}
+	if err := env.Set(&ev); err != nil {
+		return RunningOnTargetClusterEnv{}, err
+	}
+	return ev, nil
 }
