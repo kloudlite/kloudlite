6e578ff02822e86e3fe775531b189c4a18041458
nxtcoder17 <nxtcoder17@gmail.com>
2023-10-25 12:11:39 +0530

    feat(operators/nodepool): nodepool now creates clustersv1.Node objects
    - nodepool controller, whenever target updates, creates
      clustersv1.Node object, which is then managed by node controller
    - autoscaling job, though will be managed by nodepool controller
      only
    - only role of clustersv1.Node CRD and its controller is to manage
      deletion lifecycle of real corev1.Node, like draining and deleting it
      after sometime in future
    - cluster autoscaler will also delete `clustersv1.Node` object, whenever
      it needs to downscale the cluster, and update nodepool`s target size

--- operators/nodepool/main.go
+++ operators/nodepool/main.go
@@ -4,6 +4,7 @@ import (
 	clustersv1 "github.com/kloudlite/operator/apis/clusters/v1"
 	"github.com/kloudlite/operator/operator"
 	"github.com/kloudlite/operator/operators/nodepool/internal/env"
+	node_controller "github.com/kloudlite/operator/operators/nodepool/internal/node-controller"
 	nodepool_controller "github.com/kloudlite/operator/operators/nodepool/internal/nodepool-controller"
 )
 
@@ -11,9 +12,15 @@ func main() {
 	ev := env.GetEnvOrDie()
 	mgr := operator.New("nodepool")
 	mgr.AddToSchemes(clustersv1.AddToScheme)
-	mgr.RegisterControllers(&nodepool_controller.Reconciler{
-		Env:  ev,
-		Name: "nodepool",
-	})
+	mgr.RegisterControllers(
+		&nodepool_controller.Reconciler{
+			Env:  ev,
+			Name: "nodepool",
+		},
+		&node_controller.Reconciler{
+			Env:  ev,
+			Name: "node",
+		},
+	)
 	mgr.Start()
 }
