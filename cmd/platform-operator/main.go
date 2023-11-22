4228be0785dabdb01986e7c09f4a4639afafc2cc
nxtcoder17 <nxtcoder17@gmail.com>
2023-11-22 11:57:27 +0530

    fix(cmd/platform-operator): fixes Container image
    - redpanda operator needs to have bash, and rpk binaries, adds them to
      our base image
    -

--- cmd/platform-operator/main.go
+++ cmd/platform-operator/main.go
@@ -5,24 +5,38 @@ import (
 	app "github.com/kloudlite/operator/operators/app-n-lambda/controller"
 	clusters "github.com/kloudlite/operator/operators/clusters/controller"
 	helmCharts "github.com/kloudlite/operator/operators/helm-charts/controller"
-	// msvcMongo "github.com/kloudlite/operator/operators/msvc-mongo/controller"
-	// msvcAndMres "github.com/kloudlite/operator/operators/msvc-n-mres/controller"
-	// msvcRedis "github.com/kloudlite/operator/operators/msvc-redis/controller"
-	// msvcRedpanda "github.com/kloudlite/operator/operators/msvc-redpanda/controller"
-	// nodepool "github.com/kloudlite/operator/operators/nodepool/controller"
+	msvcMongo "github.com/kloudlite/operator/operators/msvc-mongo/controller"
+	msvcAndMres "github.com/kloudlite/operator/operators/msvc-n-mres/controller"
+
+	msvcRedis "github.com/kloudlite/operator/operators/msvc-redis/controller"
+	msvcRedpanda "github.com/kloudlite/operator/operators/msvc-redpanda/controller"
+	nodepool "github.com/kloudlite/operator/operators/nodepool/controller"
 	project "github.com/kloudlite/operator/operators/project/controller"
+	resourceWatcher "github.com/kloudlite/operator/operators/resource-watcher/controller"
+	routers "github.com/kloudlite/operator/operators/routers/controller"
 )
 
 func main() {
-	mgr := operator.New("platform operator")
+	mgr := operator.New("platform-operator")
+
+	// kloudlite resources
 	app.RegisterInto(mgr)
 	project.RegisterInto(mgr)
-	clusters.RegisterInto(mgr)
 	helmCharts.RegisterInto(mgr)
-	// msvcAndMres.RegisterInto(mgr)
-	// msvcMongo.RegisterInto(mgr)
-	// msvcRedis.RegisterInto(mgr)
-	// msvcRedpanda.RegisterInto(mgr)
-	// nodepool.RegisterInto(mgr)
+	routers.RegisterInto(mgr)
+
+	// kloudlite managed services
+	msvcAndMres.RegisterInto(mgr)
+	msvcMongo.RegisterInto(mgr)
+	msvcRedis.RegisterInto(mgr)
+	msvcRedpanda.RegisterInto(mgr)
+
+	// kloudlite cluster management
+	clusters.RegisterInto(mgr)
+	nodepool.RegisterInto(mgr)
+
+	// kloudlite resource status updates
+	resourceWatcher.RegisterInto(mgr, true)
+
 	mgr.Start()
 }
