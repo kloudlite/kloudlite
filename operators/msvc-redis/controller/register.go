7e02123bc11acbc4618b3574037363202baad63b
nxtcoder17 <nxtcoder17@gmail.com>
2023-12-12 17:33:23 +0530

    fix(operators/msvc-redis): standalone redis fix
    - refactorings helm redis standalone managed service
    - updates mongodb CRDs
    - some fixes to logger

--- operators/msvc-redis/controller/register.go
+++ operators/msvc-redis/controller/register.go
@@ -1,20 +1,19 @@
 package controller
 
 import (
+	crdsv1 "github.com/kloudlite/operator/apis/crds/v1"
 	redisMsvcv1 "github.com/kloudlite/operator/apis/redis.msvc/v1"
 	"github.com/kloudlite/operator/operator"
-	"github.com/kloudlite/operator/operators/msvc-redis/internal/controllers/acl-account"
-	"github.com/kloudlite/operator/operators/msvc-redis/internal/controllers/acl-configmap"
 	"github.com/kloudlite/operator/operators/msvc-redis/internal/controllers/standalone"
 	"github.com/kloudlite/operator/operators/msvc-redis/internal/env"
 )
 
 func RegisterInto(mgr operator.Operator) {
 	ev := env.GetEnvOrDie()
-	mgr.AddToSchemes(redisMsvcv1.AddToScheme)
+	mgr.AddToSchemes(redisMsvcv1.AddToScheme, crdsv1.AddToScheme)
 	mgr.RegisterControllers(
 		&standalone.ServiceReconciler{Name: "msvc-redis:standalone-svc", Env: ev},
-		&aclaccount.Reconciler{Name: "msvc-redis:acl-account", Env: ev},
-		&acl_configmap.Reconciler{Name: "msvc-redis:acl-configmap", Env: ev},
+		// &aclaccount.Reconciler{Name: "msvc-redis:acl-account", Env: ev},
+		// &acl_configmap.Reconciler{Name: "msvc-redis:acl-configmap", Env: ev},
 	)
 }
