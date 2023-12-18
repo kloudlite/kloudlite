20716166bf0cb5857fc684186add0fd8af8e2928
nxtcoder17 <nxtcoder17@gmail.com>
2023-12-18 14:17:07 +0530

    fix(infra-templates): label refactorings

--- infrastructure-templates/kl-target-cluster-aws-only-masters/main.tf
+++ infrastructure-templates/kl-target-cluster-aws-only-masters/main.tf
@@ -4,7 +4,7 @@ module "kl-master-nodes-on-aws" {
   enable_nvidia_gpu_support = var.enable_nvidia_gpu_support
   k3s_masters               = var.k3s_masters
   kloudlite_params          = var.kloudlite_params
-  tracker_id                = "${var.tracker_id}-master"
+  tracker_id                = "${var.tracker_id}-masters"
   save_ssh_key_to_path      = var.save_ssh_key_to_path
   save_kubeconfig_to_path   = var.save_kubeconfig_to_path
   extra_server_args         = var.extra_server_args
--- infrastructure-templates/kl-target-cluster-aws-only-workers/main.tf
+++ infrastructure-templates/kl-target-cluster-aws-only-workers/main.tf
@@ -1,12 +1,12 @@
 module "kl-worker-nodes-on-aws" {
-  source         = "../../terraform/bundles/kl-worker-nodes-on-aws"
-  aws_region     = var.aws_region
+  source     = "../../terraform/bundles/kl-worker-nodes-on-aws"
+  aws_region = var.aws_region
 
   ec2_nodepools              = var.ec2_nodepools
   k3s_join_token             = var.k3s_join_token
   k3s_server_public_dns_host = var.k3s_server_public_dns_host
   spot_nodepools             = var.spot_nodepools
-  tracker_id                 = "${var.tracker_id}-worker"
+  tracker_id                 = "${var.tracker_id}-workers"
   extra_agent_args           = var.extra_agent_args
   save_ssh_key_to_path       = var.save_ssh_key_to_path
 }
