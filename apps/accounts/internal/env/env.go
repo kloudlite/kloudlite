3f597cf49d9d93e8fab35ebb037b9cb2f5ad951f
Karthik Thirumalasetti <karthik@kloudlite.io>
2023-12-18 18:19:07 +0530

    Fix/env fixes (#98)
    * Editing env vars of auditloging, auth and accounts services
    
    * added event log stream name to the working-audit-logint app
    
    * removed redis config from auth and fixed compilation

--- apps/accounts/internal/env/env.go
+++ apps/accounts/internal/env/env.go
@@ -12,19 +12,7 @@ type Env struct {
 	DBName string `env:"MONGO_DB_NAME" required:"true"`
 	DBUrl  string `env:"MONGO_URI" required:"true"`
 
-	// RedisHosts    string `env:"REDIS_HOSTS" required:"true"`
-	// RedisUsername string `env:"REDIS_USERNAME" required:"true"`
-	// RedisPassword string `env:"REDIS_PASSWORD" required:"true"`
-	// RedisPrefix   string `env:"REDIS_PREFIX" required:"true"`
-
-	AuthRedisHosts    string `env:"AUTH_REDIS_HOSTS" required:"true"`
-	AuthRedisUsername string `env:"AUTH_REDIS_USERNAME" required:"true"`
-	AuthRedisPassword string `env:"AUTH_REDIS_PASSWORD" required:"true"`
-	AuthRedisPrefix   string `env:"AUTH_REDIS_PREFIX" required:"true"`
-
 	CookieDomain string `env:"COOKIE_DOMAIN" required:"true"`
-	// StripePublicKey string `env:"STRIPE_PUBLIC_KEY" required:"true"`
-	// StripeSecretKey string `env:"STRIPE_SECRET_KEY" required:"true"`
 
 	IamGrpcAddr               string `env:"IAM_GRPC_ADDR" required:"true"`
 	CommsGrpcAddr             string `env:"COMMS_GRPC_ADDR" required:"true"`
