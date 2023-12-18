2837cc5add404dee2bc885eaf8b6b76f0e168e93
nxtcoder17 <nxtcoder17@gmail.com>
2023-12-18 14:17:07 +0530

    fix(infra-templates): label refactorings

--- infrastructure-as-code/infrastructure-templates/kl-target-cluster-aws/main.tf
+++ infrastructure-as-code/infrastructure-templates/kl-target-cluster-aws/main.tf
@@ -4,7 +4,7 @@ module "kl-master-nodes-on-aws" {
   enable_nvidia_gpu_support = var.enable_nvidia_gpu_support
   k3s_masters               = var.k3s_masters
   kloudlite_params          = var.kloudlite_params
-  tracker_id                = "${var.tracker_id}-master"
+  tracker_id                = "${var.tracker_id}-masters"
   save_kubeconfig_to_path   = var.save_kubeconfig_to_path
   save_ssh_key_to_path      = var.save_ssh_key_to_path
   extra_server_args         = var.extra_server_args
@@ -19,7 +19,7 @@ module "kl-worker-nodes-on-aws" {
   k3s_join_token             = module.kl-master-nodes-on-aws.k3s_agent_token
   k3s_server_public_dns_host = module.kl-master-nodes-on-aws.k3s_public_dns_host
   spot_nodepools             = var.spot_nodepools
-  tracker_id                 = "${var.tracker_id}-agent"
+  tracker_id                 = "${var.tracker_id}-workers"
   save_ssh_key_to_path       = var.save_worker_ssh_key_to_path
   extra_agent_args           = var.extra_agent_args
 }
