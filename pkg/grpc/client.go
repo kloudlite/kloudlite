cacac6bd44c90dbd64f72bb01a159a17edc1b0e2
Piyush Kumar <piyush@kloudlite.io>
2023-12-20 17:44:43 +0530

    Changes error return type statement for packages.

--- pkg/grpc/client.go
+++ pkg/grpc/client.go
@@ -1,6 +1,7 @@
 package grpc
 
 import (
+	"github.com/kloudlite/api/pkg/errors"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -21,7 +22,7 @@ func (g *grpcClient) Close() error {
 func NewGrpcClient(serverAddr string) (Client, error) {
 	conn, err := grpc.Dial(serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		return nil, err
+		return nil, errors.NewE(err)
 	}
 	return &grpcClient{ClientConn: conn}, nil
 }
