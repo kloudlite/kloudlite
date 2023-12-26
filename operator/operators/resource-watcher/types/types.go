71d78c254b6dea0178b8dabb9bf6d61b32189118
nxtcoder17 <nxtcoder17@gmail.com>
2023-12-26 16:38:19 +0530

    fix(resource-watcher): fixes message dispatching
    - adds resource status to every message dispatched through resource
      watcher

--- operator/operators/resource-watcher/types/types.go
+++ operator/operators/resource-watcher/types/types.go
@@ -1,8 +1,42 @@
 package types
 
+import (
+	"strings"
+
+	"github.com/kloudlite/operator/pkg/constants"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
 type ResourceUpdate struct {
 	AuthToken   string         `json:"authToken"`
 	AccountName string         `json:"accountName"`
 	ClusterName string         `json:"clusterName"`
 	Object      map[string]any `json:"object"`
+}
+
+type ResourceStatus string
+
+const (
+	ResourceStatusUpdated  ResourceStatus = "updated"
+	ResourceStatusDeleting ResourceStatus = "deleting"
+	ResourceStatusDeleted  ResourceStatus = "deleted"
+)
+
+const (
+	ResourceStatusKey string = "resource-watcher-resource-status"
+)
+
+func HasOtherKloudliteFinalizers(obj client.Object) bool {
+	hasOtherKloudliteFinalizers := false
+
+	for _, f := range obj.GetFinalizers() {
+		if strings.Contains(f, ".kloudlite.io") {
+			if f == constants.StatusWatcherFinalizer {
+				continue
+			}
+			hasOtherKloudliteFinalizers = true
+		}
+	}
+
+	return hasOtherKloudliteFinalizers
 }
