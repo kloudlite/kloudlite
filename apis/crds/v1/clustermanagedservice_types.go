e71357521f2c0c0ce8e8ad591589bf299522c770
nxtcoder17 <nxtcoder17@gmail.com>
2024-01-06 22:23:39 +0530

    feat(cluster-managed-service): adds cluster managed service
    - adds cluster managed service types and controller
    - updates project managed service types and controller

--- apis/crds/v1/clustermanagedservice_types.go
+++ apis/crds/v1/clustermanagedservice_types.go
@@ -7,56 +7,57 @@ import (
 )
 
 // ProjectManagedServiceSpec defines the desired state of ProjectManagedService
-type ProjectManagedServiceSpec struct {
+type ClusterManagedServiceSpec struct {
 	TargetNamespace string             `json:"targetNamespace"`
 	MSVCSepec       ManagedServiceSpec `json:"msvcSpec"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster
 // +kubebuilder:printcolumn:JSONPath=".metadata.annotations.kloudlite\\.io\\/service-gvk",name=Service GVK,type=string
 // +kubebuilder:printcolumn:JSONPath=".status.lastReconcileTime",name=Last_Reconciled_At,type=date
 // +kubebuilder:printcolumn:JSONPath=".metadata.annotations.kloudlite\\.io\\/resource\\.ready",name=Ready,type=string
 // +kubebuilder:printcolumn:JSONPath=".metadata.creationTimestamp",name=Age,type=date
 
-// ProjectManagedService is the Schema for the projectmanagedservices API
-type ProjectManagedService struct {
+// ClusterManagedService is the Schema for the clustermanagedservices API
+type ClusterManagedService struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   ProjectManagedServiceSpec `json:"spec,omitempty"`
+	Spec   ClusterManagedServiceSpec `json:"spec,omitempty"`
 	Status rApi.Status               `json:"status,omitempty" graphql:"noinput"`
 }
 
-func (m *ProjectManagedService) EnsureGVK() {
+func (m *ClusterManagedService) EnsureGVK() {
 	if m != nil {
-		m.SetGroupVersionKind(GroupVersion.WithKind("ProjectManagedService"))
+		m.SetGroupVersionKind(GroupVersion.WithKind("ClusterManagedService"))
 	}
 }
 
-func (m *ProjectManagedService) GetStatus() *rApi.Status {
+func (m *ClusterManagedService) GetStatus() *rApi.Status {
 	return &m.Status
 }
 
-func (m *ProjectManagedService) GetEnsuredLabels() map[string]string {
+func (m *ClusterManagedService) GetEnsuredLabels() map[string]string {
 	return map[string]string{
-		constants.ProjectManagedServiceNameKey: m.Name,
+		constants.ClusterManagedServiceNameKey: m.Name,
 	}
 }
 
-func (m *ProjectManagedService) GetEnsuredAnnotations() map[string]string {
+func (m *ClusterManagedService) GetEnsuredAnnotations() map[string]string {
 	return map[string]string{}
 }
 
 //+kubebuilder:object:root=true
 
-// ProjectManagedServiceList contains a list of ProjectManagedService
-type ProjectManagedServiceList struct {
+// ClusterManagedServiceList contains a list of ClusterManagedService
+type ClusterManagedServiceList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []ProjectManagedService `json:"items"`
+	Items           []ClusterManagedService `json:"items"`
 }
 
 func init() {
-	SchemeBuilder.Register(&ProjectManagedService{}, &ProjectManagedServiceList{})
+	SchemeBuilder.Register(&ClusterManagedService{}, &ClusterManagedServiceList{})
 }
