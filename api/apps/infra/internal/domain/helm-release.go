5aff9d5810ac5f085e10f48c216feba26b3d9827
Karthik Thirumalasetti <karthik@kloudlite.io>
2024-01-13 10:15:59 +0530

    fix(apps/infra): Tested Helmchart Entity flow (#195)

--- api/apps/infra/internal/domain/helm-release.go
+++ api/apps/infra/internal/domain/helm-release.go
@@ -2,12 +2,12 @@ package domain
 
 import (
 	"github.com/kloudlite/operator/operators/resource-watcher/types"
+	"time"
 
 	iamT "github.com/kloudlite/api/apps/iam/types"
 	"github.com/kloudlite/api/apps/infra/internal/entities"
 	"github.com/kloudlite/api/common"
 	"github.com/kloudlite/api/pkg/errors"
-	fn "github.com/kloudlite/api/pkg/functions"
 	"github.com/kloudlite/api/pkg/repos"
 	t "github.com/kloudlite/api/pkg/types"
 	corev1 "k8s.io/api/core/v1"
@@ -99,6 +99,7 @@ func (d *domain) CreateHelmRelease(ctx InfraContext, clusterName string, hr enti
 
 	hr.AccountName = ctx.AccountName
 	hr.ClusterName = clusterName
+	hr.SyncStatus = t.GenSyncStatus(t.SyncActionApply, hr.RecordVersion)
 
 	cms, err := d.helmReleaseRepo.Create(ctx, &hr)
 	if err != nil {
@@ -126,48 +127,49 @@ func (d *domain) CreateHelmRelease(ctx InfraContext, clusterName string, hr enti
 	return cms, nil
 }
 
-func (d *domain) UpdateHelmRelease(ctx InfraContext, clusterName string, hr entities.HelmRelease) (*entities.HelmRelease, error) {
+func (d *domain) UpdateHelmRelease(ctx InfraContext, clusterName string, hrIn entities.HelmRelease) (*entities.HelmRelease, error) {
 	if err := d.canPerformActionInAccount(ctx, iamT.UpdateHelmRelease); err != nil {
 		return nil, errors.NewE(err)
 	}
 
-	hr.EnsureGVK()
-	if err := d.k8sClient.ValidateObject(ctx, &hr); err != nil {
+	hrIn.EnsureGVK()
+	if err := d.k8sClient.ValidateObject(ctx, &hrIn); err != nil {
 		return nil, errors.NewE(err)
 	}
 
-	cms, err := d.findHelmRelease(ctx, clusterName, hr.Name)
+	cms, err := d.findHelmRelease(ctx, clusterName, hrIn.Name)
 	if err != nil {
 		return nil, errors.NewE(err)
 	}
 
 	if cms.IsMarkedForDeletion() {
-		return nil, errors.Newf("helm release with name %q is marked for deletion", hr.Name)
+		return nil, errors.Newf("helm release with name %q is marked for deletion", hrIn.Name)
 	}
 
-	cms.IncrementRecordVersion()
-	cms.LastUpdatedBy = common.CreatedOrUpdatedBy{
-		UserId:    ctx.UserId,
-		UserName:  ctx.UserName,
-		UserEmail: ctx.UserEmail,
-	}
-
-	cms.Labels = hr.Labels
-	cms.Annotations = hr.Annotations
-
-	cms.SyncStatus = t.GenSyncStatus(t.SyncActionApply, cms.RecordVersion)
-
-	unp, err := d.helmReleaseRepo.UpdateById(ctx, cms.Id, cms)
+	unp, err := d.helmReleaseRepo.PatchById(ctx, cms.Id, repos.Document{
+		"metadata.labels":      hrIn.Labels,
+		"metadata.annotations": hrIn.Annotations,
+		"displayName":		  hrIn.DisplayName,
+		"recordVersion":    hrIn.RecordVersion+1,
+		"spec.chartVersion":			 hrIn.Spec.ChartVersion,
+		"spec.values":			 hrIn.Spec.Values,
+		"lastUpdatedBy":common.CreatedOrUpdatedBy{
+			UserId:    ctx.UserId,
+			UserName:  ctx.UserName,
+			UserEmail: ctx.UserEmail,
+		},
+		"syncStatus.lastSyncedAt": time.Now(),
+		"syncStatus.action":        t.SyncActionApply,
+		"syncStatus.state":         t.SyncStateInQueue,
+	})
 	if err != nil {
 		return nil, errors.NewE(err)
 	}
 
 	d.resourceEventPublisher.PublishHelmReleaseEvent(unp, PublishUpdate)
-
 	if err := d.applyHelmRelease(ctx, unp); err != nil {
 		return nil, errors.NewE(err)
 	}
-
 	return unp, nil
 }
 
@@ -185,9 +187,17 @@ func (d *domain) DeleteHelmRelease(ctx InfraContext, clusterName string, name st
 		return errors.Newf("helm release with name %q is marked for deletion", name)
 	}
 
-	svc.MarkedForDeletion = fn.New(true)
-	svc.SyncStatus = t.GetSyncStatusForDeletion(svc.Generation)
-	upC, err := d.helmReleaseRepo.UpdateById(ctx, svc.Id, svc)
+	upC, err := d.helmReleaseRepo.PatchById(ctx, svc.Id, repos.Document{
+		"markedForDeletion": true,
+		"lastUpdatedBy":common.CreatedOrUpdatedBy{
+			UserId:    ctx.UserId,
+			UserName:  ctx.UserName,
+			UserEmail: ctx.UserEmail,
+		},
+		"syncStatus.lastSyncedAt": time.Now(),
+		"syncStatus.action":        t.SyncActionDelete,
+		"syncStatus.state":         t.SyncStateInQueue,
+	})
 	if err != nil {
 		return errors.NewE(err)
 	}
@@ -203,11 +213,11 @@ func (d *domain) OnHelmReleaseApplyError(ctx InfraContext, clusterName string, n
 		return errors.NewE(err)
 	}
 
-	svc.SyncStatus.State = t.SyncStateErroredAtAgent
-	svc.SyncStatus.LastSyncedAt = opts.MessageTimestamp
-	svc.SyncStatus.Error = &errMsg
-
-	_, err = d.helmReleaseRepo.UpdateById(ctx, svc.Id, svc)
+	_, err = d.helmReleaseRepo.PatchById(ctx, svc.Id, repos.Document{
+		"syncStatus.state":         t.SyncStateErroredAtAgent,
+		"syncStatus.lastSyncedAt": opts.MessageTimestamp,
+		"syncStatus.error":         &errMsg,
+	})
 	d.resourceEventPublisher.PublishHelmReleaseEvent(svc, PublishUpdate)
 	return errors.NewE(err)
 }
@@ -243,19 +253,28 @@ func (d *domain) OnHelmReleaseUpdateMessage(ctx InfraContext, clusterName string
 		return d.resyncToTargetCluster(ctx, xhr.SyncStatus.Action, clusterName, xhr, xhr.RecordVersion)
 	}
 
-	xhr.Status = hr.Status
+	// Ignore error if annotation don't have record version
+	annVersion, _ := d.parseRecordVersionFromAnnotations(hr.Annotations)
 
-	xhr.SyncStatus.State = func() t.SyncState {
-		if status == types.ResourceStatusDeleting {
-			return t.SyncStateDeletingAtAgent
-		}
-		return t.SyncStateUpdatedAtAgent
-	}()
-	xhr.SyncStatus.LastSyncedAt = opts.MessageTimestamp
-	xhr.SyncStatus.Error = nil
-	xhr.SyncStatus.RecordVersion = xhr.RecordVersion
-
-	if _, err := d.helmReleaseRepo.UpdateById(ctx, xhr.Id, xhr); err != nil {
+	if _, err := d.helmReleaseRepo.PatchById(ctx, xhr.Id, repos.Document{
+		"metadata.labels":      hr.Labels,
+		"metadata.annotations": hr.Annotations,
+		"metadata.generation":  hr.Generation,
+		"metadata.creationTimestamp":  hr.CreationTimestamp,
+		"status":      hr.Status,
+		"syncStatus":  t.SyncStatus{
+			LastSyncedAt: opts.MessageTimestamp,
+			Error: 	  nil,
+			Action:       t.SyncActionApply,
+			RecordVersion: annVersion,
+			State: func() t.SyncState {
+				if status == types.ResourceStatusDeleting {
+					return t.SyncStateDeletingAtAgent
+				}
+				return t.SyncStateUpdatedAtAgent
+			}(),
+		},
+	}); err != nil {
 		return errors.NewE(err)
 	}
 	d.resourceEventPublisher.PublishHelmReleaseEvent(xhr, PublishUpdate)
