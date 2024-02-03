10287f86e60051c551e1aad8a370cd13fe5321da
nxtcoder17 <nxtcoder17@gmail.com>
2024-02-03 19:42:35 +0530

    refactor(cloudflare/dns): module now accepts only an array of DNS records

--- terraform/modules/cloudflare/dns/main.tf
+++ terraform/modules/cloudflare/dns/main.tf
@@ -3,38 +3,15 @@ locals {
   dns_entry_comment = "managed by kloudlite's infrastructure-as-code. It should not be touched and replaced manually"
 }
 
-resource "cloudflare_record" "A_records" {
-  for_each        = var.A_records
+resource "cloudflare_record" "DNS_record" {
+  for_each        = { for idx, item in var.DNS_records : idx => item }
   zone_id         = var.cloudflare_zone_id
-  name            = each.value.value
-  value           = each.key
-  type            = "A"
+  name            = each.value.domain
+  value           = each.value.value
+  type            = each.value.record_type
   ttl             = each.value.ttl
   proxied         = var.use_cloudflare_proxy
   allow_overwrite = local.allow_overwrite
   comment         = local.dns_entry_comment
 }
 
-resource "cloudflare_record" "CNAME_records" {
-  for_each        = var.CNAME_records
-  zone_id         = var.cloudflare_zone_id
-  name            = each.key
-  value           = each.value.value
-  type            = "CNAME"
-  ttl             = each.value.ttl
-  proxied         = var.use_cloudflare_proxy
-  allow_overwrite = local.allow_overwrite
-  comment         = local.dns_entry_comment
-}
-
-resource "cloudflare_record" "TXT_records" {
-  for_each        = var.TXT_records
-  zone_id         = var.cloudflare_zone_id
-  name            = each.key
-  value           = each.value.value
-  type            = "TXT"
-  ttl             = each.value.ttl
-  proxied         = var.use_cloudflare_proxy
-  allow_overwrite = local.allow_overwrite
-  comment         = local.dns_entry_comment
-}
