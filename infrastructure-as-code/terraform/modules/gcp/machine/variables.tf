07a1410cba00e8689a2dd0de0654f19df0deb028
nxtcoder17 <nxtcoder17@gmail.com>
2024-03-07 13:29:07 +0530

    feat: google cloud integration, standard as well as spot nodepools

---
+++ infrastructure-as-code/terraform/modules/gcp/machine/variables.tf
@@ -0,0 +1,63 @@
+variable "name" {
+  type        = string
+  description = "instance name"
+}
+
+variable "availability_zone" {
+  type        = string
+  description = "Availability Zone"
+}
+
+variable "ssh_key" {
+  type        = string
+  description = "ssh keys"
+}
+
+variable "provision_mode" {
+  type = string
+  validation {
+    error_message = "must be one of STANDARD or SPOT"
+    condition     = contains(["STANDARD", "SPOT"], var.provision_mode)
+  }
+}
+
+variable "machine_type" {
+  type        = string
+  description = "machine type"
+}
+
+variable "startup_script" {
+  type        = string
+  description = "startup script"
+}
+
+variable "bootvolume_type" {
+  type        = string
+  description = "bootvolume type like pd-ssd,pd-balanced etc. Read more here https://cloud.google.com/compute/docs/disks"
+}
+
+variable "bootvolume_size" {
+  type        = number
+  description = "bootvolume size in GBs"
+}
+
+variable "additional_disk" {
+  type = map(object({
+    size = number
+    #    type = string pd-ssd
+  }))
+  default = null
+}
+
+variable "network" {
+  type        = string
+  description = "network name"
+  default     = "default"
+}
+
+variable "tags" {
+  type        = list(string)
+  description = "tags that should be present on resources"
+  default     = []
+}
+
