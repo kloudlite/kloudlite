09a9c3e906be47b85d522d2b084aaaac1fde4d93
nxtcoder17 <nxtcoder17@gmail.com>
2024-03-10 16:35:02 +0530

    feat: gcp and aws infrastructure template updates

---
+++ infrastructure-as-code/infrastructure-templates/gcp/worker-nodes/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.19.0"
+    }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.13.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project     = var.gcp_project_id
+  region      = var.gcp_region
+  credentials = base64decode(var.gcp_credentials_json)
+}
