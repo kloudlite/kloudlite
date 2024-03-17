84e6ecf11e3115b6162604126383fe2e62d83bbc
Abdhesh Nayak <abdheshnayak@gmail.com>
2024-03-17 11:50:37 +0530

    :bug: Fixed issue with reponame while creating build-integration

--- web/src/apps/console/routes/_main+/$account+/repo+/$repo+/new-build/route.tsx
+++ web/src/apps/console/routes/_main+/$account+/repo+/$repo+/new-build/route.tsx
@@ -116,7 +116,7 @@ const NewBuild = () => {
           if (e) {
             throw e[0];
           }
-          navigate(`/${account}/repo/${repoName}/builds`);
+          navigate(`/${account}/repo/${atob(repoName)}/builds`);
         } catch (err) {
           handleError(err);
         }
