8a0261f8edc138786c00924a3a26c8366fd14708
nxtcoder17 <nxtcoder17@gmail.com>
2024-04-01 09:54:29 +0530

    feat(observability): migrating from go-fiber to `net/http`
    - go fiber has bugs when doing streaming response, it never cancells
      that streaming response
    - net/http does the job

--- api/apps/observability/internal/framework/framework.go
+++ api/apps/observability/internal/framework/framework.go
@@ -3,12 +3,12 @@ package framework
 import (
 	"context"
 	"fmt"
+	"net/http"
 
 	"github.com/kloudlite/api/apps/observability/internal/app"
 	"github.com/kloudlite/api/apps/observability/internal/env"
 	"github.com/kloudlite/api/common"
 	"github.com/kloudlite/api/pkg/grpc"
-	httpServer "github.com/kloudlite/api/pkg/http-server"
 	"github.com/kloudlite/api/pkg/kv"
 	"github.com/kloudlite/api/pkg/logging"
 	"github.com/kloudlite/api/pkg/nats"
@@ -18,6 +18,10 @@ import (
 var Module = fx.Module("framework",
 	fx.Provide(func(ev *env.Env) (app.IAMGrpcClient, error) {
 		return grpc.NewGrpcClient(ev.IAMGrpcAddr)
+	}),
+
+	fx.Provide(func(ev *env.Env) (app.InfraClient, error) {
+		return grpc.NewGrpcClient(ev.InfraGrpcAddr)
 	}),
 
 	fx.Provide(func(ev *env.Env, logger logging.Logger) (*nats.Client, error) {
@@ -38,14 +42,6 @@ var Module = fx.Module("framework",
 		},
 	),
 
-	fx.Provide(func(logger logging.Logger, ev *env.Env) httpServer.Server {
-		return httpServer.NewServer(httpServer.ServerArgs{
-			Logger:           logger,
-			CorsAllowOrigins: &ev.HttpCorsOrigins,
-			IsDev:            ev.IsDev,
-		})
-	}),
-
 	fx.Provide(
 		func(ev *env.Env, jc *nats.JetstreamClient) (kv.Repo[*common.AuthSession], error) {
 			cxt := context.TODO()
@@ -55,13 +51,34 @@ var Module = fx.Module("framework",
 
 	app.Module,
 
-	fx.Invoke(func(lf fx.Lifecycle, server httpServer.Server, ev *env.Env) {
+	// fx.Provide(func(logger logging.Logger, ev *env.Env) httpServer.Server {
+	// 	return httpServer.NewServer(httpServer.ServerArgs{
+	// 		Logger:           logger,
+	// 		CorsAllowOrigins: &ev.HttpCorsOrigins,
+	// 		IsDev:            ev.IsDev,
+	// 	})
+	// }),
+	//
+	// fx.Invoke(func(lf fx.Lifecycle, server httpServer.Server, ev *env.Env) {
+	// 	lf.Append(fx.Hook{
+	// 		OnStart: func(context.Context) error {
+	// 			return server.Listen(fmt.Sprintf(":%d", ev.HttpPort))
+	// 		},
+	// 		OnStop: func(context.Context) error {
+	// 			return server.Close()
+	// 		},
+	// 	})
+	// }),
+
+	fx.Provide(func() *http.ServeMux {
+		return http.NewServeMux()
+	}),
+
+	fx.Invoke(func(lf fx.Lifecycle, ev *env.Env, mux *http.ServeMux) {
 		lf.Append(fx.Hook{
 			OnStart: func(context.Context) error {
-				return server.Listen(fmt.Sprintf(":%d", ev.HttpPort))
-			},
-			OnStop: func(context.Context) error {
-				return server.Close()
+				go http.ListenAndServe(fmt.Sprintf(":%d", ev.HttpPort), mux)
+				return nil
 			},
 		})
 	}),
