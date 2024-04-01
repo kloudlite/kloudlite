3b6c68fb6e9bf73355448f6b1a919c6c85ef709f
nxtcoder17 <nxtcoder17@gmail.com>
2024-04-01 10:04:23 +0530

    feat: websocket-server now stream logs from observability-api

---
+++ apps/websocket-server/internal/domain/observability-logs.go
@@ -0,0 +1,131 @@
+package domain
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	// iamT "github.com/kloudlite/api/apps/iam/types"
+	"github.com/kloudlite/api/apps/websocket-server/internal/domain/logs"
+	"github.com/kloudlite/api/apps/websocket-server/internal/domain/types"
+	"github.com/kloudlite/api/apps/websocket-server/internal/domain/utils"
+	"github.com/kloudlite/api/constants"
+)
+
+// {"type":"response","for":"logs","data":{"message":"2024/03/26 10:17:01 [notice] 30#30: exit","timestamp":"2024-03-26T10:17:01.019879803Z","podName":"test-app-85c4759979-q2g8b","containerName":"container-0"},"message":"","id":"world.dev-team-cluster.app-fkimv6rui7osxtwenjfo8vf4ruumz"}
+
+type LogSubscriptionCtx struct {
+	Context    context.Context
+	CancelFunc context.CancelFunc
+	Reader     io.Reader
+}
+
+func (d *domain) handleObservabilityLogsMsg(ctx types.Context, subscriptions map[string]LogSubscriptionCtx, msgAny map[string]any) error {
+	// log := d.logger
+
+	var msg logs.Message
+	b, err := json.Marshal(msgAny)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(b, &msg); err != nil {
+		return err
+	}
+
+	if msg.Id == "" {
+		msg.Id = "default"
+	}
+
+	// hash := logs.LogHash(msg.Spec, ctx.Session.UserId, msg.Id)
+	hash := "tmp"
+
+	switch msg.Event {
+	case logs.EventSubscribe:
+		{
+			// if err := d.checkAccountAccess(ctx.Context, msg.Spec.Account, ctx.Session.UserId, iamT.ReadLogs); err != nil {
+			// 	return err
+			// }
+
+			tpk := logs.LogSubsId(msg.Spec, d.env.LogsStreamName)
+			d.logger.Debugf("tpk: %s", tpk)
+
+			utils.WriteInfo(ctx, "subscribed to logs", msg.Id, types.ForLogs)
+
+			nctx, cf := context.WithCancel(context.TODO())
+
+			req, err := http.NewRequestWithContext(nctx, http.MethodGet, fmt.Sprintf("http://%s/observability/logs", d.env.ObservabilityApiAddr), nil)
+			if err != nil {
+				utils.WriteError(ctx, err, msg.Id, types.ForLogs)
+			}
+
+			req.AddCookie(&http.Cookie{
+				Name:  constants.CookieName,
+				Value: string(ctx.Session.Id),
+			})
+
+			req.AddCookie(&http.Cookie{Name: "kloudlite-account", Value: msg.Spec.Account})
+
+			qp := req.URL.Query()
+			qp.Add("tracking_id", msg.Spec.TrackingId)
+			qp.Add("cluster_name", msg.Spec.Cluster)
+			req.URL.RawQuery = qp.Encode()
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				utils.WriteError(ctx, err, msg.Id, types.ForLogs)
+				defer cf()
+				return err
+			}
+
+			resp.Close = true
+
+			subscriptions[hash] = LogSubscriptionCtx{
+				Context:    nctx,
+				CancelFunc: cf,
+				Reader:     resp.Body,
+			}
+
+			go func() {
+				defer resp.Body.Close()
+				for {
+					if err := nctx.Err(); err != nil {
+						fmt.Println("subscription cancelled")
+						return
+					}
+					<-time.After(1 * time.Second)
+				}
+			}()
+
+			go func() {
+				defer resp.Body.Close()
+				// defer subscriptions[hash].Close()
+				reader := bufio.NewReader(resp.Body)
+				for {
+					b2, err := reader.ReadBytes('\n')
+					if err != nil {
+						return
+						// return err
+					}
+					ctx.Write([]byte(types.CreateResponseJson(b2, msg.Id)))
+				}
+			}()
+		}
+
+	case logs.EventUnsubscribe:
+		{
+			utils.WriteInfo(ctx, "[logs] subscription cancelled for ", msg.Id, types.ForLogs)
+			if v, ok := subscriptions[hash]; ok {
+				v.CancelFunc()
+			}
+		}
+	default:
+		return fmt.Errorf("invalid event: %s", msg.Event)
+	}
+
+	return nil
+}
--- apps/websocket-server/internal/env/env.go
+++ apps/websocket-server/internal/env/env.go
@@ -17,18 +17,7 @@ type Env struct {
 
 	LogsStreamName string `env:"LOGS_STREAM_NAME" default:"logs"`
 
-	// HttpPort uint16 `env:"HTTP_PORT" required:"true"`
-	// HttpCors string `env:"CORS_ORIGINS" required:"false"`
-	// GrpcPort uint16 `env:"GRPC_PORT" required:"true"`
-	//
-	// DBName string `env:"MONGO_DB_NAME" required:"true"`
-	// DBUrl  string `env:"MONGO_URI" required:"true"`
-	//
-	//
-	// CommsGrpcAddr             string `env:"COMMS_GRPC_ADDR" required:"true"`
-	// ContainerRegistryGrpcAddr string `env:"CONTAINER_REGISTRY_GRPC_ADDR" required:"true"`
-	// ConsoleGrpcAddr           string `env:"CONSOLE_GRPC_ADDR" required:"true"`
-	// KubernetesApiProxy        string `env:"KUBERNETES_API_PROXY"`
+	ObservabilityApiAddr string `env:"OBSERVABILITY_API_ADDR" required:"true"`
 }
 
 func LoadEnv() (*Env, error) {
