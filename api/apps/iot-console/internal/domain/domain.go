31db56469796f96c376898fb23b2d504732cb281
Piyush Kumar <piyush.acet@gmail.com>
2024-04-02 19:03:03 +0530

    iot console service graphql api: wip

---
+++ api/apps/iot-console/internal/domain/domain.go
@@ -0,0 +1,61 @@
+package domain
+
+import (
+	"github.com/kloudlite/api/apps/iot-console/internal/entities"
+	"github.com/kloudlite/api/apps/iot-console/internal/env"
+	"github.com/kloudlite/api/pkg/k8s"
+	"github.com/kloudlite/api/pkg/kv"
+	"github.com/kloudlite/api/pkg/logging"
+	"github.com/kloudlite/api/pkg/repos"
+	"go.uber.org/fx"
+)
+
+type domain struct {
+	k8sClient k8s.Client
+	logger    logging.Logger
+
+	//producer MessageDispatcher
+	//
+	//iamClient   iam.IAMClient
+	//infraClient infra.InfraClient
+
+	iotProjectRepo         repos.DbRepo[*entities.IOTProject]
+	iotEnvironmentRepo     repos.DbRepo[*entities.IOTEnvironment]
+	iotDeploymentRepo      repos.DbRepo[*entities.IOTDeployment]
+	iotDeviceRepo          repos.DbRepo[*entities.IOTDevice]
+	iotDeviceBlueprintRepo repos.DbRepo[*entities.IOTDeviceBlueprint]
+	iotAppRepo             repos.DbRepo[*entities.IOTApp]
+
+	envVars *env.Env
+	//consoleCacheStore kv.BinaryDataRepo
+}
+
+type IOTConsoleCacheStore kv.BinaryDataRepo
+
+var Module = fx.Module("domain",
+	fx.Provide(func(
+		k8sClient k8s.Client,
+		logger logging.Logger,
+
+		iotProjectRepo repos.DbRepo[*entities.IOTProject],
+		iotEnvironmentRepo repos.DbRepo[*entities.IOTEnvironment],
+		iotDeploymentRepo repos.DbRepo[*entities.IOTDeployment],
+		iotDeviceRepo repos.DbRepo[*entities.IOTDevice],
+		iotDeviceBlueprintRepo repos.DbRepo[*entities.IOTDeviceBlueprint],
+		iotAppRepo repos.DbRepo[*entities.IOTApp],
+
+		ev *env.Env,
+	) Domain {
+		return &domain{
+			k8sClient:              k8sClient,
+			logger:                 logger,
+			iotProjectRepo:         iotProjectRepo,
+			iotEnvironmentRepo:     iotEnvironmentRepo,
+			iotDeploymentRepo:      iotDeploymentRepo,
+			iotDeviceRepo:          iotDeviceRepo,
+			iotDeviceBlueprintRepo: iotDeviceBlueprintRepo,
+			iotAppRepo:             iotAppRepo,
+			envVars:                ev,
+		}
+	}),
+)
