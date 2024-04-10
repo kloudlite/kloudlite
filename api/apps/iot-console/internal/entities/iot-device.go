d752e20f21030cc9f606d6252ed0752f9aca8f3d
Piyush Kumar <piyush.acet@gmail.com>
2024-04-10 18:57:45 +0530

    move device inside deployment

--- api/apps/iot-console/internal/entities/iot-device.go
+++ api/apps/iot-console/internal/entities/iot-device.go
@@ -10,16 +10,15 @@ type IOTDevice struct {
 	repos.BaseEntity        `json:",inline" graphql:"noinput"`
 	common.ResourceMetadata `json:",inline"`
 
-	Name                string `json:"name"`
-	AccountName         string `json:"accountName" graphql:"noinput"`
-	ProjectName         string `json:"projectName" graphql:"noinput"`
-	PublicKey           string `json:"publicKey"`
-	ServiceCIDR         string `json:"serviceCIDR"`
-	PodCIDR             string `json:"podCIDR"`
-	IP                  string `json:"ip"`
-	DeviceBlueprintName string `json:"deviceBlueprintName" graphql:"noinput"`
-	Deployment          string `json:"deployment"`
-	Version             string `json:"version"`
+	Name           string `json:"name"`
+	AccountName    string `json:"accountName" graphql:"noinput"`
+	ProjectName    string `json:"projectName" graphql:"noinput"`
+	PublicKey      string `json:"publicKey"`
+	ServiceCIDR    string `json:"serviceCIDR"`
+	PodCIDR        string `json:"podCIDR"`
+	IP             string `json:"ip"`
+	DeploymentName string `json:"deploymentName" graphql:"noinput"`
+	Version        string `json:"version"`
 }
 
 var IOTDeviceIndexes = []repos.IndexField{
