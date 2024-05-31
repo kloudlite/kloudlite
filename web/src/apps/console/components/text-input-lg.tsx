6af327f5bd19d4b08dce5b0b65260837ccf41f46
nxtcoder36 <mohit0698@gmail.com>
2024-05-31 09:41:32 +0530

    invite-code page updated with latest design

---
+++ web/src/apps/console/components/text-input-lg.tsx
@@ -0,0 +1,45 @@
+import { ChangeEventHandler } from 'react';
+import { ITextInput, TextInput } from '~/components/atoms/input';
+import { IconButton } from '~/components/atoms/button';
+import { ArrowRight } from '~/components/icons';
+
+const TextInputLg = ({
+  value,
+  onChange,
+  onEnter,
+  error,
+  ...props
+}: ITextInput & {
+  onEnter?: () => void;
+}) => {
+  return (
+    <div
+      style={{ background: 'linear-gradient(#93C5FD, #3B82F6)' }}
+      className="p-[2px] rounded-md"
+      id="join-waitlist"
+    >
+      <TextInput
+        value={value}
+        onChange={onChange}
+        placeholder="Enter Code"
+        size="xl"
+        className="!border-none"
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            onEnter?.();
+            // e.stopPropagation();
+            // e.preventDefault();
+          }
+        }}
+        suffix={<IconButton variant="outline" icon={<ArrowRight />} 
+        onClick={onEnter}
+        />}
+        focusRing={false}
+        textFieldClassName="!bodyLg"
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default TextInputLg;
--- web/src/apps/console/routes/invite-code.tsx
+++ web/src/apps/console/routes/invite-code.tsx
@@ -1,7 +1,6 @@
 import { redirect } from '@remix-run/node';
 import { useNavigate } from '@remix-run/react';
-import { Button } from '~/components/atoms/button';
-import { TextInput } from '~/components/atoms/input';
+import { useState } from 'react';
 import { BrandLogo } from '~/components/branding/brand-logo';
 import { toast } from '~/components/molecule/toast';
 import { useConsoleApi } from '~/console/server/gql/api-provider';
@@ -11,12 +10,15 @@ import Yup from '~/root/lib/server/helpers/yup';
 import { IRemixCtx } from '~/root/lib/types/common';
 import { handleError } from '~/root/lib/utils/common';
 import { GQLServerHandler } from '~/lib/server/gql/saved-queries';
+import { Badge } from '~/components/atoms/badge';
+import TextInputLg from '~/console/components/text-input-lg';
 
 const InviteCode = () => {
   const api = useConsoleApi();
   const navigate = useNavigate();
+  const [apiError, setApiError] = useState('');
 
-  const { values, handleChange, isLoading, errors, handleSubmit } = useForm({
+  const { values, errors, handleChange, submit, handleSubmit } = useForm({
     initialValues: {
       inviteCode: '',
     },
@@ -29,56 +31,49 @@ const InviteCode = () => {
           invitationCode: v.inviteCode,
         });
         if (errors) {
-          throw errors[0];
+          throw new Error(errors[0].message);
         }
-        toast.success('Invitation code verification successfull.');
+        toast.success('Invitation code verification successful.');
         navigate('/teams');
       } catch (err) {
-        handleError(err);
+        const errorMessage =
+          err instanceof Error
+            ? err.message
+            : 'An error occurred. Please try again.';
+        setApiError(errorMessage);
+        toast.error(errorMessage);
       }
     },
   });
 
   return (
-    <div className="flex flex-col items-center justify-center gap-7xl h-full">
+    <div className="flex flex-col items-center justify-center gap-4xl h-full max-w-[734px] m-auto">
       <BrandLogo detailed={false} size={100} />
-      <span className="heading2xl text-text-strong">
-        Validate Your Invite Code
+      <Badge type="neutral">
+        🔥 Amazing curated{' '}
+        <span className="bodyMd-semibold text-text-strong">Open-Source</span>{' '}
+        remote local envs
+      </Badge>
+      <span className="heading5xl-marketing text-text-strong">
+        Unlock early access now!
       </span>
-      <div className="bodyLg text-text-default text-center">
-        Thank you for sign up, we have received your request and we are on it.
-        <br />
-        Currently, You are on waiting list. Thanks for your patience.
-        <br />
-      </div>
-      <div className="bodyLg text-text-default text-center">
-        If, You have invite code please proceed. Thanks.
-      </div>
-
+      <span className="text-center bodyXl">
+        Don't miss the chance to try our product. Enter your referral code now
+        to move up the waitlist and secure early access!
+      </span>
       <form
         onSubmit={handleSubmit}
         className="w-full max-w-[500px] flex items-center"
       >
         <div className="flex flex-col gap-3xl w-full">
-          <TextInput
-            label="Invite Code"
-            size="lg"
-            placeholder="Invitation Code"
+          <TextInputLg
             value={values.inviteCode}
             onChange={handleChange('inviteCode')}
-            error={!!errors.inviteCode}
-            message={errors.inviteCode}
+            onEnter={submit}
+            
+            // error={!!(errors.inviteCode || apiError)}
+            // message={errors.inviteCode || apiError}
           />
-          <div className="flex flex-col items-center">
-            <Button
-              {...{
-                variant: 'primary',
-                content: 'Verify',
-                loading: isLoading,
-                type: 'submit',
-              }}
-            />
-          </div>
         </div>
       </form>
     </div>
