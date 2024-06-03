988c0bb085159af5a84f188ede0c960f3e926004
nxtcoder17 <nxtcoder17@gmail.com>
2024-06-03 17:06:20 +0530

    feat(networking): networkingv1.Gateway implementation
    - each pod created within kloudlite namespace, will be a peer to
      Gateway, which is why, we have a mutation webhook to track pod
      creation and deletion
    
    - each service created within kloudlite namespace, will now be resoved
      at the Gateway only, which is why, we have a mutation webhook to
      track service creation and deletion

---
+++ operators/networking/internal/cmd/ip-manager/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"net/http"
+
+	"github.com/charmbracelet/log"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	networkingv1 "github.com/kloudlite/operator/apis/networking/v1"
+	"github.com/kloudlite/operator/operators/networking/internal/cmd/ip-manager/env"
+	"github.com/kloudlite/operator/operators/networking/internal/cmd/ip-manager/manager"
+	"github.com/kloudlite/operator/pkg/kubectl"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func main() {
+	var addr string
+	flag.StringVar(&addr, "addr", ":8080", "--address <host>:<port>")
+	var isDev bool
+	flag.BoolVar(&isDev, "dev", false, "--dev")
+	flag.Parse()
+
+	ev, err := env.LoadEnv()
+	if err != nil {
+		panic(err)
+	}
+
+	ev.IsDev = isDev
+
+	restCfg, err := func() (*rest.Config, error) {
+		if isDev {
+			return &rest.Config{Host: "localhost:8080"}, nil
+		}
+		return rest.InClusterConfig()
+	}()
+	if err != nil {
+		panic(err)
+	}
+
+	scheme := runtime.NewScheme()
+	clientgoscheme.AddToScheme(scheme)
+	networkingv1.AddToScheme(scheme)
+
+	cli, err := client.New(restCfg, client.Options{
+		Scheme: scheme,
+		WarningHandler: client.WarningHandlerOptions{
+			SuppressWarnings:   true,
+			AllowDuplicateLogs: false,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	yamlClient, err := kubectl.NewYAMLClient(restCfg, kubectl.YAMLClientOpts{})
+	if err != nil {
+		panic(err)
+	}
+
+	manager, err := manager.NewManager(ev, yamlClient.Client(), cli)
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetReportTimestamp(false)
+
+	r := chi.NewRouter()
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "/healthz" {
+				next.ServeHTTP(w, r)
+				return
+			}
+			middleware.Logger(next).ServeHTTP(w, r)
+		})
+	})
+
+	r.Post("/pod", func(w http.ResponseWriter, r *http.Request) {
+		s, err := manager.RegisterPod(r.Context())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		b, err := json.Marshal(s)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(b)
+	})
+
+	r.Get("/pod/{pod_ip}", func(w http.ResponseWriter, r *http.Request) {
+		podIP := chi.URLParam(r, "pod_ip")
+		wgconfig, err := manager.GetWgConfigForPod(r.Context(), podIP)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(wgconfig)
+		// if err := manager.RestartWireguard(); err != nil {
+		// 	log.Error(err)
+		// }
+	})
+
+	r.Delete("/pod/{pb_ip}/{pb_uid}", func(w http.ResponseWriter, r *http.Request) {
+		pbIP, pbUID := chi.URLParam(r, "pb_ip"), chi.URLParam(r, "pb_uid")
+		if err := manager.DeregisterPod(r.Context(), pbIP, pbUID); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		// if err := manager.RestartWireguard(); err != nil {
+		// 	log.Error(err)
+		// }
+	})
+
+	r.Post("/service/{svc_namespace}/{svc_name}", func(w http.ResponseWriter, r *http.Request) {
+		result, err := manager.RegisterService(r.Context(), chi.URLParam(r, "svc_namespace"), chi.URLParam(r, "svc_name"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if err := json.NewEncoder(w).Encode(result); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	r.Put("/service/{svc_binding_name}", func(w http.ResponseWriter, r *http.Request) {
+		if err := manager.RegisterAndSyncNginxStreams(r.Context(), chi.URLParam(r, "svc_binding_name")); err != nil {
+			log.Error(err, "while", "registering and syncing nginx streams")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+
+	r.Delete("/service/{svc_binding_ip}/{svc_binding_uid}", func(w http.ResponseWriter, r *http.Request) {
+		if err := manager.DeregisterService(r.Context(), chi.URLParam(r, "svc_binding_ip"), chi.URLParam(r, "svc_binding_uid")); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	log.Infof("Starting server on %s", addr)
+	if err := http.ListenAndServe(addr, r); err != nil {
+		log.Fatal(err)
+	}
+}
