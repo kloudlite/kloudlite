f06061b6dd1a4345ba0d885dc22da3a85ec01be1
nxtcoder17 <nxtcoder17@gmail.com>
2024-06-06 19:21:20 +0530

    feat(service-binding-controller): now updates service DNS hostnames to gateway DNS api

--- operators/networking/internal/cmd/service-binding-controller/env/env.go
+++ operators/networking/internal/cmd/service-binding-controller/env/env.go
@@ -5,6 +5,8 @@ import "github.com/codingconcepts/env"
 type Env struct {
 	MaxConcurrentReconciles int    `env:"MAX_CONCURRENT_RECONCILES" default:"1"`
 	GatewayAdminApiAddr     string `env:"GATEWAY_ADMIN_API_ADDR" required:"true"`
+	ServiceDNSHttpAddr      string `env:"SERVICE_DNS_HTTP_ADDR" required:"true"`
+	GatewayDNSSuffix        string `env:"GATEWAY_DNS_SUFFIX" required:"true"`
 }
 
 func LoadEnv() (*Env, error) {
