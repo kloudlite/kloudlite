ddee269b0928ecc0345630a493c82c40cbbb3069
nxtcoder17 <nxtcoder17@gmail.com>
2024-06-10 20:18:30 +0530

    feat(msvc): CRDS cleanup

--- operators/msvc-n-mres/controller/register.go
+++ operators/msvc-n-mres/controller/register.go
@@ -2,14 +2,15 @@ package controller
 
 import (
 	crdsv1 "github.com/kloudlite/operator/apis/crds/v1"
-	elasticsearchMsvcv1 "github.com/kloudlite/operator/apis/elasticsearch.msvc/v1"
-	influxdbMsvcv1 "github.com/kloudlite/operator/apis/influxdb.msvc/v1"
 	mongodbMsvcv1 "github.com/kloudlite/operator/apis/mongodb.msvc/v1"
-	mysqlMsvcv1 "github.com/kloudlite/operator/apis/mysql.msvc/v1"
-	neo4jMsvcv1 "github.com/kloudlite/operator/apis/neo4j.msvc/v1"
-	redisMsvcv1 "github.com/kloudlite/operator/apis/redis.msvc/v1"
-	redpandaMsvcv1 "github.com/kloudlite/operator/apis/redpanda.msvc/v1"
-	zookeeperMsvcv1 "github.com/kloudlite/operator/apis/zookeeper.msvc/v1"
+
+	// elasticsearchMsvcv1 "github.com/kloudlite/operator/apis/elasticsearch.msvc/v1"
+	// influxdbMsvcv1 "github.com/kloudlite/operator/apis/influxdb.msvc/v1"
+	// mysqlMsvcv1 "github.com/kloudlite/operator/apis/mysql.msvc/v1"
+	// neo4jMsvcv1 "github.com/kloudlite/operator/apis/neo4j.msvc/v1"
+	// redisMsvcv1 "github.com/kloudlite/operator/apis/redis.msvc/v1"
+	// redpandaMsvcv1 "github.com/kloudlite/operator/apis/redpanda.msvc/v1"
+	// zookeeperMsvcv1 "github.com/kloudlite/operator/apis/zookeeper.msvc/v1"
 	"github.com/kloudlite/operator/operator"
 	cmsvc "github.com/kloudlite/operator/operators/msvc-n-mres/internal/cluster-msvc"
 	"github.com/kloudlite/operator/operators/msvc-n-mres/internal/env"
@@ -23,13 +24,14 @@ func RegisterInto(mgr operator.Operator) {
 	mgr.AddToSchemes(
 		crdsv1.AddToScheme,
 		mongodbMsvcv1.AddToScheme,
-		mysqlMsvcv1.AddToScheme,
-		redisMsvcv1.AddToScheme,
-		elasticsearchMsvcv1.AddToScheme,
-		influxdbMsvcv1.AddToScheme,
-		redpandaMsvcv1.AddToScheme,
-		zookeeperMsvcv1.AddToScheme,
-		neo4jMsvcv1.AddToScheme,
+
+		// mysqlMsvcv1.AddToScheme,
+		// redisMsvcv1.AddToScheme,
+		// elasticsearchMsvcv1.AddToScheme,
+		// influxdbMsvcv1.AddToScheme,
+		// redpandaMsvcv1.AddToScheme,
+		// zookeeperMsvcv1.AddToScheme,
+		// neo4jMsvcv1.AddToScheme,
 	)
 	mgr.RegisterControllers(
 		&cmsvc.Reconciler{Name: "cluster-msvc", Env: ev},
