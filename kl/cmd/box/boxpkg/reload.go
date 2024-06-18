b3a9be01dcf6a44cd5918a7368e9933b56aa8b5d
nxtcoder36 <mohit0698@gmail.com>
2024-06-18 17:45:54 +0530

    box reload command will work outside the box as well

--- kl/cmd/box/boxpkg/reload.go
+++ kl/cmd/box/boxpkg/reload.go
@@ -2,12 +2,10 @@ package boxpkg
 
 import (
 	"github.com/kloudlite/kl/domain/server"
-
-	"github.com/kloudlite/kl/pkg/ui/spinner"
 )
 
 func (c *client) Reload() error {
-	defer spinner.Client.Start("Reloading environments please wait")()
+	//defer spinner.Client.Start("Reloading environments please wait")()
 
 	if err := server.SyncBoxHash(); err != nil {
 		return err
