91f014794f70efff37098024c1cc59b4e20918b8
nxtcoder17 <nxtcoder17@gmail.com>
2024-07-25 15:40:38 +0530

    chore: excluding /_healthy from fiber logs
    - adds option to set SlogLogger as a global slog logger
    - grpc client auto-reconnect updates

--- pkg/logging/slog-logger.go
+++ pkg/logging/slog-logger.go
@@ -16,6 +16,8 @@ type SlogOptions struct {
 	ShowTimestamp bool
 	ShowCaller    bool
 	ShowDebugLogs bool
+
+	SetAsDefaultLogger bool
 }
 
 func NewSlogLogger(opts SlogOptions) *slog.Logger {
@@ -28,15 +30,26 @@ func NewSlogLogger(opts SlogOptions) *slog.Logger {
 		level = log.DebugLevel
 	}
 
-	logger := log.NewWithOptions(opts.Writer, log.Options{ReportCaller: opts.ShowCaller, ReportTimestamp: opts.ShowTimestamp, Prefix: opts.Prefix, Level: level})
+	logger := log.NewWithOptions(opts.Writer, log.Options{
+		ReportCaller:    opts.ShowCaller,
+		ReportTimestamp: opts.ShowTimestamp,
+		Prefix:          opts.Prefix,
+		Level:           level,
+	})
 
 	styles := log.DefaultStyles()
 	styles.Levels[log.DebugLevel] = styles.Levels[log.DebugLevel].Foreground(lipgloss.Color("#5b717f"))
+	styles.Levels[log.InfoLevel] = styles.Levels[log.InfoLevel].Foreground(lipgloss.Color("#36cbfa"))
 
-	// styles.Key = lipgloss.NewStyle().Background(lipgloss.Color("#083e54")).Foreground(lipgloss.Color("#9dbdc9")).Bold(true)
 	styles.Key = lipgloss.NewStyle().Foreground(lipgloss.Color("#36cbfa")).Bold(true)
 
 	logger.SetStyles(styles)
 
-	return slog.New(logger)
+	l := slog.New(logger)
+
+	if opts.SetAsDefaultLogger {
+		slog.SetDefault(l)
+	}
+
+	return l
 }
