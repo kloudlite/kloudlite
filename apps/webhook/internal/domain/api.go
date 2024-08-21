ebd6b6dd5c1e2e2687cd469457d45f2f82f28822
Piyush Kumar <piyush.acet@gmail.com>
2024-08-21 16:58:57 +0530

    modified email template for contact us

--- apps/webhook/internal/domain/api.go
+++ apps/webhook/internal/domain/api.go
@@ -7,7 +7,7 @@ import (
 type ContactUsData struct {
 	Name         string `json:"name"`
 	Email        string `json:"email"`
-	MobileNumber int64  `json:"mobileNumber"`
+	MobileNumber string `json:"mobileNumber"`
 	CompanyName  string `json:"CompanyName"`
 	Country      string `json:"country"`
 	Message      string `json:"message"`
