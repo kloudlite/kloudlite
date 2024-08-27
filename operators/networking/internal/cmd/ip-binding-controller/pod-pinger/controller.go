d179324d011eeb4e2d07c3d39caf9d48369818cc
nxtcoder17 <nxtcoder17@gmail.com>
2024-08-28 00:22:11 +0530

    fix: improves pod-pinger logging, and better service de-registration

--- operators/networking/internal/cmd/ip-binding-controller/pod-pinger/controller.go
+++ operators/networking/internal/cmd/ip-binding-controller/pod-pinger/controller.go
@@ -3,6 +3,9 @@ package pod_pinger
 import (
 	"context"
 	"fmt"
+	"log/slog"
+	"os"
+	"strings"
 	"time"
 
 	networkingv1 "github.com/kloudlite/operator/apis/networking/v1"
@@ -12,6 +15,7 @@ import (
 	"github.com/kloudlite/operator/pkg/kubectl"
 	"github.com/kloudlite/operator/pkg/logging"
 	rApi "github.com/kloudlite/operator/pkg/operator"
+	probing "github.com/prometheus-community/pro-bing"
 	corev1 "k8s.io/api/core/v1"
 	apiLabels "k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -20,15 +24,13 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
-	probing "github.com/prometheus-community/pro-bing"
 )
 
 type Reconciler struct {
 	client.Client
 	Scheme     *runtime.Scheme
 	Env        *env.Env
-	logger     logging.Logger
+	logger     *slog.Logger
 	Name       string
 	yamlClient kubectl.YAMLClient
 }
@@ -49,9 +51,11 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
+	r.logger.Debug("[start] reconciling", "pod", fmt.Sprintf("%s/%s", pod.GetNamespace(), pod.GetName()))
+
 	v, ok := pod.GetLabels()[constants.KloudliteGatewayEnabledLabel]
 	if !ok {
-		r.logger.Infof("pod %s/%s is not registered with gateway, deleting it", pod.GetNamespace(), pod.GetName())
+		r.logger.Info("pod not registered with gateway, deleting it", "pod", fmt.Sprintf("%s/%s", pod.GetNamespace(), pod.GetName()))
 		if err := r.Delete(ctx, pod); err != nil {
 			return ctrl.Result{}, err
 		}
@@ -78,9 +82,21 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 		return ctrl.Result{}, nil
 	}
 
+	// if out, err := exec.CommandContext(ctx, "timeout", "1", "ping", "-c", "1", pblist.Items[0].Spec.GlobalIP).CombinedOutput(); err != nil {
+	// 	r.logger.Errorf(err, "failed to ping %s (%s/%s)", pblist.Items[0].Spec.GlobalIP, pod.GetNamespace(), pod.GetName())
+	// 	r.logger.Infof("output of ping: %s", out)
+	// 	if _, ok := pod.Labels[KloudlitePodActiveLabel]; !ok {
+	// 		return ctrl.Result{RequeueAfter: 1 * time.Second}, nil
+	// 	}
+	// 	if err := r.Delete(ctx, pod); err != nil {
+	// 		return ctrl.Result{}, err
+	// 	}
+	// 	return ctrl.Result{}, err
+	// }
+
 	pinger, err := probing.NewPinger(pblist.Items[0].Spec.GlobalIP)
 	if err != nil {
-		r.logger.Errorf(err, "failed to create pinger for %s", v)
+		r.logger.Error("failed to create pinger, got", "err", err, "pod", fmt.Sprintf("%s/%s", pod.GetNamespace(), pod.GetName()))
 		return ctrl.Result{}, err
 	}
 
@@ -88,7 +104,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 	pinger.Timeout = 500 * time.Millisecond
 
 	if err = pinger.RunWithContext(ctx); err != nil {
-		r.logger.Errorf(err, "failed to ping %s", v)
+		r.logger.Error("failed to ping, got", "err", err, "pod", fmt.Sprintf("%s/%s", pod.GetNamespace(), pod.GetName()))
 		if _, ok := pod.Labels[KloudlitePodActiveLabel]; !ok {
 			return ctrl.Result{RequeueAfter: 1 * time.Second}, nil
 		}
@@ -101,20 +117,24 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 	if _, ok := pod.Labels[KloudlitePodActiveLabel]; !ok {
 		fn.MapSet(&pod.Labels, KloudlitePodActiveLabel, "true")
 		if err := r.Update(ctx, pod); err != nil {
-			r.logger.Errorf(err, "failed to update pod %s/%s", pod.GetNamespace(), pod.GetName())
+			r.logger.Error("failed to update pod, got", "err", err, "pod", fmt.Sprintf("%s/%s", pod.GetNamespace(), pod.GetName()))
 			return ctrl.Result{}, err
 		}
 	}
 
-	r.logger.Debugf("ping success for %s, requeing after 5s", v)
+	r.logger.Debug("ping success, requeing after 5s", "pod", fmt.Sprintf("%s/%s", pod.GetNamespace(), pod.GetName()))
 	return ctrl.Result{RequeueAfter: 5 * time.Second}, nil
 }
 
 func (r *Reconciler) SetupWithManager(mgr ctrl.Manager, logger logging.Logger) error {
 	r.Client = mgr.GetClient()
 	r.Scheme = mgr.GetScheme()
-	r.logger = logger.WithName(r.Name)
-	r.yamlClient = kubectl.NewYAMLClientOrDie(mgr.GetConfig(), kubectl.YAMLClientOpts{Logger: r.logger})
+	r.logger = logging.NewSlogLogger(logging.SlogOptions{
+		Prefix:        r.Name,
+		ShowCaller:    true,
+		ShowDebugLogs: strings.ToLower(os.Getenv("DEBUG_LOG")) == "true",
+	})
+	r.yamlClient = kubectl.NewYAMLClientOrDie(mgr.GetConfig(), kubectl.YAMLClientOpts{Logger: logger.WithName(r.Name)})
 
 	builder := ctrl.NewControllerManagedBy(mgr).For(&networkingv1.PodBinding{})
 	builder.Watches(&corev1.Pod{}, handler.EnqueueRequestsFromMapFunc(func(ctx context.Context, obj client.Object) []reconcile.Request {
