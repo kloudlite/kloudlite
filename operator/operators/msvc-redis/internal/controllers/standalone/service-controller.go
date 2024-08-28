1c98aa5a849918a36e6e67e6701ca957fdad54ab
nxtcoder17 <nxtcoder17@gmail.com>
2024-08-28 15:32:02 +0530

    fix(msvc-services): fixes updates to forbidden fields in statefulsets

--- operator/operators/msvc-redis/internal/controllers/standalone/service-controller.go
+++ operator/operators/msvc-redis/internal/controllers/standalone/service-controller.go
@@ -288,7 +288,7 @@ func (r *ServiceReconciler) createStatefulSet(req *rApi.Request[*redisMsvcv1.Sta
 			fn.MapSet(&sts.Labels, k, v)
 		}
 
-		sts.Spec = appsv1.StatefulSetSpec{
+		spec := appsv1.StatefulSetSpec{
 			Replicas: fn.New(int32(1)),
 			Selector: &metav1.LabelSelector{
 				MatchLabels: selectorLabels,
@@ -341,6 +341,16 @@ func (r *ServiceReconciler) createStatefulSet(req *rApi.Request[*redisMsvcv1.Sta
 					},
 				},
 			},
+		}
+
+		if obj.GetGeneration() > 0 {
+			// resource exists, and is being updated now
+			// INFO: k8s statefulsets forbids update to spec fields, other than "replicas", "template", "ordinals", "updateStrategy",  "persistentVolumeClaimRetentionPolicy" and "minReadySeconds",
+
+			sts.Spec.Replicas = spec.Replicas
+			sts.Spec.Template = spec.Template
+		} else {
+			sts.Spec = spec
 		}
 		return nil
 	}); err != nil {
