7f6bb41afd816fc717054e1ef0504ddacb25a054
nxtcoder36 <mohit0698@gmail.com>
2024-09-03 16:31:14 +0530

    image url api changes

--- apps/console/internal/env/env.go
+++ apps/console/internal/env/env.go
@@ -22,6 +22,7 @@ type Env struct {
 	NatsReceiveFromAgentStream string `env:"NATS_RECEIVE_FROM_AGENT_STREAM" required:"true"`
 	EventsNatsStream           string `env:"EVENTS_NATS_STREAM" required:"true"`
 	WebhookTokenHashingSecret  string `env:"WEBHOOK_TOKEN_HASHING_SECRET" required:"true"`
+	WebhookURL                 string `env:"WEBHOOK_URL" required:"true"`
 
 	IAMGrpcAddr   string `env:"IAM_GRPC_ADDR" required:"true"`
 	InfraGrpcAddr string `env:"INFRA_GRPC_ADDR" required:"true"`
