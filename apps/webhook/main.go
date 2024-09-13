3bf116c05a4395554a51f34e2ccb892a020a7ad8
nxtcoder17 <nxtcoder17@gmail.com>
2024-09-13 17:25:00 +0530

    chore(apps/webhook): startup message updates

--- apps/webhook/main.go
+++ apps/webhook/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log/slog"
+	"os"
+	"time"
 
 	"github.com/kloudlite/api/apps/webhook/internal/env"
 	"github.com/kloudlite/api/apps/webhook/internal/framework"
+	"github.com/kloudlite/api/common"
 	"github.com/kloudlite/api/pkg/config"
 	fn "github.com/kloudlite/api/pkg/functions"
 	"github.com/kloudlite/api/pkg/logging"
@@ -17,7 +21,13 @@ func main() {
 	flag.BoolVar(&isDev, "dev", false, "--dev")
 	flag.Parse()
 
-	fx.New(
+	start := time.Now()
+	common.PrintBuildInfo()
+
+	logger := logging.NewSlogLogger(logging.SlogOptions{ShowCaller: true, ShowDebugLogs: isDev, SetAsDefaultLogger: true})
+
+	app := fx.New(
+		fx.NopLogger,
 		fx.Provide(
 			func() (logging.Logger, error) {
 				return logging.New(&logging.Options{Name: "webhooks", Dev: isDev})
@@ -35,5 +45,21 @@ func main() {
 		fn.FxErrorHandler(),
 		config.EnvFx[env.Env](),
 		framework.Module,
-	).Run()
+	)
+
+	ctx, cancel := func() (context.Context, context.CancelFunc) {
+		if isDev {
+			return context.WithTimeout(context.TODO(), 5*time.Second)
+		}
+		return context.WithTimeout(context.Background(), 2*time.Second)
+	}()
+	defer cancel()
+
+	if err := app.Start(ctx); err != nil {
+		logger.Error("failed to start webhooks-api, got", slog.String("err", err.Error()))
+		os.Exit(1)
+	}
+
+	common.PrintReadyBanner2(time.Since(start))
+	<-app.Done()
 }
