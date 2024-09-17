917c6abd7e01cc0bb8da10cd5dcc68a97b17744e
Piyush Kumar <piyush.acet@gmail.com>
2024-09-17 13:26:12 +0530

    adding registration ui

---
+++ web/src/apps/webinar/src/app/components/container.tsx
@@ -0,0 +1,26 @@
+import { cn } from 'kl-design-system/utils';
+import React, { ReactNode } from 'react';
+import Header from './header';
+
+interface IContainer {
+    children: ReactNode;
+    headerExtra?: ReactNode;
+}
+
+const Container = ({ children, headerExtra }: IContainer) => {
+    return (
+        <div className="flex flex-col h-full">
+            <Header headerExtra={headerExtra} />
+            <div
+                className={cn(
+                    'flex flex-1 flex-col md:items-center self-stretch justify-center px-3xl py-5xl md:py-9xl'
+                )}
+            >
+                {children}
+            </div>
+            {/* <Footer /> */}
+        </div>
+    );
+};
+
+export default Container;
--- web/src/apps/webinar/src/app/layout.tsx
+++ web/src/apps/webinar/src/app/layout.tsx
@@ -17,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={inter.className}>{children}</body> */}
+      
+      <body className={inter.className}>
+        <main>{children}</main>
+      </body>
+
     </html>
   );
 }
