bf99a5df6f5f218062d8e3763da1fe534df3f570
nxtcoder17 <nxtcoder17@gmail.com>
2024-09-17 18:10:01 +0530

    chore: removes nodepool, and networking from platform operator
    - nodepool's will now have their own controller in cmd/nodepool-operator
    - same should follow for networking

--- cmd/platform-operator/main.go
+++ cmd/platform-operator/main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"github.com/kloudlite/operator/operator"
 	app "github.com/kloudlite/operator/operators/app-n-lambda/controller"
+
 	// clusters "github.com/kloudlite/operator/operators/clusters/controller"
 	helmCharts "github.com/kloudlite/operator/operators/helm-charts/controller"
 	lifecycle "github.com/kloudlite/operator/operators/lifecycle/controller"
 	msvcAndMres "github.com/kloudlite/operator/operators/msvc-n-mres/controller"
+
 	// msvcRedis "github.com/kloudlite/operator/operators/msvc-redis/controller"
-	networkingv1 "github.com/kloudlite/operator/operators/networking/register"
-	nodepool "github.com/kloudlite/operator/operators/nodepool/controller"
+
+	// networkingv1 "github.com/kloudlite/operator/operators/networking/register"
+	// nodepool "github.com/kloudlite/operator/operators/nodepool/controller"
 	project "github.com/kloudlite/operator/operators/project/controller"
 	routers "github.com/kloudlite/operator/operators/routers/controller"
+
 	// virtualMachine "github.com/kloudlite/operator/operators/virtual-machine/registration"
-	wireguard "github.com/kloudlite/operator/operators/wireguard/controller"
+
+	// wireguard "github.com/kloudlite/operator/operators/wireguard/controller"
 
 	msvcMongo "github.com/kloudlite/operator/operators/msvc-mongo/controller"
 	msvcMysql "github.com/kloudlite/operator/operators/msvc-mysql/controller"
@@ -38,11 +43,11 @@ func main() {
 	lifecycle.RegisterInto(mgr)
 
 	// clusters.RegisterInto(mgr)
-	nodepool.RegisterInto(mgr)
+	// nodepool.RegisterInto(mgr) // MIGRATE
 	// virtualMachine.RegisterInto(mgr)
 
-	wireguard.RegisterInto(mgr)
-	networkingv1.RegisterInto(mgr)
+	// wireguard.RegisterInto(mgr)    // MIGRATE
+	// networkingv1.RegisterInto(mgr) // MIGRATE
 
 	mgr.Start()
 }
