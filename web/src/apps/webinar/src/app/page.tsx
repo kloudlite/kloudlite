aa3c7b179dd05f33a79dbdeda7d21ee35f9c786e
Bikash <tulsiojhaamg@gmail.com>
2024-09-19 19:10:22 +0545

    updated env access in dev

--- web/src/apps/webinar/src/app/page.tsx
+++ web/src/apps/webinar/src/app/page.tsx
@@ -1,20 +1,12 @@
 import axios from 'axios';
 //@ts-ignore
 import { cookies } from 'next/headers';
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation';
 import { WebinarUI } from './orgs/webinar-ui';
 
-type EnvVars = {
-  dyteOrgId: string,
-  dyteApiKey: string,
-  dyteMeetingId: string,
-  marketApiUrl: string,
-}
-
 export default async function Home() {
-
   // console.log("all-env", process.env)
-  const cookie = cookies().get("hotspot-session")
+  const cookie = cookies().get('hotspot-session');
   const callbackUrl = process.env.CALLBACK_URL;
   const redirectUrl = `${process.env.REDIRECT_URL}?callback=${callbackUrl}`;
   const token = btoa(`${process.env.DYTE_ORG_ID}:${process.env.DYTE_API_KEY}`);
@@ -34,37 +26,30 @@ export default async function Home() {
       },
     });
 
-    const { data: { success, data } } = await axios.get(
+    const {
+      data: { success, data },
+    } = await axios.get(
       `https://api.dyte.io/v2/meetings/${process.env.DYTE_MEETING_ID}`,
       {
         headers: {
           Authorization: `Basic ${token}`,
-        }
-      }
-    )
+        },
+      },
+    );
     if (!success) {
       throw new Error('Failed to get meeting details');
     }
 
     const userDetails = res.data.data;
     if (userDetails) {
-
-      const envVars: EnvVars = {
-        dyteOrgId: process.env.DYTE_ORG_ID as string,
-        dyteApiKey: process.env.DYTE_API_KEY as string,
-        dyteMeetingId: process.env.DYTE_MEETING_ID as string,
-        marketApiUrl: process.env.MARKETING_API_URL as string,
-      }
-
       return (
-        <main className='flex flex-col h-full'>
-          <WebinarUI userDetails={userDetails} meetingStatus={data.status} envVars={envVars} />
-        </main >
+        <main className="flex flex-col h-full">
+          <WebinarUI userDetails={userDetails} meetingStatus={data.status} />
+        </main>
       );
     }
     redirect(redirectUrl);
   } catch (e) {
     redirect(redirectUrl);
   }
-
 }
