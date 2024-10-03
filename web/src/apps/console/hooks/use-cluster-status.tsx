082550ef383239cd6623be7cc82e7d96b4178568
Abdhesh Nayak <abdheshnayak@gmail.com>
2024-10-03 18:18:57 +0530

    :zap: Improved cluster-status

--- web/src/apps/console/hooks/use-cluster-status.tsx
+++ web/src/apps/console/hooks/use-cluster-status.tsx
@@ -2,6 +2,20 @@ import { useCallback, useEffect, useState } from 'react';
 import { useConsoleApi } from '../server/gql/api-provider';
 import { parseNodes } from '../server/r-utils/common';
 
+export const findClusterStatusv3 = (onlineAt?: string): boolean => {
+  if (!onlineAt || !onlineAt) {
+    return false;
+  }
+
+  const lastTime = new Date(onlineAt);
+  const currentTime = new Date();
+
+  const timeDifference =
+    (currentTime.getTime() - lastTime.getTime()) / (1000 * 60);
+
+  return timeDifference <= 1;
+};
+
 export const findClusterStatus = (item?: {
   lastOnlineAt?: string;
 }): boolean => {
