1d4acee6a829a72c923186fe5d6aaa7987669157
nxtcoder36 <mohit0698@gmail.com>
2024-10-11 16:23:12 +0530

    base url updated

--- kl/k3s/impl.go
+++ kl/k3s/impl.go
@@ -275,7 +275,7 @@ func (c *client) generateConnectionScript(clusterConfig *fileclient.TeamClusterC
 		ClusterCIDR:         clusterConfig.ClusterCIDR,
 		IpAddress:           vpnTeamConfig.IpAddress,
 		K3sTrackerImageName: constants.GetK3sTrackerImageName(),
-		BaseURL:             constants.GetDefaultBaseURL(),
+		BaseURL:             constants.BaseURL,
 	}
 
 	b := new(bytes.Buffer)
