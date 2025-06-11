5a21a529e2a4d3108abd37d11bcec54e889c7117
Karthik Th <karthik@kloudlite.io>
2025-06-11 09:27:30 +0530

    - (grpc-interfaces) added signup - (grpc-interfaces) updated protoc version. - (web) implemented signup and login - (api/auth) implemented signup in grpc

--- web/src/actions/auth.ts
+++ web/src/actions/auth.ts
@@ -1,9 +1,10 @@
 "use server";
 
-import { AuthV2Client, LoginRequest, LoginResponse } from "@grpc/auth.v2";
+import { AuthV2Client, LoginRequest, LoginResponse, SignupRequest, SignupResponse } from "@grpc/auth.v2";
 import util from "util"
 import * as grpc from "@grpc/grpc-js";
 import { promiseWrap } from "./grpc-wrapper";
+import { cookies } from "next/headers";
 
 const AUTHV2_SERVER_ADDRESS = process.env.AUTHV2_SERVER_ADDRESS || "localhost:50051";
 
@@ -13,16 +14,37 @@ const AuthClient = new AuthV2Client(
 );
 
 const serverMethods = {
-  login: promiseWrap<LoginRequest, LoginResponse>(AuthClient.login.bind(AuthClient))
+  login: promiseWrap<LoginRequest, LoginResponse>(AuthClient.login.bind(AuthClient)),
+  signup: promiseWrap<SignupRequest, SignupResponse>(AuthClient.signup.bind(AuthClient))
 };
 
 export const login = async (email: string, password: string) => {
   try {
+    console.log("Attempting to login with email:", email);
     const res = await serverMethods.login({
       email, password
     });
-    return res.userId
+    console.log("Login response:", res);
+    const cookieStore = await cookies()
+    cookieStore.set("userId", res.userId)
+    return [true, null];
   } catch (error) {
-    console.error("Login failed:", error);
+    console.error("Login error:", error);
+    return [false, util.inspect(error)];
+  }
+}
+
+
+export const signup = async (name:string, email: string, password: string) => {
+  try {
+    const res = await serverMethods.signup({
+      name, email, password
+    });
+    const cookieStore = await cookies()
+    cookieStore.set("userId", res.userId)
+    return [true, null];
+  } catch (error) {
+    console.error("Signup error:", error);
+    return [false, util.inspect(error)];
   }
 }
---
+++ web/src/app/auth/_components/login-form.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import { login, signup } from "@/actions/auth";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { HoverCardContent } from "@/components/ui/hover-card";
+import { HoverCard, HoverCardTrigger } from "@radix-ui/react-hover-card";
+import { Lock, LogIn, ScanFace } from "lucide-react";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+export default function LoginForm({ withSSO = false }: { withSSO?: boolean }) {
+  const form = useForm();
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(async (data) => {
+          await login(data.email, data.password);
+        })}
+      >
+        <Card className="w-[400px] mx-auto mt-20">
+          <CardHeader>
+            <CardTitle className="flex items-center gap-2">
+              <ScanFace />
+              Sign in
+            </CardTitle>
+            <CardDescription>to access account</CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={() => {
+                return (
+                  <FormItem>
+                    <FormLabel>
+                      Email
+                    </FormLabel>
+                    <FormControl>
+                      <input
+                        type="email"
+                        placeholder="Email"
+                        className="border p-2 rounded-md w-full"
+                        {...form.register("email", {
+                          required: "Email is required",
+                          pattern: {
+                            value:
+                              /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: "Invalid email address",
+                          },
+                        })}
+                      />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={() => {
+                return (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <input
+                        type="password"
+                        placeholder="Password"
+                        className="border p-2 rounded-md w-full"
+                        {...form.register("password", {
+                          required: "Password is required",
+                          minLength: {
+                            value: 6,
+                            message: "Password must be at least 6 characters",
+                          },
+                        })}
+                      />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+          </CardContent>
+          <CardFooter className="flex flex-col gap-4 items-center">
+            <div className="w-full flex justify-between text-sm items-center">
+              <HoverCard>
+                <HoverCardTrigger className="cursor-pointer">
+                  Forgot Password?
+                </HoverCardTrigger>
+                <HoverCardContent className="text-sm">
+                  Contact administrator to reset your password.
+                </HoverCardContent>
+              </HoverCard>
+              <Button type="submit">
+                <LogIn />
+                Login
+              </Button>
+            </div>
+            {withSSO && (
+              <>
+                <div className="flex items-center gap-2 text-sm">
+                  <hr className="w-[100px]" />
+                  OR
+                  <hr className="w-[100px]" />
+                </div>
+
+                <div className="flex gap-2">
+                  <Button variant={"secondary"} asChild>
+                    <Link href="/teams">
+                      <Lock />
+                      Login With SSO
+                    </Link>
+                  </Button>
+                </div>
+              </>
+            )}
+          </CardFooter>
+        </Card>
+        {!withSSO && (
+          <div className="p-4 text-sm flex gap-2 justify-center">
+            <span>
+              Don't have an account?
+            </span>
+            <Link href="/auth/signup" className="text-blue-500">
+              Signup
+            </Link>
+          </div>
+        )}
+      </form>
+    </Form>
+  );
+}
