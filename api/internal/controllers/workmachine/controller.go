2262bf5e09668a45f38af26ef387d638a2f49fb4
Karthik Th <karthik@kloudlite.io>
2025-11-07 01:38:37 +0530

    fix: use consistent SSH paths in both host manager and workspace sshd configs
    Changed workspace sshd_config AuthorizedKeysFile from
    /etc/ssh/kl-authorized-keys/authorized_keys to
    /var/lib/kloudlite/ssh-config/authorized_keys
    
    Both host manager and workspace SSH servers now use:
    - AuthorizedKeysFile /var/lib/kloudlite/ssh-config/authorized_keys
    - HostKey /var/lib/kloudlite/ssh-config/ssh_host_rsa_key

--- api/internal/controllers/workmachine/controller.go
+++ api/internal/controllers/workmachine/controller.go
@@ -894,7 +894,7 @@ PubkeyAuthentication yes
 PasswordAuthentication no
 PermitEmptyPasswords no
 ChallengeResponseAuthentication no
-AuthorizedKeysFile /etc/ssh/kl-authorized-keys/authorized_keys
+AuthorizedKeysFile /var/lib/kloudlite/ssh-config/authorized_keys
 
 # Host Keys
 HostKey /var/lib/kloudlite/ssh-config/ssh_host_rsa_key
