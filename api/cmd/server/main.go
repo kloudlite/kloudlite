ce9d1965d4e194e492b45370b88ea36ad7d3578c
Karthik Th <karthik@kloudlite.io>
2026-02-01 09:20:41 +0530

    Test API server deployment automation
    Add startup log message to verify deployment pipeline

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -26,6 +26,8 @@ func main() {
 	}
 	defer appLogger.Sync()
 
+	appLogger.Info("🚀 API Server starting - Build: 2026-01-31-TEST-DEPLOYMENT")
+
 	// Create and start server
 	srv := server.New(cfg, appLogger)
 
