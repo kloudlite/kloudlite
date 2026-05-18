378577bf656f0a957d78ccbc31a407669d81ea8f
Karthik Thirumalasetti <karthik@kloudlite.io>
2026-05-18 12:47:06 +0000

    refactor: remove user dashboard routes

--- web/apps/dashboard/src/app/(main)/artifacts/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-// Redirect to the default artifact type (container repos)
-export default function ArtifactsPage() {
-  redirect('/artifacts/container-repos')
-}
